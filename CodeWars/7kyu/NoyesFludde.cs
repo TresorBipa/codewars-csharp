1f578154dd6d99f883265a44e81151d7ab692215
Alexander Kozhanov <a.kozhanov@outlook.com>
2020-10-14 23:59:17 +0300

    Added solution "Noye's Fludde" 7 kyu

---
+++ CodeWars/7kyu/NoyesFludde.cs
@@ -0,0 +1,45 @@
+﻿/*
+https://www.codewars.com/kata/57ab1f7cbb9944a7860002a6/train/csharp
+
+7 kyu
+Noye's Fludde
+
+There is a man, and there is a boat, and there are a lot of animals.
+
+This man wants to get his animals onto the boat, but only in pairs, and in alphabetical order. 
+If there are any excess animals they are to be cruelly abandoned. 
+He has developed a helpful to code to make life easier, where animals are referred to by the first letter of their name. 
+Unfortunately there are more than 26 animals, for example there are 'g' - Gerbils and 'G' - Giraffes. 
+These are very different animals and if they got mixed up it could be a disaster!
+
+The animals are currently in a long and disorganised queue. like so ['g','p','h','g','c','c'] In this case we can see that there are two gerbils, 
+one pelican, one hamster, and two cats. They are out of order.
+
+Your job is arrange the animals into pairs like so [[‘c','c'],['g','g']] remember that animals are case sensitive! 
+And for everyone’s convenience we want the bigger animals to come before smaller animals that occupy the same place in the alphabet. 
+Giraffes before gerbils! alsoo please note we only want one pair of each animal, boat space is limited
+
+Oh and one last thing, there are rumours of sneaky numbers trying to get on board.
+
+Don't let any numbers onto the boat!!
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class NoyesFludde
+    {
+        public static char[][] BoatLoader(object[] animals)
+        {
+            return animals
+                .OfType<char>()
+                .GroupBy(c => c)
+                .Where(x => x.Count() > 1)
+                .OrderBy(x => char.ToUpper(x.Key))
+                .ThenBy(x => char.IsLower(x.Key))
+                .Select(x => new[] {x.Key, x.Key})
+                .ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/NoyesFluddeTests.cs
@@ -0,0 +1,39 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    public class NoyesFluddeTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var expected = new char[][] {new char[] {'c', 'c'}, new char[] {'g', 'g'}};
+            var actual = NoyesFludde.BoatLoader(new object[] {'g', 'c', 'h', 'c', 'g', 'm'});
+            Assert.AreEqual(expected, actual, "Your result did not match up properly! This is a titanic problem!");
+
+            expected = new char[][]
+            {
+                new char[] {'a', 'a'}, new char[] {'F', 'F'},
+                new char[] {'G', 'G'}, new char[] {'g', 'g'}, new char[] {'r', 'r'},
+                new char[] {'s', 's'}, new char[] {'X', 'X'}
+            };
+            actual = NoyesFludde.BoatLoader(new object[]
+            {
+                'a', 'd', 'G', 'V', 'X', 'F', 'g', 'h', 's', 'r', 'a', 'g', 'n', 's', 'e', 'r', 'j', 'a', 'f', 'D', 'F',
+                'G', 'R', 'H', 'C', 'X', 'B', 'N', 'G', 'U', 'G', 'F', 'p', 's', 'r', 'g', 'a'
+            });
+            Assert.AreEqual(expected, actual, "More animals, more problems");
+
+            expected = new char[][] {new char[] {'c', 'c'}, new char[] {'G', 'G'}, new char[] {'g', 'g'}};
+            actual = NoyesFludde.BoatLoader(new object[] {'g', 'g', 'G', 'c', 'p', 'x', 'z', 'Z', 'G', 'c', 'g', 'g'});
+            Assert.AreEqual(expected, actual,
+                "Oh no! The animals are not in size order! The bedding arrangments will get very confused!");
+
+            expected = new char[][] {new char[] {'c', 'c'}, new char[] {'G', 'G'}, new char[] {'g', 'g'}};
+            actual = NoyesFludde.BoatLoader(new object[]
+                {5, 6, 5, 'g', 'g', 'G', 'c', 'p', 'x', 'z', 'Z', 'G', 'c', 'g', 'g'});
+            Assert.AreEqual(expected, actual, "Some insiduous numbers snuck onto the boat! All is lost!");
+        }
+    }
+}
