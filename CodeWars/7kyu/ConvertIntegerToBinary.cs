7e6c88f5c1662b312a7c96d08295a785a2e62642
Alexander Kozhanov <a.kozhanov@outlook.com>
2020-10-16 07:55:54 +0300

    Added solution "Convert Integer to Binary" 7 kyu

---
+++ CodeWars/7kyu/ConvertIntegerToBinary.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/55606aeebf1f0305f900006f/csharp
+
+7 kyu
+Convert Integer to Binary
+
+Convert integers to binary as simple as that. You would be given an integer as a argument and you have to return its binary form. 
+To get an idea about how to convert a decimal number into a binary number, visit here.
+
+Notes: negative numbers should be handled as two's complement; assume all numbers are integers stored using 4 bytes (or 32 bits) in any language.
+
+Your output should ignore leading 0s.
+
+So, for example:
+
+Converter.ToBinary(3) == "11"
+Converter.ToBinary(-3) == "11111111111111111111111111111101"
+Be Ready for Large Numbers. Happy Coding ^_^
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class ConvertIntegerToBinary
+    {
+        public static string ToBinary(int n)
+        {
+            return Convert.ToString(n, 2);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ConvertIntegerToBinaryTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ConvertIntegerToBinaryTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("10", ConvertIntegerToBinary.ToBinary(2));
+            Assert.AreEqual("11", ConvertIntegerToBinary.ToBinary(3));
+            Assert.AreEqual("100", ConvertIntegerToBinary.ToBinary(4));
+            Assert.AreEqual("101", ConvertIntegerToBinary.ToBinary(5));
+        }
+    }
+}
