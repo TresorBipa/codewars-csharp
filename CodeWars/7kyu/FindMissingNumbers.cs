cb392b786e5897903c9ca64997fd2da8291510ab
Alexander Kozhanov <a.kozhanov@outlook.com>
2020-10-17 09:05:54 +0300

    Added solution "Find missing numbers" 7 kyu

---
+++ CodeWars/7kyu/FindMissingNumbers.cs
@@ -0,0 +1,31 @@
+﻿/*
+https://www.codewars.com/kata/56d02e6cc6c8b49c510005bb/csharp
+
+7 kyu
+Find missing numbers
+
+You will get an array of numbers.
+
+Every preceding number is smaller than the one following it.
+
+Some numbers will be missing, for instance:
+[-3,-2,1,5] //missing numbers are: -1,0,2,3,4
+
+Your task is to return an array of those missing numbers:
+[-1,0,2,3,4]
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataFindMissingNumbers
+    {
+        public static int[] FindMissingNumbers(int[] arr)
+        {
+            return arr.Any() ? Enumerable.Range(arr[0], arr[^1] - arr[0]).Except(arr).ToArray() : new int[0];
+            // return arr.Any() ? Enumerable.Range(arr[0], arr[^1] - arr[0]).Except(arr).ToArray() : Array.Empty<int>();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/FindMissingNumbersTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataFindMissingNumbersTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new int[] { -1, 0, 2, 3 },
+                KataFindMissingNumbers.FindMissingNumbers(new int[] { -3, -2, 1, 4 }));
+            Assert.AreEqual(new int[0], KataFindMissingNumbers.FindMissingNumbers(new int[] { -1, 0, 1, 2, 3, 4 }));
+            Assert.AreEqual(new int[0], KataFindMissingNumbers.FindMissingNumbers(new int[0]));
+        }
+    }
+}
