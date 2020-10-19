3a00a79915ed005aa85b5b26c2dd4db42f5fd466
Alexander Kozhanov <a.kozhanov@outlook.com>
2020-10-19 10:07:46 +0300

    Added solution "Fix My Phone Numbers!" 7 kyu

---
+++ CodeWarsTests/7kyu/FixMyPhoneNumbersTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FixMyPhoneNumbersTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("02078834982", FixMyPhoneNumbers.IsItANum("S:)0207ERGQREG88349F82!efRF)"));
+            Assert.AreEqual("Not a phone number", FixMyPhoneNumbers.IsItANum("sjfniebienvr12312312312ehfWh"));
+            Assert.AreEqual("Not a phone number", FixMyPhoneNumbers.IsItANum("0192387415456"));
+            Assert.AreEqual("02084564165", FixMyPhoneNumbers.IsItANum("v   uf  f 0tt2eg qe0b 8rtyq4eyq564()(((((165"));
+            Assert.AreEqual("Not a phone number",
+                FixMyPhoneNumbers.IsItANum("stop calling me no I have never been in an accident"));
+        }
+    }
+}
