bff8e5642cc0f63c943b995f512b88f4b4c11d34
Alexander Kozhanov <a.kozhanov@outlook.com>
2020-10-20 09:10:25 +0300

    Added solution "Get Length" 7 kyu

---
+++ CodeWars/7kyu/GetLength.cs
@@ -0,0 +1,23 @@
+﻿/*
+https://www.codewars.com/kata/566f571ed78037c7b6000036/csharp
+
+7 kyu
+Get Length
+
+Get Length
+You don't have any idea what is the type of the input object, 
+but you know that it has Length property. (The input can be any type of object that has Length property)
+
+So, the GetLength method should return the Length of input.
+*/
+
+namespace CodeWars
+{
+    public class KataGetLength
+    {
+        public static int GetLength(dynamic input)
+        {
+            return input.Length;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/GetLengthTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Dynamic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataGetLengthTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            int[] a = new int[] { 1, 2, 3, 4, 5 };
+            Assert.AreEqual(5, KataGetLength.GetLength(a));
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            string a = "abcde";
+            Assert.AreEqual(5, KataGetLength.GetLength(a));
+        }
+
+        // [Test]
+        // public void ThirdTest()
+        // {
+        //     object a = new { Length = 5, Width = 2 };
+        //     Assert.AreEqual(5, KataGetLength.GetLength(a));
+        // }
+
+        [Test]
+        public void FourthTest()
+        {
+            dynamic a = new ExpandoObject();
+            a.Length = 5;
+            Assert.AreEqual(5, KataGetLength.GetLength(a));
+        }
+    }
+}
