d3a1a46670c739d6716e38d3e2689ea604ba9e95
Alexander Kozhanov <a.kozhanov@outlook.com>
2020-10-21 17:14:47 +0300

    Added solution "IPv4 Validator" 7 kyu

---
+++ CodeWars/7kyu/IPv4Validator.cs
@@ -0,0 +1,39 @@
+﻿/*
+https://www.codewars.com/kata/57193694938fcdfe3a001dd7/csharp
+
+7 kyu
+IPv4 Validator
+
+In this kata you have to write a method to verify the validity of IPv4 addresses.
+
+Example of valid inputs:
+
+"1.1.1.1"
+
+"127.0.0.1"
+
+"255.255.255.255"
+
+Example of invalid input:
+
+"1444.23.9"
+
+"153.500.234.444"
+
+"-12.344.43.11"
+*/
+
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class IPv4Validator
+    {
+        public static bool IpValidator(string ip)
+        {
+            // return Regex.IsMatch(ip, @"^(?:(?:^|\.)(?:2(?:5[0-5]|[0-4]\d)|1?\d?\d)){4}$");
+            return ip.Split('.').Length == 4 && IPAddress.TryParse(ip, out _);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/IPv4ValidatorTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class IPv4ValidatorTests
+    {
+        [TestCase("123.456.789.0", false)]
+        [TestCase("127.0.0.1", true)]
+        public void IpValidatorTest(string ip, bool isValid)
+        {
+            Assert.AreEqual(isValid, IPv4Validator.IpValidator(ip));
+        }
+    }
+}
