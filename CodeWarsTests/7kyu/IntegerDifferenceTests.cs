18838868602a5f8547f69faf2ec6fa8739fa8e27
Alexander Kozhanov <a.kozhanov@outlook.com>
2020-10-22 12:12:45 +0300

    Added solution "Integer Difference" 7 kyu

---
+++ CodeWarsTests/7kyu/IntegerDifferenceTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class IntegerDifferenceTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(3, IntegerDifference.IntDiff(new int[] { 1, 1, 5, 6, 9, 16, 27 }, 4));
+            Assert.AreEqual(4, IntegerDifference.IntDiff(new int[] { 1, 1, 3, 3 }, 2));
+        }
+    }
+}
