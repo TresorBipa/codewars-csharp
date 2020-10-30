4d3169790b441687693d31a5a263c7f7239bd894
Alexander Kozhanov <a.kozhanov@outlook.com>
2020-10-30 12:27:31 +0300

    Added solution "The Office III - Broken Photocopier" 7 kyu

---
+++ CodeWars/7kyu/OfficeIIIBrokenPhotocopier.cs
@@ -0,0 +1,34 @@
+﻿/*
+https://www.codewars.com/kata/57ed56657b45ef922300002b/csharp
+
+7 kyu
+The Office III - Broken Photocopier
+
+The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+
+Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+
+Given a string of binary, return the version the photocopier gives you as a string.
+
+The Office I - Outed
+The Office II - Boredeom Score
+The Office IV - Find a Meeting Room
+The Office V - Find a Chair
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class OfficeIIIBrokenPhotocopier
+    {
+        public static string Broken(string x)
+        {
+            return x.Aggregate("", (s, c) => s + (c == '0' ? '1' : '0'));
+            // return string.Concat(x.Select(a => 1 - (a - '0')));
+            // return string.Concat(x.Select(a => a == '0' ? '1' : '0'));
+            // return x.Replace('1', '*').Replace('0', '1').Replace('*', '0');
+            // return new string(x.Select(e => e == '1' ? '0' : '1').ToArray());
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/OfficeIIIBrokenPhotocopierTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class OfficeIIIBrokenPhotocopierTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("0", OfficeIIIBrokenPhotocopier.Broken("1"));
+            
+            Assert.AreEqual("01111111010010000001100110111",
+                OfficeIIIBrokenPhotocopier.Broken("10000000101101111110011001000"));
+            
+            Assert.AreEqual("011101", OfficeIIIBrokenPhotocopier.Broken("100010"));
+        }
+    }
+}
