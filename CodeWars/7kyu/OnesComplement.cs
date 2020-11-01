8852342c802b42362626ec5745a6b4940c6426b7
Alexander Kozhanov <a.kozhanov@outlook.com>
2020-11-01 09:30:10 +0300

    Added solution "Ones' Complement" 7 kyu

---
+++ CodeWars/7kyu/OnesComplement.cs
@@ -0,0 +1,28 @@
+﻿/*
+https://www.codewars.com/kata/59b11f57f322e5da45000254/csharp
+
+7 kyu
+Ones' Complement
+
+The Ones' Complement of a binary number is the number obtained by swapping all the 0s for 1s and all the 1s for 0s. For example:
+
+onesComplement(1001) = 0110
+onesComplement(1001) = 0110
+For any given binary number,formatted as a string, return the Ones' Complement of that number.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataOnesComplement
+    {
+        public static string OnesComplement(string n)
+        {
+            //return n.Replace('1', 'x').Replace('0', '1').Replace('x', '0');
+            //return string.Concat(n.Select(x => x == '1' ? "0" : "1"));
+            return string.Concat(n.Select(c => (char) (c ^ 1)));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/OnesComplementTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataOnesComplementTests
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual("1", KataOnesComplement.OnesComplement("0"));
+            Assert.AreEqual("0", KataOnesComplement.OnesComplement("1"));
+            Assert.AreEqual("01", KataOnesComplement.OnesComplement("10"));
+            Assert.AreEqual("10", KataOnesComplement.OnesComplement("01"));
+            Assert.AreEqual("0010", KataOnesComplement.OnesComplement("1101"));
+        }
+    }
+}
