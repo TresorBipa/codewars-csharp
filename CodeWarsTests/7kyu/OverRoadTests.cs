c8445f2edb49f15b16fcbbd77b482c6027fd72bb
Alexander Kozhanov <a.kozhanov@outlook.com>
2020-11-04 12:35:29 +0300

    Added solution "Over The Road" 7 kyu

---
+++ CodeWarsTests/7kyu/OverRoadTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class OverRoadTests
+    {
+        [Test]
+        public void basic_test_cases()
+        {
+            Assert.AreEqual(6, OverRoad.overTheRoad(1, 3));
+            Assert.AreEqual(4, OverRoad.overTheRoad(3, 3));
+            Assert.AreEqual(5, OverRoad.overTheRoad(2, 3));
+            Assert.AreEqual(8, OverRoad.overTheRoad(3, 5));
+            Assert.AreEqual(16, OverRoad.overTheRoad(7, 11));
+        }
+    }
+}
