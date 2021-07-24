c5ab4a553c2c791571ef85c60237fb13309bd9c9
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-07-24 09:05:58 +0300

    Added solution "Holiday III - Fire on the boat" 7 kyu

---
+++ CodeWars/7kyu/HolidayIIIFireOnBoat.cs
@@ -0,0 +1,27 @@
+﻿/*
+https://www.codewars.com/kata/57e8fba2f11c647abc000944/csharp
+
+7 kyu
+Holiday III - Fire on the boat
+
+Enjoying your holiday, you head out on a scuba diving trip!
+
+Disaster!! The boat has caught fire!!
+
+You will be provided a string that lists many boat related items. 
+If any of these items are "Fire" you must spring into action. 
+Change any instance of "Fire" into "~~". Then return the string.
+
+Go to work!
+*/
+
+namespace CodeWars
+{
+    public class HolidayIIIFireOnBoat
+    {
+        public static string FireFight(string s)
+        {
+            return s.Replace("Fire", "~~");
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/HolidayIIIFireOnBoatTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HolidayIIIFireOnBoatTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast",
+                HolidayIIIFireOnBoat.FireFight(
+                    "Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"));
+
+            Assert.AreEqual("Mast Deck Engine Water ~~", HolidayIIIFireOnBoat.FireFight("Mast Deck Engine Water Fire"));
+
+            Assert.AreEqual("~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain",
+                HolidayIIIFireOnBoat.FireFight(
+                    "Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"));
+        }
+    }
+}
