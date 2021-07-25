20177392402d741e47d47db815153a970d386e33
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-07-25 13:08:37 +0300

    Added solution "Upturn Numeral Triangle" 7 kyu

---
+++ CodeWars/7kyu/KataUpturnNumeralTriangle.cs
@@ -0,0 +1,110 @@
+﻿/*
+https://www.codewars.com/kata/564f3d49a06556d27c000077/csharp
+
+7 kyu
+Upturn Numeral Triangle
+
+Task
+Raj has got into a problem, he solved the triangle pattern but stuck in the codes of "inverse triangle". 
+Help him with the codes and remember to get the output as per given in examples below.
+
+Rules:
+Take input as 'n' which is always a natural number
+Space between each digit
+No space after the rows end
+Examples
+pattern(5)
+
+1 1 1 1 1
+ 2 2 2 2
+  3 3 3
+   4 4
+    5
+    
+    
+pattern(9)
+
+  1 1 1 1 1 1 1 1 1
+   2 2 2 2 2 2 2 2
+    3 3 3 3 3 3 3
+     4 4 4 4 4 4
+      5 5 5 5 5
+       6 6 6 6
+        7 7 7
+         8 8
+          9
+        
+        
+pattern(16)
+
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+ 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+  3 3 3 3 3 3 3 3 3 3 3 3 3 3
+   4 4 4 4 4 4 4 4 4 4 4 4 4
+    5 5 5 5 5 5 5 5 5 5 5 5
+     6 6 6 6 6 6 6 6 6 6 6
+      7 7 7 7 7 7 7 7 7 7
+       8 8 8 8 8 8 8 8 8
+        9 9 9 9 9 9 9 9
+         0 0 0 0 0 0 0
+          1 1 1 1 1 1
+           2 2 2 2 2
+            3 3 3 3
+             4 4 4
+              5 5
+               6
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataUpturnNumeralTriangle
+    {
+        public static string UpturnNumeralTriangle(int n)
+        {
+            return string.Join("\n",
+                Enumerable.Range(1, n).Select(i =>
+                    new string(' ', i) + string.Join(" ", Enumerable.Repeat(i % 10, n + 1 - i))));
+
+
+            // return string.Join("\n", Enumerable.Range(1, n).Select(i => new string(' ', i) +
+            //                                                             string.Join(" ",
+            //                                                                 (new string((char) ((i % 10) + '0'),
+            //                                                                     n - i + 1)).ToCharArray())));
+
+
+            // return string.Join("\n",
+            //     Enumerable.Range(1, n).Select(i => string.Concat(
+            //         Enumerable.Repeat(" ", i - 1).Concat(
+            //             Enumerable.Repeat($" {i % 10}", n - i + 1)))));
+
+
+            // return string.Join("\n",
+            //     Enumerable.Range(1, n).Select(i =>
+            //         $"{new string(' ', i)}{string.Join(" ", Enumerable.Repeat(i % 10, n + 1 - i))}"));
+
+
+            // return string.Join("\n",
+            //     Enumerable.Range(0, n).Select(i =>
+            //         new string(' ', i) + string.Concat(Enumerable.Range(1, n - i).Select(y => $" {(i + 1) % 10}"))));
+
+
+            // return string.Join("\n",
+            //     Enumerable.Range(1, n).Select(x =>
+            //         $"{new string(' ', x)}{new string('*', n - x + 1).Replace("*", $"{x % 10} ")}".TrimEnd()));
+
+
+            // List<string> result = new List<string>();
+            // for (int i = 1; i <= n; i++)
+            // {
+            //     result.Add(new string(' ', i) + string.Join(" ", Enumerable.Repeat(i % 10, n + 1 - i)));
+            //     result.Add(new string(' ', i) + string.Join(" ", new string($"{i % 10}"[0], n + 1 - i).ToArray()));
+            //     result.Add($"{new string(' ', i)}{string.Join(" ", new string($"{i % 10}"[0], n + 1 - i).ToArray())}");
+            // }
+            //
+            // return string.Join("\n", result);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/KataUpturnNumeralTriangleTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataUpturnNumeralTriangleTests
+    {
+        [Test]
+        public void Pattern7()
+        {
+            string expectetPattern =
+                " 1 1 1 1 1 1 1\n  2 2 2 2 2 2\n   3 3 3 3 3\n    4 4 4 4\n     5 5 5\n      6 6\n       7";
+            Assert.AreEqual(expectetPattern, KataUpturnNumeralTriangle.UpturnNumeralTriangle(7));
+        }
+
+        [Test]
+        public void Pattern12()
+        {
+            string expectetPattern =
+                " 1 1 1 1 1 1 1 1 1 1 1 1\n  2 2 2 2 2 2 2 2 2 2 2\n   3 3 3 3 3 3 3 3 3 3\n    4 4 4 4 4 4 4 4 4\n     5 5 5 5 5 5 5 5\n      6 6 6 6 6 6 6\n       7 7 7 7 7 7\n        8 8 8 8 8\n         9 9 9 9\n          0 0 0\n           1 1\n            2";
+            Assert.AreEqual(expectetPattern, KataUpturnNumeralTriangle.UpturnNumeralTriangle(12));
+        }
+
+        [Test]
+        public void Pattern21()
+        {
+            string expectetPattern =
+                " 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n  2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2\n   3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3\n    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4\n     5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5\n      6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6\n       7 7 7 7 7 7 7 7 7 7 7 7 7 7 7\n        8 8 8 8 8 8 8 8 8 8 8 8 8 8\n         9 9 9 9 9 9 9 9 9 9 9 9 9\n          0 0 0 0 0 0 0 0 0 0 0 0\n           1 1 1 1 1 1 1 1 1 1 1\n            2 2 2 2 2 2 2 2 2 2\n             3 3 3 3 3 3 3 3 3\n              4 4 4 4 4 4 4 4\n               5 5 5 5 5 5 5\n                6 6 6 6 6 6\n                 7 7 7 7 7\n                  8 8 8 8\n                   9 9 9\n                    0 0\n                     1";
+            Assert.AreEqual(expectetPattern, KataUpturnNumeralTriangle.UpturnNumeralTriangle(21));
+        }
+    }
+}
