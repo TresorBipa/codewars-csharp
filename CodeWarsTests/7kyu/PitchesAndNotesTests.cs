e9d7418a2cdfacdd9521c23830be9ba6674a4a53
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-07-29 09:38:56 +0300

    Added solution "Pitches and Notes" 7 kyu

---
+++ CodeWarsTests/7kyu/PitchesAndNotesTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PitchesAndNotesTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(PitchesAndNotes.GetNote(440), Is.EqualTo("A"));
+            Assert.That(PitchesAndNotes.GetNote(220), Is.EqualTo("A"));
+            Assert.That(PitchesAndNotes.GetNote(880), Is.EqualTo("A"));
+
+            Assert.That(PitchesAndNotes.GetNote(523.25), Is.EqualTo("C"));
+            Assert.That(PitchesAndNotes.GetNote(261.625), Is.EqualTo("C"));
+            Assert.That(PitchesAndNotes.GetNote(1046.5), Is.EqualTo("C"));
+        }
+    }
+}
