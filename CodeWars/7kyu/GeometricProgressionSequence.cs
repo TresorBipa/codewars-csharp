11aeb734d3199b181d929a705401a5d6d1e6e500
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-08-02 11:33:46 +0300

    Added solution "Geometric Progression Sequence" 7 kyu

---
+++ CodeWars/7kyu/GeometricProgressionSequence.cs
@@ -0,0 +1,29 @@
+﻿/*
+https://www.codewars.com/kata/55caef80d691f65cb6000040/csharp
+
+7 kyu
+Geometric Progression Sequence
+
+In your class, you have started lessons about geometric progression. Since you are also a programmer, 
+you have decided to write a function that will print first n elements of the sequence with the given constant r and first element a.
+
+Result should be separated by comma and space.
+
+Example
+Kata.GeometricSequenceElements(2, 3, 5); // => "2, 6, 18, 54, 162"
+More info: https://en.wikipedia.org/wiki/Geometric_progression
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class GeometricProgressionSequence
+    {
+        public static string GeometricSequenceElements(int a, int r, int n)
+        {
+            return string.Join(", ", Enumerable.Range(0, n).Select(i => Math.Pow(r, i) * a));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/GeometricProgressionSequenceTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GeometricProgressionSequenceTests
+    {
+        [Test]
+        public void FixedTests()
+        {
+            Assert.AreEqual("2, 6, 18, 54, 162", GeometricProgressionSequence.GeometricSequenceElements(2, 3, 5));
+            Assert.AreEqual("2, 4, 8, 16, 32, 64, 128, 256, 512, 1024",
+                GeometricProgressionSequence.GeometricSequenceElements(2, 2, 10));
+            Assert.AreEqual("1, -2, 4, -8, 16, -32, 64, -128, 256, -512",
+                GeometricProgressionSequence.GeometricSequenceElements(1, -2, 10));
+        }
+    }
+}
