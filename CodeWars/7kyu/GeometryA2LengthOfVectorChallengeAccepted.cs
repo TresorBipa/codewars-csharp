33bd569c42d30b907bb3abd1db49ba790f126038
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-08-03 19:35:15 +0300

    Added solution "[Geometry A-2]: Length of a vectorChallenge accepted" 7 kyu

---
+++ CodeWars/7kyu/GeometryA2LengthOfVectorChallengeAccepted.cs
@@ -0,0 +1,109 @@
+﻿/*
+https://www.codewars.com/kata/554dc2b88fbafd2e95000125/csharp
+
+7 kyu
+[Geometry A-2]: Length of a vectorChallenge accepted
+
+For a given 2D vector described by cartesian coordinates of its initial point and terminal point in the following format:
+
+// Argument will be passed as a Vector2
+
+public struct Vector2
+{
+  public Point2 Head;
+  public Point2 Tail;
+
+  public Vector2(Point2 tail, Point2 head)
+  {
+    this.Tail = tail;
+    this.Head = head;
+  }
+
+  public Point2 this[int i]
+  {
+    get
+    {
+      return new Point2[] {this.Tail, this.Head}[i];
+    }
+  }
+}
+
+public struct Point2
+{
+  public double X;
+  public double Y;
+
+  public Point2(double x, double y)
+  {
+    this.X = x;
+    this.Y = y;
+  }
+
+  public double this[int i]
+  {
+    get
+    {
+      return new double[] {this.X, this.Y}[i];
+    }
+  }
+}
+Your function must return this vector's length represented as a floating point number.
+
+Error must be within 1e-7.
+
+Coordinates can be integers or floating point numbers.
+*/
+
+using System;
+
+//using System.Numerics;
+
+namespace CodeWars
+{
+    public class GeometryA2LengthOfVectorChallengeAccepted
+    {
+        public struct Vector2
+        {
+            public Point2 Head;
+            public Point2 Tail;
+
+            public Vector2(Point2 tail, Point2 head)
+            {
+                this.Tail = tail;
+                this.Head = head;
+            }
+
+            public Point2 this[int i]
+            {
+                get { return new Point2[] {this.Tail, this.Head}[i]; }
+            }
+        }
+
+        public struct Point2
+        {
+            public double X;
+            public double Y;
+
+            public Point2(double x, double y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public double this[int i]
+            {
+                get { return new double[] {this.X, this.Y}[i]; }
+            }
+        }
+
+        public static double VectorLength(Vector2 vec)
+        {
+            //return Math.Sqrt(Math.Pow(vec[0][0] - vec[1][0], 2) + Math.Pow(vec[0][1] - vec[1][1], 2));
+            //return Math.Sqrt(Math.Pow(vec.Tail.X - vec.Head.X, 2) + Math.Pow(vec.Tail.Y - vec.Head.Y, 2));
+
+            var x = vec.Head.X - vec.Tail.X;
+            var y = vec.Head.Y - vec.Tail.Y;
+            return Math.Sqrt(x * x + y * y);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/GeometryA2LengthOfVectorChallengeAcceptedTests.cs
@@ -0,0 +1,34 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GeometryA2LengthOfVectorChallengeAcceptedTests
+    {
+        private static double epsilon = 1e-7;
+
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.That(
+                GeometryA2LengthOfVectorChallengeAccepted.VectorLength(
+                    new GeometryA2LengthOfVectorChallengeAccepted.Vector2(
+                        new GeometryA2LengthOfVectorChallengeAccepted.Point2(0, 1),
+                        new GeometryA2LengthOfVectorChallengeAccepted.Point2(0, 0))),
+                Is.EqualTo(1).Within(epsilon));
+
+            Assert.That(
+                GeometryA2LengthOfVectorChallengeAccepted.VectorLength(
+                    new GeometryA2LengthOfVectorChallengeAccepted.Vector2(
+                        new GeometryA2LengthOfVectorChallengeAccepted.Point2(0, 3),
+                        new GeometryA2LengthOfVectorChallengeAccepted.Point2(4, 0))),
+                Is.EqualTo(5).Within(epsilon));
+
+            // Assert.That(
+            //     GeometryA2LengthOfVectorChallengeAccepted.VectorLength(
+            //         new GeometryA2LengthOfVectorChallengeAccepted(new GeometryA2LengthOfVectorChallengeAccepted.Point2(1, -1), new GeometryA2LengthOfVectorChallengeAccepted.Point2(1, -1))),
+            //     Is.EqualTo(0).Within(epsilon));
+        }
+    }
+}
