5659ca4b75e94b3a121eff4e0a8a31ce32783c85
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-08-04 14:48:45 +0300

    Added solution "Geometry Basics: Distance between circles in 2D" 7 kyu

---
+++ CodeWars/7kyu/GeometryBasicsDistanceBetweenCirclesIn2D.cs
@@ -0,0 +1,45 @@
+﻿/*
+https://www.codewars.com/kata/58e3031ce265671f6a000542/csharp
+
+7 kyu
+Geometry Basics: Distance between circles in 2D
+
+This series of katas will introduce you to basics of doing geometry with computers.
+Point objects have x, y attributes. Circle objects have center which is a Point, and radius which is a number.
+Write a function calculating distance between Circle a and Circle b.
+If they're overlapping or one is completely within the other, just return zero.
+Tests round answers to 6 decimal places, so you don't need to round them yourselves.
+*/
+
+using System.Drawing;
+using System;
+
+namespace CodeWars
+{
+    public class GeometryBasicsDistanceBetweenCirclesIn2D
+    {
+        public class Circle
+        {
+            public Point Center { get; set; }
+            public int Radius { get; set; }
+
+            public Circle(Point cen, int rad)
+            {
+                Center = cen;
+                Radius = rad;
+            }
+        }
+
+        public static double DistanceBetweenCircles(Circle a, Circle b)
+        {
+            // var dx = a.Center.X - b.Center.X;
+            // var dy = a.Center.Y - b.Center.Y;
+            // var dz = Math.Sqrt(dx * dx + dy * dy);
+            // return Math.Max(0.0, dz - a.Radius - b.Radius);
+
+
+            return Math.Max(0, Math.Sqrt(Math.Pow(a.Center.X - b.Center.X, 2) + Math.Pow(a.Center.Y - b.Center.Y, 2)) -
+                               a.Radius - b.Radius);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/GeometryBasicsDistanceBetweenCirclesIn2DTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GeometryBasicsDistanceBetweenCirclesIn2DTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(32,
+                Math.Round(
+                    GeometryBasicsDistanceBetweenCirclesIn2D.DistanceBetweenCircles(
+                        new GeometryBasicsDistanceBetweenCirclesIn2D.Circle(new Point(10, 60), 11),
+                        new GeometryBasicsDistanceBetweenCirclesIn2D.Circle(new Point(40, 20), 7)),
+                    6));
+            Assert.AreEqual(0,
+                Math.Round(
+                    GeometryBasicsDistanceBetweenCirclesIn2D.DistanceBetweenCircles(
+                        new GeometryBasicsDistanceBetweenCirclesIn2D.Circle(new Point(10, 60), 40),
+                        new GeometryBasicsDistanceBetweenCirclesIn2D.Circle(new Point(40, 20), 40)),
+                    6));
+            Assert.AreEqual(0,
+                Math.Round(
+                    GeometryBasicsDistanceBetweenCirclesIn2D.DistanceBetweenCircles(
+                        new GeometryBasicsDistanceBetweenCirclesIn2D.Circle(new Point(100, 100), 50),
+                        new GeometryBasicsDistanceBetweenCirclesIn2D.Circle(new Point(102, 110), 10)), 6));
+        }
+
+
+        [Test]
+        public void RandomTests()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                GeometryBasicsDistanceBetweenCirclesIn2D.Circle a =
+                    new GeometryBasicsDistanceBetweenCirclesIn2D.Circle(
+                        new Point(rand.Next(-50, 50), rand.Next(-50, 50)), rand.Next(1, 50));
+                GeometryBasicsDistanceBetweenCirclesIn2D.Circle b =
+                    new GeometryBasicsDistanceBetweenCirclesIn2D.Circle(
+                        new Point(rand.Next(-50, 50), rand.Next(-50, 50)), rand.Next(1, 50));
+                Assert.AreEqual(Math.Round(Solve.GetResult(a, b), 6),
+                    Math.Round(GeometryBasicsDistanceBetweenCirclesIn2D.DistanceBetweenCircles(a, b), 6));
+            }
+        }
+
+        public static class Solve
+        {
+            public static double GetResult(GeometryBasicsDistanceBetweenCirclesIn2D.Circle a,
+                GeometryBasicsDistanceBetweenCirclesIn2D.Circle b)
+            {
+                var distanceDiff =
+                    Math.Sqrt(Math.Pow(a.Center.X - b.Center.X, 2) + Math.Pow(a.Center.Y - b.Center.Y, 2)) - a.Radius -
+                    b.Radius;
+                return distanceDiff > 0 ? distanceDiff : 0;
+            }
+        }
+    }
+}
