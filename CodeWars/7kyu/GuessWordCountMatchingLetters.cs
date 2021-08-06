9d6847264191dc0bb0f72132b0abcb334ae9a59e
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-08-06 14:31:07 +0300

    Added solution "Guess the Word: Count Matching Letters" 7 kyu

---
+++ CodeWars/7kyu/GuessWordCountMatchingLetters.cs
@@ -0,0 +1,54 @@
+﻿/*
+https://www.codewars.com/kata/5912ded3f9f87fd271000120/csharp
+
+7 kyu
+Guess the Word: Count Matching Letters
+
+Consider a game, wherein the player has to guess a target word. All the player knows is the length of the target word.
+
+To help them in their goal, the game will accept guesses, and return the number of letters that are in the correct position.
+
+Write a method that, given the correct word and the player's guess, returns this number.
+
+For example, here's a possible thought process for someone trying to guess the word "dog":
+
+CountCorrectCharacters("dog", "car"); //0 (No letters are in the correct position)
+CountCorrectCharacters("dog", "god"); //1 ("o")
+CountCorrectCharacters("dog", "cog"); //2 ("o" and "g")
+CountCorrectCharacters("dog", "cod"); //1 ("o")
+CountCorrectCharacters("dog", "bog"); //2 ("o" and "g")
+CountCorrectCharacters("dog", "dog"); //3 (Correct!)
+The caller should ensure that the guessed word is always the same length as the correct word, 
+but since it could cause problems if this were not the case, you need to check for this eventuality:
+
+//Throw an InvalidOperationException if the two parameters are of different lengths.
+You may assume, however, that the two parameters will always be in the same case.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class GuessWordCountMatchingLetters
+    {
+        public int CountCorrectCharacters(string correctWord, string guess)
+        {
+            return correctWord.Length == guess.Length
+                ? correctWord.Where((x, i) => x == guess[i]).Count()
+                : throw new InvalidOperationException();
+
+            // return correctWord.Length == guess.Length
+            //     ? correctWord.Zip(guess, (x, y) => x == y).Count(x => x)
+            //     : throw new InvalidOperationException();
+
+            // return correctWord.Length == guess.Length
+            //     ? correctWord.Zip(guess, (x, y) => x == y ? 1 : 0).Sum()
+            //     : throw new InvalidOperationException();
+
+            // return correctWord.Length == guess.Length
+            //     ? Enumerable.Range(0, guess.Length).Count(i => guess[i] == correctWord[i])
+            //     : throw new InvalidOperationException();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/GuessWordCountMatchingLettersTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GuessWordCountMatchingLettersTests
+    {
+        [TestFixture]
+        public class WordGuesserTests
+        {
+            [TestCase("dog", "car", 0)]
+            [TestCase("dog", "god", 1)]
+            [TestCase("dog", "cog", 2)]
+            [TestCase("dog", "cod", 1)]
+            [TestCase("dog", "bog", 2)]
+            [TestCase("dog", "dog", 3)]
+            public void GuesserTest(string correctWord, string guess, int expected)
+            {
+                Assert.AreEqual(expected,
+                    new GuessWordCountMatchingLetters().CountCorrectCharacters(correctWord, guess));
+            }
+        }
+    }
+}
