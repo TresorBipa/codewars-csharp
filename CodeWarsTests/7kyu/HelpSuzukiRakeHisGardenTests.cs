690a50d26d59a9e589963e2cff303c2bf21b62da
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-08-12 09:50:03 +0300

    Added solution "Help Suzuki rake his garden!" 7 kyu

---
+++ CodeWarsTests/7kyu/HelpSuzukiRakeHisGardenTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HelpSuzukiRakeHisGardenTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var garden1 = "slug spider rock gravel gravel gravel gravel gravel gravel gravel";
+            var expectedGarden1 = "gravel gravel rock gravel gravel gravel gravel gravel gravel gravel";
+            Assert.AreEqual(expectedGarden1, HelpSuzukiRakeHisGarden.RakeGarden(garden1));
+
+            var garden2 =
+                "gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock gravel gravel gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel gravel gravel gravel gravel snail gravel gravel rock gravel snail slug gravel gravel spider gravel gravel gravel gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel";
+            var expectedGarden2 =
+                "gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel gravel rock gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel gravel gravel gravel gravel gravel rock gravel gravel";
+            Assert.AreEqual(expectedGarden2, HelpSuzukiRakeHisGarden.RakeGarden(garden2));
+        }
+    }
+}
