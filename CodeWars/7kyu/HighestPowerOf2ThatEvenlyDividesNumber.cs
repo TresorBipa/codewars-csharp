d47f621ad1726b4c6074ae38ab5e7afaf1d3225d
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-08-15 11:49:42 +0300

    Added solution "Highest power of 2 that evenly divides a number" 7 kyu

---
+++ CodeWars/7kyu/HighestPowerOf2ThatEvenlyDividesNumber.cs
@@ -0,0 +1,36 @@
+﻿/*
+https://www.codewars.com/kata/58dabca7ce77a12dbd00000f/csharp
+
+7 kyu
+Highest power of 2 that evenly divides a number
+
+Write a function that takes a number and returns highest power of 2 that evenly divides the number.
+
+For example:
+
+Kata.Solution(123)    => 1
+Kata.Solution(192)    => 64
+Kata.Solution(132232) => 8
+Assume that: number is an integer within the range [1..10^18].
+*/
+
+namespace CodeWars
+{
+    public class HighestPowerOf2ThatEvenlyDividesNumber
+    {
+        public static long Solution(long n)
+        {
+            // long p = 1;
+            // while ((n & 1) == 0)
+            // {
+            //     n >>= 1;
+            //     p <<= 1;
+            // }
+            //
+            // return p;
+
+
+            return n & -n;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/HighestPowerOf2ThatEvenlyDividesNumberTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HighestPowerOf2ThatEvenlyDividesNumberTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(1, HighestPowerOf2ThatEvenlyDividesNumber.Solution(123));
+            Assert.AreEqual(64, HighestPowerOf2ThatEvenlyDividesNumber.Solution(64));
+            Assert.AreEqual(8, HighestPowerOf2ThatEvenlyDividesNumber.Solution(132232));
+            Assert.AreEqual(64, HighestPowerOf2ThatEvenlyDividesNumber.Solution(192));
+        }
+    }
+}
