c1accd31d9bdeefd6bc46ac245d9bbdac1e02e12
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-08-18 21:48:18 +0300

    Added solution "How many times does it contain?" 7 kyu

---
+++ CodeWars/7kyu/HowManyTimesDoesItContain.cs
@@ -0,0 +1,37 @@
+﻿/*
+https://www.codewars.com/kata/584466950d3bedb9b300001f/csharp
+
+7 kyu
+How many times does it contain?
+
+Your task is to return how many times a string contains a given character.
+
+The function takes a string(inputS) as a parameter and a char(charS) which is the character that you will have to find and count.
+
+For example, if you get an input string "Hello world" and the character to find is "o", return 2.
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class HowManyTimesDoesItContain
+    {
+        public static int stringCounter(string inputS, string charS)
+        {
+            // return Regex.Replace(inputS, @"[^" + charS + "]", "").Length;
+            // return Regex.Matches(inputS, Regex.Escape(charS)).Count;
+            // return inputS.Count(x => Convert.ToString(x) == charS);
+            // return Regex.Replace(inputS, $"[^{charS}]", "").Length;
+            // return new Regex($"[{charS}]").Matches(inputS).Count();
+            // return inputS.Count(c => charS.Equals(c.ToString()));
+            // return inputS.Count(q => q == char.Parse(charS));
+            // return inputS.Count(c => c.ToString() == charS);
+            // return inputS.Split(charS[0]).Length - 1;
+            // return inputS.Count(x => x == charS[0]);
+            return inputS.Count(charS.Contains);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/HowManyTimesDoesItContainTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HowManyTimesDoesItContainTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(2, HowManyTimesDoesItContain.stringCounter("Hello world", "o"));
+            Assert.AreEqual(4, HowManyTimesDoesItContain.stringCounter("Wait isn't it supposed to be cynical?", "i"));
+            Assert.AreEqual(4,
+                HowManyTimesDoesItContain.stringCounter("I'm gona be the best code warrior ever dad", "r"));
+            Assert.AreEqual(1, HowManyTimesDoesItContain.stringCounter("Do you like Harry Potter?", "?"));
+        }
+    }
+}
