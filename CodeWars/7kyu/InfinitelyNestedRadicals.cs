2390ad7b5c4337383716ba507faa92a83be65787
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-08-20 00:17:44 +0300

    Added solution "Infinitely Nested Radicals" 7 kyu

---
+++ CodeWars/7kyu/InfinitelyNestedRadicals.cs
@@ -0,0 +1,79 @@
+﻿/*
+https://www.codewars.com/kata/5af2b240d2ee2764420000a2/csharp
+
+7 kyu
+Infinitely Nested Radicals
+
+Hello,
+
+I am Jomo Pipi
+
+and today we will be evaluating an expression like this:
+
+(there are an infinite number of radicals)
+
+x+x+x+x+⋯\sqrt{x + \sqrt{x + \sqrt{x + \sqrt{x + \cdots}}}} 
+x+ 
+x+ 
+x+ 
+x+⋯
+​	
+ 
+​	
+ 
+​	
+ 
+​	
+ 
+
+for a given value x
+
+Simple!
+
+arguments passed in will be 1 or greater
+
+(Check out my other kata!)
+
+Matrix Diagonal Sort OMG
+
+String -> N iterations -> String
+
+String -> X iterations -> String
+
+ANTISTRING
+
+Array - squareUp b!
+
+Matrix - squareUp b!
+
+Infinitely Nested Radical Expressions
+
+pipi Numbers!
+
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class InfinitelyNestedRadicals
+    {
+        public static double evaluateFunction(int x)
+        {
+            //return Math.Sqrt(x + .25) + .5;
+            return Math.Sqrt(x + 0.25) + 0.5;
+
+            //return (Math.Sqrt(4 * x + 1) + 1) / 2;
+            //return (1 + Math.Sqrt(1 + 4 * x)) / 2;
+            //return (1 + Math.Sqrt(1 + 4 * x)) / 2;
+            //return 0.5 * (1.0 + Math.Sqrt(4.0 * x + 1.0));
+
+            // var result = Math.Sqrt(x);
+            // for (int i = 0; i < 100; i++)
+            // {
+            //     result = Math.Sqrt(result + x);
+            // }
+            // return result;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/InfinitelyNestedRadicalsTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class InfinitelyNestedRadicalsTests
+    {
+        [Test]
+        public void testbasic()
+        {
+            Assert.AreEqual(2.0, InfinitelyNestedRadicals.evaluateFunction(2), 0.000000001);
+            Assert.AreEqual(3.0, InfinitelyNestedRadicals.evaluateFunction(6), 0.000000001);
+            Assert.AreEqual(4.0, InfinitelyNestedRadicals.evaluateFunction(12), 0.000000001);
+        }
+    }
+}
