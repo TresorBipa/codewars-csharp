801692738f70d94796e93519a0b2bd96ae718b11
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-08-22 18:53:45 +0300

    Added solution "Invalid Input - Error Handling #1" 7 kyu

---
+++ CodeWars/7kyu/InvalidInputErrorHandling1.cs
@@ -0,0 +1,85 @@
+﻿/*
+https://www.codewars.com/kata/55e6125ad777b540d9000042/csharp
+
+7 kyu
+Invalid Input - Error Handling #1
+
+Error Handling is very important in coding and seems to be overlooked or not implemented properly.
+
+#Task
+Your task is to implement a function which takes a string as input and return an object containing the properties vowels and consonants. 
+The vowels property must contain the total count of vowels {a,e,i,o,u}, and the total count of consonants {a,..,z} - {a,e,i,o,u}. 
+Handle invalid input and don't forget to return valid ones.
+
+#Input
+The input is any random string. 
+You must then discern what are vowels and what are consonants and sum for each category their total occurrences in an object. 
+However you could also receive inputs that are not strings. 
+If this happens then you must return an object with a vowels and consonants total of 0 because the input was NOT a string. 
+Refer to the Example section for a more visual representation of which inputs you could receive and the outputs expected. :)
+
+Example:
+C#
+A Counter class has been put in the preloaded section taking two parameters Vowels and Consonants this must be the Object you return!
+
+public class Counter {
+    public int Vowels { get; set; }
+    public int Consonants { get; set; }
+    public Counter(int vowels, int consonants)
+    {
+        Vowels = vowels;
+        Consonants = consonants;
+    }
+}
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class InvalidInputErrorHandling1
+    {
+        public class Counter
+        {
+            public int Vowels { get; set; }
+            public int Consonants { get; set; }
+
+            public Counter(int vowels, int consonants)
+            {
+                Vowels = vowels;
+                Consonants = consonants;
+            }
+        }
+
+        public static Counter GetCount(object word)
+        {
+            var s = (word as string ?? "").ToLower().Where(char.IsLetter).ToArray();
+            //return new Counter(s.Count("aeiou".Contains), s.Count("bcdfghjklmnpqrstvwxyz".Contains));
+            return new Counter(s.Count("aeiou".Contains), s.Count(c => !"aeiou".Contains(c)));
+
+
+            // Counter ret = new Counter(0, 0);
+            // if (word is string)
+            // {
+            //     var str = word.ToString().ToLower().Where(c => char.IsLetter(c));
+            //     ret.Vowels = str.Count(c => "aeiou".Contains(c));
+            //     ret.Consonants = str.Count() - ret.Vowels;
+            // }
+            // return ret;
+
+
+            // if (word is string)
+            //     return new Counter(Regex.Matches(word.ToString().ToLower(), "[aeiou]").Count,
+            //         Regex.Matches(word.ToString().ToLower(), "[b-df-hj-np-t-v-z]").Count);
+            // else return new Counter(0, 0);
+
+
+            // Console.WriteLine("'" + word + "'");
+            // var vowels = word.ToString()?.ToLower().Count(c => "aeiou".Contains(c)) ?? 0;
+            // var consonants = word.ToString()?.ToLower().Count(c => !"aeiou".Contains(c) && char.IsLetter(c)) ?? 0;
+            // return new Counter(vowels, consonants);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/InvalidInputErrorHandling1Tests.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class InvalidInputErrorHandling1Tests
+    {
+        [Test]
+        public static void FixedTest()
+        {
+            InvalidInputErrorHandling1.Counter re = InvalidInputErrorHandling1.GetCount("Test");
+            Assert.AreEqual(1, re.Vowels);
+            Assert.AreEqual(3, re.Consonants);
+
+            re = InvalidInputErrorHandling1.GetCount("Here is some text!");
+            Assert.AreEqual(6, re.Vowels);
+            Assert.AreEqual(8, re.Consonants);
+
+            re = InvalidInputErrorHandling1.GetCount("To be a Codewarrior or not to be");
+            Assert.AreEqual(12, re.Vowels);
+            Assert.AreEqual(13, re.Consonants);
+
+            re = InvalidInputErrorHandling1.GetCount("To Kata or not to Kata");
+            Assert.AreEqual(8, re.Vowels);
+            Assert.AreEqual(9, re.Consonants);
+
+            re = InvalidInputErrorHandling1.GetCount("aeiou");
+            Assert.AreEqual(5, re.Vowels);
+            Assert.AreEqual(0, re.Consonants);
+        }
+
+        private static string[] TestText =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam sed enim non euismod. Aliquam ut turpis at tellus tincidunt aliquam. Nam feugiat urna ac lacus ultrices, eu tempor nulla semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris hendrerit lectus eu neque ornare semper. Donec purus elit, cursus quis maximus a, viverra a lorem. Morbi sollicitudin congue volutpat. Vivamus scelerisque, leo quis luctus mattis, sapien quam gravida leo, in tincidunt erat nisl ac augue. Suspendisse placerat a urna nec porta.Nullam facilisis sagittis blandit. Vivamus fermentum consectetur sodales. In rutrum dapibus feugiat. Aenean iaculis lacinia vulputate. Curabitur neque libero, feugiat sed volutpat nec, commodo at sem. Quisque quis mattis ipsum, lacinia aliquet est. Nulla efficitur purus convallis finibus egestas. Curabitur ullamcorper leo ornare velit viverra, mollis vehicula metus vehicula. Praesent id aliquet lacus, eget sagittis sem. Morbi pretium tellus fermentum, mattis nisl ut, sodales sapien.Maecenas sed sollicitudin nunc. Nunc id nunc tellus. Donec ultrices neque nunc, a imperdiet nunc porta at. In id elit convallis, cursus sapien eu, pharetra arcu. Etiam rutrum, neque sit amet porta ultrices, odio risus aliquet odio, ac tempus justo mi ultricies justo. Donec facilisis, diam non sollicitudin mollis, arcu velit sodales nibh, egestas tristique est nulla id purus. Donec venenatis cursus mauris, sit amet posuere leo. Nulla consequat dui est, ut pharetra mauris semper nec. Cras blandit lectus eu neque accumsan facilisis. Donec varius feugiat neque, in cursus leo lobortis quis. Nam nec auctor neque, id suscipit."
+                .Split(' ');
+
+        [Test]
+        public static void FixedHappyTest()
+        {
+            InvalidInputErrorHandling1.Counter re = InvalidInputErrorHandling1.GetCount("Test");
+            Assert.AreEqual(1, re.Vowels);
+            Assert.AreEqual(3, re.Consonants);
+
+            re = InvalidInputErrorHandling1.GetCount("Here is some text!");
+            Assert.AreEqual(6, re.Vowels);
+            Assert.AreEqual(8, re.Consonants);
+
+            re = InvalidInputErrorHandling1.GetCount("To be a Codewarrior or not to be");
+            Assert.AreEqual(12, re.Vowels);
+            Assert.AreEqual(13, re.Consonants);
+
+            re = InvalidInputErrorHandling1.GetCount("To Kata or not to Kata");
+            Assert.AreEqual(8, re.Vowels);
+            Assert.AreEqual(9, re.Consonants);
+
+            re = InvalidInputErrorHandling1.GetCount("aeiou");
+            Assert.AreEqual(5, re.Vowels);
+            Assert.AreEqual(0, re.Consonants);
+        }
+
+        [Test]
+        public static void FixedUnappyTest()
+        {
+            InvalidInputErrorHandling1.Counter re = InvalidInputErrorHandling1.GetCount("Test");
+            Assert.AreEqual(1, re.Vowels);
+            Assert.AreEqual(3, re.Consonants);
+
+            re = InvalidInputErrorHandling1.GetCount("HEre Is sOme text   ");
+            Assert.AreEqual(6, re.Vowels);
+            Assert.AreEqual(8, re.Consonants);
+
+            re = InvalidInputErrorHandling1.GetCount('k');
+            Assert.AreEqual(0, re.Vowels);
+            Assert.AreEqual(0, re.Consonants);
+
+            re = InvalidInputErrorHandling1.GetCount(67457468);
+            Assert.AreEqual(0, re.Vowels);
+            Assert.AreEqual(0, re.Consonants);
+
+            re = InvalidInputErrorHandling1.GetCount(true);
+            Assert.AreEqual(0, re.Vowels);
+            Assert.AreEqual(0, re.Consonants);
+        }
+
+        [Test]
+        public static void RandomTests()
+        {
+            Random rand = new Random();
+
+            object[] invalid = new object[] {"HEre Is sOme text   ", 'k', 563, true};
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (rand.Next(1, 100) < 50)
+                {
+                    int textCount = rand.Next(1, TestText.Length);
+                    string text = "";
+                    for (int x = 0; x < textCount; x++)
+                        text += TestText[x].Trim().ToLower();
+
+
+                    InvalidInputErrorHandling1.Counter re = InvalidInputErrorHandling1.GetCount(text);
+                    InvalidInputErrorHandling1.Counter ans = solve(text);
+                    Assert.AreEqual(ans.Vowels, re.Vowels);
+                    Assert.AreEqual(ans.Consonants, re.Consonants);
+                }
+                else
+                {
+                    object input = invalid[rand.Next(0, invalid.Length)];
+
+                    InvalidInputErrorHandling1.Counter re = InvalidInputErrorHandling1.GetCount(input);
+                    InvalidInputErrorHandling1.Counter ans = solve(input);
+                    Assert.AreEqual(ans.Vowels, re.Vowels);
+                    Assert.AreEqual(ans.Consonants, re.Consonants);
+                }
+            }
+        }
+
+        private static InvalidInputErrorHandling1.Counter solve(object word)
+        {
+            if (word.GetType() != typeof(String))
+                return new InvalidInputErrorHandling1.Counter(0, 0);
+
+            string myWord = ((String) word).Trim().ToLower();
+
+            char[] vowels = new char[] {'a', 'e', 'i', 'o', 'u'};
+
+            string pattern = @"[^\w]";
+            string replacement = "";
+            Regex rgx = new Regex(pattern);
+            myWord = rgx.Replace(myWord, replacement);
+
+            return new InvalidInputErrorHandling1.Counter(
+                myWord.ToCharArray().Where(l => Array.IndexOf(vowels, l) != -1).ToArray().Length,
+                myWord.ToCharArray().Where(l => Array.IndexOf(vowels, l) == -1).ToArray().Length
+            );
+        }
+    }
+}
