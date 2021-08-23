9be00ab2db4ac0a88512d09ff950e8c36ae62e4d
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-08-23 10:33:54 +0300

    Added solution "Knight vs Bishop" 7 kyu

---
+++ CodeWars/7kyu/KnightVsBishop.cs
@@ -0,0 +1,34 @@
+﻿/*
+https://www.codewars.com/kata/564e21ba7cd824845b000097/train/csharp
+
+7 kyu
+Knight vs Bishop
+
+Knight vs Bishop
+If you are not familiar with chess game you can learn more here.
+
+You will be provided with two object arrays. 
+Each of them contains an integer and a string which represent the positions of the pieces on the chess board (e.g. [4, "C"] and [6, "D"]).
+
+Implement a function which returns:
+
+"Knight" if the knight can capture the bishop
+"Bishop" if the bishop can capture the knight
+"None" if both pieces are safe
+Check the test cases and Happy coding :)
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class KataKnightVsBishop
+    {
+        public static string KnightVsBishop(object[] knightPosition, object[] bishopPosition)
+        {
+            var x = Math.Abs((int) knightPosition[0] - (int) bishopPosition[0]);
+            var y = Math.Abs(((string) knightPosition[1])[0] - ((string) bishopPosition[1])[0]);
+            return x == y ? "Bishop" : x + y == 3 && x != 0 && y != 0 ? "Knight" : "None";
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/KnightVsBishopTests.cs
@@ -0,0 +1,36 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataKnightVsBishopTests
+    {
+        [Test]
+        public void KnightTest()
+        {
+            object[] bishopPosition = {4, "C"};
+            object[] knightPosition = {6, "D"};
+            StringAssert.AreEqualIgnoringCase("Knight",
+                KataKnightVsBishop.KnightVsBishop(knightPosition, bishopPosition));
+        }
+
+        [Test]
+        public void BishopTest()
+        {
+            object[] bishopPosition = {2, "G"};
+            object[] knightPosition = {6, "C"};
+            StringAssert.AreEqualIgnoringCase("Bishop",
+                KataKnightVsBishop.KnightVsBishop(knightPosition, bishopPosition));
+        }
+
+        [Test]
+        public void NoneTest()
+        {
+            object[] bishopPosition = {2, "F"};
+            object[] knightPosition = {7, "B"};
+            StringAssert.AreEqualIgnoringCase("None",
+                KataKnightVsBishop.KnightVsBishop(knightPosition, bishopPosition));
+        }
+    }
+}
