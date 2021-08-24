d1ab21f0136770ae354190aaa9d2f2b53293a81b
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-08-24 09:24:12 +0300

    Added solution "Largest Square Inside A Circle" 7 kyu

---
+++ CodeWars/7kyu/LargestSquareInsideCircle.cs
@@ -0,0 +1,30 @@
+﻿/*
+https://www.codewars.com/kata/5887a6fe0cfe64850800161c/csharp
+
+7 kyu
+Largest Square Inside A Circle
+
+Determine the area of the largest square that can fit inside a circle with radius r.
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class LargestSquareInsideCircle
+    {
+        public class Circle
+        {
+            public double AreaLargestSquare(int r)
+            {
+                return Math.Pow(2.0 * r, 2) / 2.0;
+            }
+        }
+
+        public double AreaLargestSquare(int r)
+        {
+            return r * r * 2;
+            //return Math.Pow(r, 2) * 2;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/LargestSquareInsideCircleTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class LargestSquareInsideCircleTests
+    {
+        [Test]
+        public void AreaLargestSquareTest()
+        {
+            LargestSquareInsideCircle.Circle source = new LargestSquareInsideCircle.Circle();
+
+            Assert.AreEqual(50, source.AreaLargestSquare(5));
+            Assert.AreEqual(98, source.AreaLargestSquare(7));
+            Assert.AreEqual(450, source.AreaLargestSquare(15));
+        }
+    }
+}
