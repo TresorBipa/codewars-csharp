8c1fdb1ad9e51e9d0052808d71cf3c79a549b5a5
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-08-29 12:25:28 +0300

    Added solution "MOD 256 without the MOD operator" 7 kyu

---
+++ CodeWarsTests/7kyu/MOD256WithoutMODOperatorTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class MOD256WithoutMODOperatorTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(254, MOD256WithoutMODOperator.Mod256WithoutMod(254));
+            Assert.AreEqual(0, MOD256WithoutMODOperator.Mod256WithoutMod(256));
+            Assert.AreEqual(2, MOD256WithoutMODOperator.Mod256WithoutMod(258));
+
+            Assert.AreEqual(-254, MOD256WithoutMODOperator.Mod256WithoutMod(-254));
+            Assert.AreEqual(0, MOD256WithoutMODOperator.Mod256WithoutMod(-256));
+            Assert.AreEqual(-2, MOD256WithoutMODOperator.Mod256WithoutMod(-258));
+        }
+    }
+}
