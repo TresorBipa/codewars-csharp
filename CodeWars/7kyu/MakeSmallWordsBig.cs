354411f45f950a05d0c05bd725b91f049423d84d
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-01 09:08:36 +0300

    Added solution "Make the small words big!" 7 kyu

---
+++ CodeWars/7kyu/MakeSmallWordsBig.cs
@@ -0,0 +1,41 @@
+﻿/*
+https://www.codewars.com/kata/57b4dd38d2a31c75f7000299/csharp
+
+7 kyu
+Make the small words big!
+
+Life isn't always easy as a small word amongst big words. If only they had a code warrior to help them out...
+
+Your task is to make all words which are 3 characters or less into capitals. 
+You should also remove any vowels from 'long' (4 characters or more) words.
+
+For example:
+"The quick brown fox jumps over the lazy dog"
+
+Should become:
+"THE qck brwn FOX jmps vr THE lzy DOG"
+
+For the purposes of this kata, mid-word punctuation counts towards the character limit of a word.
+
+e.g: "it's / I'm" should become: "t's / I'M"
+
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class MakeSmallWordsBig
+    {
+        public static string SmallWordHelper(string sentence)
+        {
+            //return string.Join(" ", sentence.Split().Select(w => w.Length <= 3 ? w.ToUpper() : string.Concat(w.Where(c => !"aeouiAEIOU".Contains(c)))));
+
+            //return string.Join(" ", sentence.Split().Select(x => x.Length < 4 ? x.ToUpper() : Regex.Replace(x, "[aeiouAEIOU]", "")));
+
+            return string.Join(" ",
+                sentence.Split().Select(x => x.Length < 4 ? x.ToUpper() : Regex.Replace(x, "(?i)[aeiou]", "")));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/MakeSmallWordsBigTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class MakeSmallWordsBigTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("THE qck brwn FOX jmps vr THE lzy DOG",
+                MakeSmallWordsBig.SmallWordHelper("The quick brown fox jumps over the lazy dog"));
+
+            Assert.AreEqual("I'M jst A smll wrd frm A smll wrd fmly",
+                MakeSmallWordsBig.SmallWordHelper("I'm just a small word from a small word family"));
+        }
+    }
+}
