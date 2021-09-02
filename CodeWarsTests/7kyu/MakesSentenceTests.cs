082018905db456824e9c737b9de0346cc67b1df8
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-02 15:04:33 +0300

    Added solution "Makes the Sentence" 7 kyu

---
+++ CodeWarsTests/7kyu/MakesSentenceTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class MakesSentenceTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(
+                MakesSentence.MakesTheSentence(new List<char> {'D', 'u', 'c', 'k', 's', 'q', 'u', 'a', 'c', 'k', '.'},
+                    "Ducks quack."), Is.EqualTo(true));
+
+            Assert.That(MakesSentence.MakesTheSentence(new List<char> {'S', 'h', 'e', 'a', 'd', 's', '.'}, "She adds."),
+                Is.EqualTo(false));
+        }
+    }
+}
