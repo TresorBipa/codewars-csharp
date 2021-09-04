f5264604b9c1da62465b7ac71e14511962a7e275
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-04 12:52:38 +0300

    Added solution "Mean vs. Median" 7 kyu

---
+++ CodeWars/7kyu/MeanVsMedian.cs
@@ -0,0 +1,42 @@
+﻿/*
+https://www.codewars.com/kata/5806445c3f1f9c2f72000031/csharp
+
+7 kyu
+Mean vs. Median
+
+Your goal is to implement the method meanVsMedian which accepts an odd-length array of integers and returns one of the following:
+
+'mean' - in case mean value is larger than median value
+'median' - in case median value is larger than mean value
+'same' - in case both mean and median share the same value
+Reminder: Median
+
+Array will always be valid (odd-length >= 3)
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class KataMeanVsMedian
+    {
+        public static string MeanVsMedian(int[] numbers)
+        {
+            return numbers.Average().CompareTo(numbers.OrderBy(x => x).ToArray()[numbers.Length / 2]) switch
+            {
+                1 => "mean",
+                -1 => "median",
+                _ => "same"
+            };
+
+            // double mean = numbers.Average();
+            // //Array.Sort(numbers);
+            // var median = numbers.OrderBy(x => x).ToArray()[numbers.Length / 2];
+            //
+            // return mean == median ? "same"
+            //     : mean < median ? "median"
+            //     : "mean";
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/MeanVsMedianTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class MeanVsMedianTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("same", KataMeanVsMedian.MeanVsMedian(new int[] {1, 1, 1}));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual("mean", KataMeanVsMedian.MeanVsMedian(new int[] {1, 2, 37}));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual("median", KataMeanVsMedian.MeanVsMedian(new int[] {7, 14, -70}));
+        }
+    }
+}
