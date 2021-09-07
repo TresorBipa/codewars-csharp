9bcf53c960487e41081311c328fc5e73d78564f1
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-07 10:45:25 +0300

    Added solution "Monotone travel" 7 kyu

---
+++ CodeWars/7kyu/MonotoneTravel.cs
@@ -0,0 +1,44 @@
+﻿/*
+https://www.codewars.com/kata/54466996990c921f90000d61/csharp
+
+7 kyu
+Monotone travel
+
+Story
+Peter lives on a hill, and he always moans about the way to his home. "It's always just up. I never get a rest". 
+But you're pretty sure that at least at one point Peter's altitude doesn't rise, but fall. 
+To get him, you use a nefarious plan: you attach an altimeter to his backpack and you read the data from his way back at the next day.
+
+Task
+You're given a list of compareable elements:
+
+heights = new int[0 ... 100]
+Your job is to check whether for any x all successors are greater or equal to x.
+
+Kata.IsMonotone(new int[] {1,2,3}) => true
+Kata.IsMonotone(new int[] {1,1,2}) => true
+Kata.IsMonotone(new int[] {1})     => true
+Kata.IsMonotone(new int[] {3,2,1}) => false
+Kata.IsMonotone(new int[] {3,2,2}) => false
+If the list is empty, Peter has probably removed your altimeter, so we cannot prove him wrong and he's still right:
+
+Kata.IsMonotone(new int[] {}) => true
+Such a sequence is also called monotone or monotonic sequence, hence the name isMonotone.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class MonotoneTravel
+    {
+        public static bool IsMonotone(int[] arr)
+        {
+            //return arr.Skip(1).Select((x, i) => x >= arr[i]).All(x => x);
+            //return !arr.Where((x, i) => i != 0 && arr[i - 1] > x).Any();
+            //return arr.SequenceEqual(arr.OrderBy(i => i));
+            return arr.OrderBy(i => i).SequenceEqual(arr);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/MonotoneTravelTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class MonotoneTravelTests
+    {
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test, Description("should work on increasing lists")]
+            public void IncreasingTest()
+            {
+                Assert.AreEqual(true, MonotoneTravel.IsMonotone(Enumerable.Range(1, 10).ToArray()));
+                Assert.AreEqual(true, MonotoneTravel.IsMonotone(Enumerable.Range(4, 9).ToArray()));
+            }
+
+            [Test, Description("should work on constant lists")]
+            public void ConstantTest()
+            {
+                Assert.AreEqual(true, MonotoneTravel.IsMonotone(new int[] {5, 5, 5, 5, 5}));
+            }
+
+            [Test, Description("should work on an empty list")]
+            public void EmptyTest()
+            {
+                Assert.AreEqual(true, MonotoneTravel.IsMonotone(new int[] { }));
+            }
+
+            [Test, Description("should return false on a decreasing list")]
+            public void DecreasingTest()
+            {
+                Assert.AreEqual(false, MonotoneTravel.IsMonotone(Enumerable.Range(1, 5).Reverse().ToArray()));
+            }
+
+            [Test, Description("should work on a non-decreasing list")]
+            public void NonDecreasingTest()
+            {
+                Assert.AreEqual(true, MonotoneTravel.IsMonotone(new int[] {1, 2, 3, 3, 4, 5}));
+            }
+        }
+    }
+}
