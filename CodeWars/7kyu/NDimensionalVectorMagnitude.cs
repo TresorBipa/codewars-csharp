b2206b0ceb15c59b3c58f20c81c1abd09760955b
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-13 09:44:54 +0300

    Added solution "N-Dimensional Vector Magnitude" 7 kyu

---
+++ CodeWars/7kyu/NDimensionalVectorMagnitude.cs
@@ -0,0 +1,29 @@
+﻿/*
+https://www.codewars.com/kata/5806c2f897dba05dd900004c/csharp
+
+7 kyu
+N-Dimensional Vector Magnitude
+
+Return the magnitude of a vector, given as an array of its component values.
+
+The vector (2, 3, 5) would be represented as an array containing three values at indices 0, 1 and 2 respectively.
+The array will always contain at least 2 and at most 100 elements (2 ≤ vector.Length ≤ 100), each of which will be within the range [-100, 100].
+An array containing four elements represents a vector in 4D space.
+
+More info on finding the length of a vector: http://farside.ph.utexas.edu/teaching/301/lectures/node28.html
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class NDimensionalVectorMagnitude
+    {
+        public static double Magnitude(double[] vector)
+        {
+            return Math.Sqrt(vector.Sum(x => x * x));
+            //return vector.Aggregate(0d, (a, x) => a + x * x, Math.Sqrt);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/NDimensionalVectorMagnitudeTests.cs
@@ -0,0 +1,30 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class NDimensionalVectorMagnitudeTests
+    {
+        [Test]
+        public void ZeroVector()
+        {
+            double[] vector = new[] {0d, 0d};
+            Assert.AreEqual(0d, NDimensionalVectorMagnitude.Magnitude(vector), 1e-10);
+        }
+
+        [Test]
+        public void UnitVector()
+        {
+            double[] vector = new[] {2d / 3d, 1d / 3d, 2d / 3d};
+            Assert.AreEqual(1d, NDimensionalVectorMagnitude.Magnitude(vector), 1e-10);
+        }
+
+        [Test]
+        public void NegativeValues()
+        {
+            double[] vector = new[] {-2d, -4d, 4d};
+            Assert.AreEqual(6d, NDimensionalVectorMagnitude.Magnitude(vector), 1e-10);
+        }
+    }
+}
