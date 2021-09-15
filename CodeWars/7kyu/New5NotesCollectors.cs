a315d97336d9798132ce53169e794c3c88aee604
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-15 11:07:49 +0300

    Added solution "New £5 notes collectors!" 7 kyu

---
+++ CodeWars/7kyu/New5NotesCollectors.cs
@@ -0,0 +1,42 @@
+﻿/*
+https://www.codewars.com/kata/58029cc9af749f80e3001e34/csharp
+
+7 kyu
+New £5 notes collectors!
+
+The new £5 notes have been recently released in the UK and they've certainly became a sensation! 
+Even those of us who haven't been carrying any cash around for a while, 
+having given in to the convenience of cards, suddenly like to have some of these in their purses and pockets. 
+But how many of them could you get with what's left from your salary after paying all bills? 
+The programme that you're about to write will count this for you!
+
+Given a salary and the array of bills, 
+calculate your disposable income for a month and return it as a number of new £5 notes you can get with that amount. 
+If the money you've got (or do not!) doesn't allow you to get any £5 notes return 0.
+
+£££ GOOD LUCK! £££
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class New5NotesCollectors
+    {
+        public static int GetNewNotes(int salary, int[] bills)
+        {
+            //return salary - bills.Sum() >= 0 ? (salary - bills.Sum()) / 5 : 0;
+            
+            //return Math.Max((salary - bills.Sum()) / 5, 0);
+
+            // int amount = salary - bills.Sum();
+            // return amount > 0 ? amount / 5 : 0;
+
+            // salary -= bills.Sum();
+            // return salary < 5 ? 0 : salary / 5;
+            
+            return (salary -= bills.Sum()) < 5 ? 0 : salary / 5;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/New5NotesCollectorsTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class New5NotesCollectorsTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(188, New5NotesCollectors.GetNewNotes(2000, new int[] {500, 160, 400}));
+            Assert.AreEqual(122, New5NotesCollectors.GetNewNotes(1260, new int[] {500, 50, 100}));
+            Assert.AreEqual(95, New5NotesCollectors.GetNewNotes(3600, new int[] {1800, 350, 460, 500, 15}));
+            Assert.AreEqual(86, New5NotesCollectors.GetNewNotes(1995, new int[] {1500, 19, 44}));
+        }
+    }
+}
