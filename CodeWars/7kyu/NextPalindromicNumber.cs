33b906301899ac4c1453f1d3bcaef9b6cfcde5bd
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-16 09:59:33 +0300

    Added solution "Next Palindromic Number" 7 kyu

---
+++ CodeWars/7kyu/NextPalindromicNumber.cs
@@ -0,0 +1,69 @@
+﻿/*
+https://www.codewars.com/kata/56a6ce697c05fb4667000029/csharp
+
+7 kyu
+Next Palindromic Number.
+
+There were and still are many problem in CW about palindrome numbers and palindrome strings. 
+We suposse that you know which kind of numbers they are. If not, 
+you may search about them using your favourite search engine.
+
+In this kata you will be given a positive integer, 
+val and you have to create the function next_pal()(nextPal Javascript) that will output the smallest palindrome number higher than val.
+
+Let's see:
+
+For C#
+Kata.NextPal(11) == 22
+
+Kata.NextPal(188) == 191
+
+Kata.NextPal(191) == 202
+
+Kata.NextPal(2541) == 2552
+You will be receiving values higher than 10, all valid.
+
+Enjoy it!!
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class NextPalindromicNumber
+    {
+        public static int NextPal(int val)
+        {
+            // do
+            // {
+            //     val++;
+            // } while (val.ToString() != string.Concat(val.ToString().Reverse()));
+            //
+            // return val;
+
+
+            // while (true)
+            // {
+            //     val += 1;
+            //     if (string.Join("", val.ToString().ToCharArray().Reverse()) == val.ToString())
+            //     {
+            //         return val;
+            //     }
+            // }
+
+            // val++;
+            // while (!$"{val}".SequenceEqual($"{val}".Reverse()))
+            //     val++;
+            // return val;
+
+
+            // while ((++val).ToString() != new string(val.ToString().Reverse().ToArray()))
+            // {
+            // }
+            // return val;
+
+            //return Enumerable.Range(val + 1, val).First(x => x.ToString() == new string(x.ToString().Reverse().ToArray()));
+            return Enumerable.Range(val + 1, val).First(x => $"{x}" == string.Concat($"{x}".Reverse()));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/NextPalindromicNumberTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class NextPalindromicNumberTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(22, NextPalindromicNumber.NextPal(11));
+            Assert.AreEqual(191, NextPalindromicNumber.NextPal(188));
+            Assert.AreEqual(202, NextPalindromicNumber.NextPal(191));
+            Assert.AreEqual(2552, NextPalindromicNumber.NextPal(2541));
+        }
+    }
+}
