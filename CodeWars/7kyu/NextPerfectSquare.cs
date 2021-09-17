4a9412b7fadb04f0a68fd6208042445c5871c03e
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-17 20:16:57 +0300

    Added solution "nextPerfectSquare" 7 kyu

---
+++ CodeWars/7kyu/NextPerfectSquare.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/599f403119afacf9f1000051/csharp
+
+7 kyu
+nextPerfectSquare
+
+Write a function name nextPerfectSquare / next_perfect_square that returns the first perfect square that is greater than its integer argument. 
+A perfect square is a integer that is equal to some integer squared. For example 16 is a perfect square because 16=4*4.
+
+example
+
+n   next perfect square
+
+6    9
+36   49 
+0    1
+-5   0 
+Caution! The largest number test is close to Int64.MaxValue
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class KataNextPerfectSquare
+    {
+        public static long NextPerfectSquare(long n)
+        {
+            //return n >= 0 ? (long) Math.Pow(Math.Floor(Math.Sqrt(n) + 1), 2) : 0;
+            return n < 0 ? 0 : (long) Math.Pow((long) Math.Sqrt(n) + 1, 2);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/NextPerfectSquareTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataNextPerfectSquareTests
+    {
+        [TestFixture]
+        public class Sample_Tests
+        {
+            private static IEnumerable<TestCaseData> testCases
+            {
+                get
+                {
+                    yield return new TestCaseData(6).Returns(9);
+                    yield return new TestCaseData(36).Returns(49);
+                    yield return new TestCaseData(0).Returns(1);
+                    yield return new TestCaseData(-5).Returns(0);
+                }
+            }
+
+            [Test, TestCaseSource("testCases")]
+            public long SampleTest(long n) => KataNextPerfectSquare.NextPerfectSquare(n);
+        }
+    }
+}
