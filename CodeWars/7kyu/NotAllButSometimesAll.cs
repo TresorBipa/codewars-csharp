0df62dbc8b36f3d2cf86ea06cef7aaed08444b62
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-18 12:10:09 +0300

    Added solution "Not all but sometimes all" 7 kyu

---
+++ CodeWars/7kyu/NotAllButSometimesAll.cs
@@ -0,0 +1,46 @@
+﻿/*
+https://www.codewars.com/kata/564ab935de55a747d7000040/csharp
+
+7 kyu
+Not all but sometimes all
+
+Write
+
+public static string Remove(string str, Dictionary<char,int> what)
+that takes in a string str(text in Python) and an object/hash/dict/Dictionary what and returns a string with the chars removed in what. For example:
+
+Kata.Remove("this is a string", new Dictionary<char,int> { {'t', 1 }, {'i', 2 }}); // --> "hs s a string"
+// remove from 'this is a string' the first 1 't' and the first 2 i's.
+Kata.Remove("hello world", new Dictionary<char,int> { { 'x',5 }, {'i',2 }}); // --> "hello world"
+// there are no x's or i's, so nothing gets removed
+Kata.Remove("apples and bananas", new Dictionary<char,int> { {'a', 50 }, {'n', 1 }}); // --> "pples d bnns"
+// we don't have 50 a's, so just remove it till we hit end of string.
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class NotAllButSometimesAll
+    {
+        public static string Remove(string str, Dictionary<char, int> what)
+        {
+            return new string(str.Where(c => !what.ContainsKey(c) || what[c]-- < 1).ToArray());
+            // return string.Concat(str.Where(c => !what.ContainsKey(c) || what[c]-- < 1).ToArray());
+            // return str.Where(c => !what.ContainsKey(c) || what[c]-- < 1).Aggregate("", (s, c) => s + c);
+
+
+            // var result = "";
+            // foreach (var c in str)
+            // {
+            //     if (!what.ContainsKey(c) || what[c]-- < 1)
+            //     {
+            //         result += c;
+            //     }
+            // }
+            //
+            // return result;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/NotAllButSometimesAllTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class NotAllButSometimesAllTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("hs s a string",
+                NotAllButSometimesAll.Remove("this is a string", new Dictionary<char, int> {{'t', 1}, {'i', 2}}));
+
+            Assert.AreEqual("hello world",
+                NotAllButSometimesAll.Remove("hello world", new Dictionary<char, int> {{'x', 5}, {'i', 2}}));
+
+            Assert.AreEqual("pples d bnns",
+                NotAllButSometimesAll.Remove("apples and bananas", new Dictionary<char, int> {{'a', 50}, {'n', 1}}));
+
+            Assert.AreEqual("", NotAllButSometimesAll.Remove("a", new Dictionary<char, int> {{'a', 1}, {'n', 1}}));
+
+            Assert.AreEqual("",
+                NotAllButSometimesAll.Remove("codewars",
+                    new Dictionary<char, int>
+                        {{'c', 5}, {'o', 1}, {'d', 1}, {'e', 1}, {'w', 1}, {'z', 1}, {'a', 1}, {'r', 1}, {'s', 1}}));
+        }
+    }
+}
