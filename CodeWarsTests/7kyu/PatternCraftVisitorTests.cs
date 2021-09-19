bb55d208f43b15d5f87ce200aaa86a67348065d8
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-19 12:38:20 +0300

    Added solution "PatternCraft - Visitor" 7 kyu

---
+++ CodeWarsTests/7kyu/PatternCraftVisitorTests.cs
@@ -0,0 +1,31 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PatternCraftVisitorTests
+    {
+        [Test]
+        public void _0_VisitLight()
+        {
+            PatternCraftVisitor.IVisitor bullet = new PatternCraftVisitor.TankBullet();
+            PatternCraftVisitor.ILightUnit light = new PatternCraftVisitor.Marine();
+
+            light.Accept(bullet);
+
+            Assert.AreEqual(100 - 21, light.Health);
+        }
+
+        [Test]
+        public void _1_VisitArmored()
+        {
+            PatternCraftVisitor.IVisitor bullet = new PatternCraftVisitor.TankBullet();
+            PatternCraftVisitor.IArmoredUnit armored = new PatternCraftVisitor.Marauder();
+
+            armored.Accept(bullet);
+
+            Assert.AreEqual(125 - 32, armored.Health);
+        }
+    }
+}
