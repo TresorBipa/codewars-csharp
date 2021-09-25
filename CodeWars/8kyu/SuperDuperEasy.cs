30a28a852cb542ee6d32b87bd0e81970ca675836
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-25 11:07:25 +0300

    Updated solution "Super Duper Easy" 8 kyu

--- CodeWars/8kyu/SuperDuperEasy.cs
+++ CodeWars/8kyu/SuperDuperEasy.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿/*
+https://www.codewars.com/kata/super-duper-easy/csharp
+https://www.codewars.com/kata/55a5bfaa756cfede78000026/csharp
 
-//https://www.codewars.com/kata/super-duper-easy/csharp
+8 kyu
+Super Duper Easy
 
-//Super Duper Easy
-//8 kyu
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
 
-//Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
-//Note: in C#, you'll always get the input as a string, so the above applies if the string isn't representing a double value.
+Note: in C#, you'll always get the input as a string, so the above applies if the string isn't representing a double value.
+*/
+
+using System;
 
 namespace CodeWars
 {
