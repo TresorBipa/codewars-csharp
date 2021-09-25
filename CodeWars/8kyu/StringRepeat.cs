e7d9d36be8299c6761115c61f22551747b0ab066
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-25 11:08:23 +0300

    Updated solution "String repeat" 8 kyu

--- CodeWars/8kyu/StringRepeat.cs
+++ CodeWars/8kyu/StringRepeat.cs
@@ -1,15 +1,20 @@
-﻿using System;
+﻿/*
+https://www.codewars.com/kata/string-repeat/csharp
+https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/csharp
+
+8 kyu
+String repeat
+
+Write a function called repeatStr which repeats the given string string exactly n times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
-//https://www.codewars.com/kata/string-repeat/train/csharp
-//https://www.codewars.com/kata/string-repeat/solutions
-
-//String repeat
-
-//repeatStr(6, "I") // "IIIIII"
-//repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
 
 
 namespace CodeWars
@@ -90,8 +95,5 @@ namespace CodeWars
         //{
         //    return string.Join(s, new string[n + 1]);
         //}
-
-
-
     }
 }
