f6a128392b663b4971ae1b9bba0501f691e4a0c6
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-26 19:30:26 +0300

    Updated solution "Count characters in your string" 6 kyu

--- CodeWars/6kyu/CountCharacters.cs
+++ CodeWars/6kyu/CountCharacters.cs
@@ -1,30 +1,36 @@
-﻿using System;
+﻿/*
+https://www.codewars.com/kata/count-characters-in-your-string/csharp
+https://www.codewars.com/kata/52efefcbcdf57161d4000091/csharp
+
+6 kyu
+Count characters in your string
+
+The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+*/
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CodeWars
 {
-
-    //https://www.codewars.com/kata/count-characters-in-your-string/train/csharp
-    //
-
-    //The main idea is to count all the occuring characters(UTF-8) in string.
-    //If you have string like this aba then the result should be { 'a': 2, 'b': 1 }
-
-    //What if the string is empty? Then the result should be empty object literal { }
-    //For C#: Use a Dictionary<char, int> for this kata!
-
     public class CountCharacters
     {
         public static Dictionary<char, int> Count(string str)
         {
+            // return str.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+
             var result = new Dictionary<char, int>();
 
             foreach (var uniqueChar in str.Distinct())
             {
                 var countChar = 0;
                 foreach (var ch in str)
-                    if (uniqueChar == ch) countChar++;
+                    if (uniqueChar == ch)
+                        countChar++;
                 result.Add(uniqueChar, countChar);
             }
 
