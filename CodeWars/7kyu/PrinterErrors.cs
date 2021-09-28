42c357b7394f92d89cda04776f45be938365e95b
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-09-28 13:35:44 +0300

    Updated solution "Printer Errors" 7 kyu

--- CodeWars/7kyu/PrinterErrors.cs
+++ CodeWars/7kyu/PrinterErrors.cs
@@ -1,30 +1,32 @@
-﻿using System;
+﻿/*
+https://www.codewars.com/kata/printer-errors/csharp
+https://www.codewars.com/kata/56541980fa08ab47a0000040/csharp
+
+7 kyu
+Printer Errors
+
+In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+The string has a length greater or equal to one and contains only letters from ato z.
+
+Examples:
+s="aaabbbbhaijjjm"
+printer_error(s) => "0/14"
+
+s="aaaxbbbbyyhwawiwjjjwwm"
+printer_error(s) => "8/22"
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
-
-//https://www.codewars.com/kata/printer-errors/csharp
-//https://www.codewars.com/kata/printer-errors/solutions/csharp
-
-//Printer Errors
-
-//    In a factory a printer prints labels for boxes.For one kind of boxes the printer has to use colors which, for the sake of simplicity,
-// are named with letters from a to m.
-//    The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm
-// meaning that the printer used three times color a, four times color b, one time color h then one time color a...
-//    Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g.aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
-//    You have to write a function printer_error which given a string will output the error rate of the printer as a string
-// representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
-//    The string has a length greater or equal to one and contains only letters from ato z.
-
-//#Examples:
-//    s= "aaabbbbhaijjjm"
-//    error_printer(s) => "0/14"
-
-//    s= "aaaxbbbbyyhwawiwjjjwwm"
-//    error_printer(s) => "8/22"
-
 
 namespace CodeWars
 {
@@ -40,6 +42,5 @@ namespace CodeWars
         //{
         //    return s.Where(c => c > 'm').Count() + "/" + s.Length;
         //}
-
     }
 }
