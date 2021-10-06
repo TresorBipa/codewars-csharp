2a8e21b8f7be83ad56de5574e32777cddda47ab8
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-10-06 09:48:05 +0300

    Updated solution "Grasshopper - Terminal game move function" 8 kyu

--- CodeWars/8kyu/GrasshopperTerminalGameMove.cs
+++ CodeWars/8kyu/GrasshopperTerminalGameMove.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿/*
+https://www.codewars.com/kata/grasshopper-terminal-game-move-function/csharp
+https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/csharp
 
-//https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/csharp
-//
+8 kyu
+Grasshopper - Terminal game move function
 
-//Grasshopper - Terminal game move function
+Terminal game move function
+In this game, the hero moves from left to right. The player rolls the die and moves the number of spaces indicated by the die two times.
 
-//Terminal game move function
-//In this game, the hero moves from left to right.The player rolls the dice and moves the number of spaces indicated by the dice two times.
-//Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
-//Example:
-//move(3, 6) should equal 15
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
 
+Example:
+move(3, 6) should equal 15
+*/
+
+using System;
 
 namespace CodeWars
 {
