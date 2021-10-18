f1112fef14eec300b61dd4dd72b13b253f67c18c
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-10-18 13:05:46 +0300

    Updated solution "Check the exam" 8 kyu

--- CodeWarsTests/8kyu/CheckExamTests.cs
+++ CodeWarsTests/8kyu/CheckExamTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using CodeWars;
 using NUnit.Framework;
 
@@ -7,29 +8,29 @@ namespace CodeWarsTests
     [TestFixture]
     public class KataCheckExamTests
     {
-        [TestCase(new string[] {"a", "a", "b", "b"}, new string[] {"a", "c", "b", "d"}, 6)]
-        [TestCase(new string[] {"a", "a", "c", "b"}, new string[] {"a", "a", "b", ""}, 7)]
-        [TestCase(new string[] {"a", "a", "b", "c"}, new string[] {"a", "a", "b", "c"}, 16)]
-        [TestCase(new string[] {"b", "c", "b", "a"}, new string[] {"", "a", "a", "c"}, 0)]
-        [TestCase(new string[] {"a", "c", "b", "d"}, new string[] {"", "", "", ""}, 0)]
-        public void BasicTests(string[] arr1, string[] arr2, int score)
+        [TestCase(new string[] { "a", "a", "b", "b" }, new string[] { "a", "c", "b", "d" }, 6)]
+        [TestCase(new string[] { "a", "a", "c", "b" }, new string[] { "a", "a", "b", "" }, 7)]
+        [TestCase(new string[] { "a", "a", "b", "c" }, new string[] { "a", "a", "b", "c" }, 16)]
+        [TestCase(new string[] { "b", "c", "b", "a" }, new string[] { "", "a", "a", "c" }, 0)]
+        [TestCase(new string[] { "a", "c", "b", "d" }, new string[] { "", "", "", "" }, 0)]
+        public void SampleTest(string[] arr1, string[] arr2, int score)
         {
             Assert.AreEqual(score, KataCheckExam.CheckExam(arr1, arr2));
         }
 
-        [TestCase(new string[] {"a", "a", "a", "a", "a"}, new string[] {"a", "a", "a", "a", "a"}, null)]
-        [TestCase(new string[] {"b", "b", "b"}, new string[] {"b", "b", "b"}, null)]
-        public void LengthParamsTests(string[] arr1, string[] arr2, int score)
-        {
-            Assert.AreEqual(score, KataCheckExam.CheckExam(arr1, arr2));
-            Assert.AreEqual(score, KataCheckExam.CheckExam(arr1, arr2));
-        }
+        // [TestCase(new string[] { "a", "a", "a", "a", "a" }, new string[] { "a", "a", "a", "a", "a" }, null)]
+        // [TestCase(new string[] { "b", "b", "b" }, new string[] { "b", "b", "b" }, null)]
+        // public void LengthParamsTests(string[] arr1, string[] arr2, int score)
+        // {
+        //     Assert.AreEqual(score, KataCheckExam.CheckExam(arr1, arr2));
+        //     Assert.AreEqual(score, KataCheckExam.CheckExam(arr1, arr2));
+        // }
 
         [Test]
-        public void RandomTests()
+        public void RandomTests1()
         {
-            var letsAnswers = new string[] {"a", "b", "c", "d"};
-            var letsStudent = new string[] {"a", "b", "c", "d", ""};
+            var letsAnswers = new string[] { "a", "b", "c", "d" };
+            var letsStudent = new string[] { "a", "b", "c", "d", "" };
 
             for (int i = 0; i <= 100; i++)
             {
@@ -43,9 +44,46 @@ namespace CodeWarsTests
                 }
 
                 int expected = Solution(rndAnswers, rndStudent);
+                string message = FailureMessage(rndAnswers, rndStudent, expected);
                 int actual = KataCheckExam.CheckExam(rndAnswers, rndStudent);
-                Assert.AreEqual(expected, actual);
+                // Console.WriteLine(message);
+                Assert.AreEqual(expected, actual, message);
             }
+        }
+
+        [Test]
+        public void RandomTests2()
+        {
+            var letsAnswers = new string[] { "a", "b", "c", "d" };
+            var letsStudent = new string[] { "a", "b", "c", "d", "" };
+
+            for (int i = 0; i <= 100; i++)
+            {
+                int arrLength = Rnd.Next(4, 20);
+                string[] rndAnswers = new string[arrLength];
+                string[] rndStudent = new string[arrLength];
+
+                for (int j = 0; j < arrLength; j++)
+                {
+                    rndAnswers[j] = letsAnswers[Rnd.Next(0, letsAnswers.Length)];
+                    rndStudent[j] = letsStudent[Rnd.Next(0, letsStudent.Length)];
+                }
+
+                int expected = Solution(rndAnswers, rndStudent);
+                string message = FailureMessage(rndAnswers, rndStudent, expected);
+                int actual = KataCheckExam.CheckExam(rndAnswers, rndStudent);
+                // Console.WriteLine(message);
+
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        private static string FailureMessage(string[] answers, string[] student, int value)
+        {
+            var joinAnswers = string.Join(", ", answers.Select(x => $"\"{x}\""));
+            var joinStudent = string.Join(", ", student.Select(x => $"\"{x}\""));
+            var result = $"Should return {value} with [{joinAnswers}] and [{joinStudent}]";
+            return result;
         }
 
         private static readonly Random Rnd = new Random();
