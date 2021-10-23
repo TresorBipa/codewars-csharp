cf56f576eda02ede5e5172464679b25873c66b06
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-10-23 10:00:03 +0300

    Added solution "Multiply" 7 kyu

---
+++ CodeWars/8kyu/Multiply.cs
@@ -0,0 +1,24 @@
+﻿/*
+https://www.codewars.com/kata/50654ddff44f800200000004/csharp
+
+8 kyu
+Multiply
+
+This code does not execute properly. Try to figure out why.
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public class KataMultiply
+    {
+        public static int multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ CodeWarsTests/8kyu/MultiplyTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataMultiplyTests
+    {
+        [Test]
+        public void ShouldMultiple()
+        {
+            Assert.AreEqual(2, KataMultiply.multiply(2, 1));
+            Assert.AreEqual(8, KataMultiply.multiply(2, 4));
+        }
+    }
+}
