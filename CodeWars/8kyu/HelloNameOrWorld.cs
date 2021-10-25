b1d2dcfe544492e54c2ee41afc79bb7e787fd069
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-10-25 09:39:47 +0300

    Added solution "Hello, Name or World!" 7 kyu

---
+++ CodeWars/8kyu/HelloNameOrWorld.cs
@@ -0,0 +1,47 @@
+﻿/*
+https://www.codewars.com/kata/57e3f79c9cb119374600046b/csharp
+
+8 kyu
+Hello, Name or World!
+
+Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+Examples:
+hello "john"   => "Hello, John!"
+hello "aliCE"  => "Hello, Alice!"
+hello          => "Hello, World!" # name not given
+hello ""       => "Hello, World!" # name is an empty String
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace CodeWars
+{
+    public static class HelloNameOrWorld
+    {
+        public static string Hello(string name = "")
+        {
+            if (name == "") return "Hello, World!";
+
+            return "Hello, " + name.Remove(1).ToUpper() + name.ToLower().Substring(1) + "!";
+
+
+            // return string.IsNullOrEmpty(name)
+            //     ? "Hello, World!"
+            //     : $"Hello, {name[..1].ToUpper()}{name[1..].ToLower()}!";
+
+            // return string.IsNullOrEmpty(name)
+            //     ? "Hello, World!"
+            //     : $"Hello, {CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name.ToLower())}!";
+
+            // return string.IsNullOrEmpty(name)
+            //     ? "Hello, World!"
+            //     : $"Hello, {string.Concat(name.Select((c, i) => i == 0 ? char.ToUpper(c) : char.ToLower(c)))}!";
+        }
+    }
+}
---
+++ CodeWarsTests/8kyu/HelloNameOrWorldTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class HelloNameOrWorldTests
+    {
+        private static readonly Random Rand = new Random();
+
+        private static string Solution(string name = "")
+        {
+            return string.IsNullOrEmpty(name)
+                ? "Hello, World!"
+                : $"Hello, {name.ToUpper()[0] + name[1..].ToLower()}!";
+        }
+
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual("Hello, Jeff!", HelloNameOrWorld.Hello("jEFF"));
+            Assert.AreEqual("Hello, Tony!", HelloNameOrWorld.Hello("tonY"));
+            Assert.AreEqual("Hello, Alicia!", HelloNameOrWorld.Hello("Alicia"));
+            Assert.AreEqual("Hello, World!", HelloNameOrWorld.Hello(""));
+            Assert.AreEqual("Hello, John!", HelloNameOrWorld.Hello("JOHN"));
+            Assert.AreEqual("Hello, Vasya!", HelloNameOrWorld.Hello("vasya"));
+            Assert.AreEqual("Hello, World!", HelloNameOrWorld.Hello());
+        }
+
+        [Test]
+        public void RandomTests1()
+        {
+            string[] names =
+            {
+                "James", "Christopher", "Ronald", "Mary", "Lisa", "Michelle",
+                "John", "Daniel", "Anthony", "Patricia", "Nancy", "Laura",
+                "Robert", "Paul", "Kevin", "Linda", "Karen", "Sarah", "Michael",
+                "Mark", "Jason", "Barbara", "Betty", "Kimberly", "William", "Donald",
+                "Jeff", "Elizabeth", "Helen", "Deborah", "David", "George", "Jennifer",
+                "Sandra", "Richard", "Kenneth", "Maria", "Donna", "Charles", "Steven",
+                "Susan", "Carol", "Joseph", "Edward", "Margaret", "Ruth", "Thomas",
+                "Brian", "Dorothy", "Sharon", "Vasya", ""
+            };
+
+            for (var i = 0; i < 10000; i++)
+            {
+                var randomName = names[Rand.Next(0, names.Length)];
+                var expected = Solution(randomName);
+                var message = FailureMessage(randomName, expected);
+                var actual = HelloNameOrWorld.Hello(randomName);
+
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        private static string RandomName()
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return string.Concat(Enumerable.Repeat(chars, Rand.Next(2, chars.Length))
+                .Select(s => s[Rand.Next(s.Length)]));
+        }
+
+        [Test]
+        public void RandomTests2()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                var randomName = RandomName();
+                // Console.WriteLine(randomName);
+                var expected = Solution(randomName);
+                var message = FailureMessage(randomName, expected);
+                var actual = HelloNameOrWorld.Hello(randomName);
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        private static string FailureMessage(string card, string value)
+        {
+            return $"Should return {value} with \"{card}\"";
+        }
+    }
+}
