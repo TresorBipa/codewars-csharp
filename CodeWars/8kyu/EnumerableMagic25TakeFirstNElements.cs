3ecc2aaa8b3d4c84409d5b9694d1a79443533c79
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-10-26 09:13:31 +0300

    Added solution "Enumerable Magic #25 - Take the First N Elements" 7 kyu

---
+++ CodeWars/8kyu/EnumerableMagic25TakeFirstNElements.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/545afd0761aa4c3055001386
+
+8 kyu
+Enumerable Magic #25 - Take the First N Elements
+
+Create a method take that accepts a list/array and a number n, 
+and returns a list/array array of the first n elements from the list/array.
+
+If you need help, here's a reference:
+
+https://docs.python.org/3/library/stdtypes.html#sequence-types-list-tuple-range
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace CodeWarsTests
+{
+    public class EnumerableMagic25TakeFirstNElements
+    {
+        public static int[] Take(int[] arr, int n)
+        {
+            return arr.Take(n).ToArray();
+            // return arr[..Math.Min(n, arr.Length)];
+            // return new ArraySegment<int>(arr, 0, Math.Min(n, arr.Length)).ToArray();
+            // return new Span<int>(arr, 0, Math.Min(n, arr.Length)).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/8kyu/EnumerableMagic25TakeFirstNElementsTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class EnumerableMagic25TakeFirstNElementsTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new int[] { 0, 1, 2 },
+                EnumerableMagic25TakeFirstNElements.Take(new int[] { 0, 1, 2, 3, 5, 8, 13 }, 3));
+
+            Assert.AreEqual(new int[] { },
+                EnumerableMagic25TakeFirstNElements.Take(new int[] { 0, 1, 2, 3, 5, 8, 13 }, 0));
+            Assert.AreEqual(new int[] { }, EnumerableMagic25TakeFirstNElements.Take(new int[] { }, 3));
+        }
+
+
+        private static readonly Random Rand = new Random();
+
+        private static int[] Solution(int[] arr, int n)
+        {
+            return arr.Take(n).ToArray();
+        }
+
+        private static int[] RandomArray()
+        {
+            return Enumerable.Range(0, Rand.Next(15)).Select(x => Rand.Next(0, 100)).ToArray();
+        }
+
+        [Test]
+        public void RandomTests1()
+        {
+            for (var i = 0; i < 200; i++)
+            {
+                var randomArray = RandomArray();
+                var n = Rand.Next(0, 15);
+                var expected = Solution(randomArray, n);
+                var message = FailureMessage(randomArray, expected, n);
+                var actual = EnumerableMagic25TakeFirstNElements.Take(randomArray, n);
+                // Console.WriteLine(message);
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        [Test]
+        public void RandomTests2()
+        {
+            for (var i = 0; i < 200; i++)
+            {
+                var randomArray = RandomArray();
+                var n = Rand.Next(15, 100);
+                var expected = Solution(randomArray, n);
+                var message = FailureMessage(randomArray, expected, n);
+                var actual = EnumerableMagic25TakeFirstNElements.Take(randomArray, n);
+                // Console.WriteLine(message);
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        private static string FailureMessage(int[] arr, int[] value, int n)
+        {
+            return $"Should return [{string.Join(", ", value)}] with [{string.Join(", ", arr)}] and take {n}";
+        }
+    }
+}
+
+
