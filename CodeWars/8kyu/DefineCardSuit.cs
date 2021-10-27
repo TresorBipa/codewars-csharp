335b8228ac2fe6c3486c4e019380aa648ce46675
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-10-27 09:00:18 +0300

    Added solution "Define a card suit" 8 kyu

---
+++ CodeWars/8kyu/DefineCardSuit.cs
@@ -0,0 +1,73 @@
+﻿/*
+https://www.codewars.com/kata/5a360620f28b82a711000047/
+
+8 kyu
+Define a card suit
+
+You get any card as an argument. Your task is to return a suit of this card.
+
+Our deck (is preloaded):
+
+deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+        '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+        '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+        '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+        
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public static class DefineCardSuit
+    {
+        public static readonly string[] Deck =
+        {
+            "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣", "A♣",
+            "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦", "A♦",
+            "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥", "A♥",
+            "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠", "A♠"
+        };
+
+        public static string DefineSuit(string card)
+        {
+            // return card[^1] switch
+            // {
+            //     '♣' => "clubs",
+            //     '♦' => "diamonds",
+            //     '♥' => "hearts",
+            //     '♠' => "spades",
+            //     _ => throw new ArgumentException()
+            // };
+
+
+            // return card.Contains('♥') ? "hearts" :
+            //     card.Contains('♦') ? "diamonds" :
+            //     card.Contains('♣') ? "clubs" : "spades";
+
+            // return new Dictionary<char, string>()
+            // {
+            //     { '♣', "clubs" },
+            //     { '♦', "diamonds" },
+            //     { '♥', "hearts" },
+            //     { '♠', "spades" }
+            // }[card[^1]];
+
+            // return card.Last() switch
+            return card[^1] switch
+            {
+                '♣' => "clubs",
+                '♦' => "diamonds",
+                '♥' => "hearts",
+                _ => "spades"
+            };
+        }
+    }
+}
---
+++ CodeWarsTests/8kyu/DefineCardSuitTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public partial class DefineCardSuitTests
+    {
+        private static readonly string[] Deck =
+        {
+            "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣", "A♣",
+            "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦", "A♦",
+            "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥", "A♥",
+            "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠", "A♠"
+        };
+
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("clubs", DefineCardSuit.DefineSuit("3♣"), "Should return clubs with \"3♣\"");
+            Assert.AreEqual("spades", DefineCardSuit.DefineSuit("Q♠"), "Should return clubs with \"Q♠\"");
+            Assert.AreEqual("diamonds", DefineCardSuit.DefineSuit("9♦"), "Should return clubs with \"9♦\"");
+            Assert.AreEqual("hearts", DefineCardSuit.DefineSuit("J♥"), "Should return clubs with \"J♥\"");
+        }
+
+        [Test]
+        public void FixedTest()
+        {
+            foreach (var card in Deck)
+            {
+                var expected = Solution(card);
+                var message = FailureMessage(card, expected);
+                var actual = DefineCardSuit.DefineSuit(card);
+
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        private static string Solution(string card)
+        {
+            return card.Last() switch
+            {
+                '♣' => "clubs",
+                '♦' => "diamonds",
+                '♥' => "hearts",
+                '♠' => "spades",
+                _ => throw new ArgumentException()
+            };
+
+            // return card.Last() switch
+            // {
+            //     '♣' => "clubs",
+            //     '♦' => "diamonds",
+            //     '♥' => "hearts",
+            //     _ => "spades"
+            // };
+        }
+
+        private static readonly Random Random = new Random();
+
+        private static string RandomCard()
+        {
+            var card = Deck[Random.Next(Deck.Length)];
+            return card;
+        }
+
+        private static string FailureMessage(string card, string value)
+        {
+            return $"Should return {value} with \"{card}\"";
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                var card = RandomCard();
+                var expected = Solution(card);
+                var message = FailureMessage(card, expected);
+                var actual = DefineCardSuit.DefineSuit(card);
+                // Console.WriteLine(message);
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+    }
+}
+
+
