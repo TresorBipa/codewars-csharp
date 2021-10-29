cf41aedecd49400b26482afe1f1c578c901bf5fb
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-10-29 08:51:35 +0300

    Added solution "sPoNgEbOb MeMe" 7 kyu

---
+++ CodeWars/7kyu/sPoNgEbObMeMe.cs
@@ -0,0 +1,29 @@
+﻿/*
+https://www.codewars.com/kata/5982619d2671576e90000017/csharp
+
+7 kyu
+sPoNgEbOb MeMe
+
+Remember the spongebob meme that is meant to make fun of people by repeating what they say in a mocking way?
+
+You need to create a function that converts the input into this format, 
+with the output being the same string expect there is a pattern of uppercase and lowercase letters.
+
+Examples:
+
+spongeMeme("stop Making spongebob Memes!") // => 'StOp mAkInG SpOnGeBoB MeMeS!'
+spongeMeme("colored teens cant Be successful in tech") // =>'CoLoReD TeEnS CaNt bE SuCcEsSfUl iN TeCh'
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class sPoNgEbObMeMe
+    {
+        public static string SpongeMeme(string sentence)
+        {
+            return string.Concat(sentence.Select((c, i) => i % 2 == 1 ? char.ToLower(c) : char.ToUpper(c)));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/sPoNgEbObMeMeTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class sPoNgEbObMeMeTests
+    {
+        private static object[] Basic_Test_Cases = new object[]
+        {
+            new object[] { "stop Making spongebob Memes!", "StOp mAkInG SpOnGeBoB MeMeS!" },
+            new object[] { "colored teens cant Be successful in tech", "CoLoReD TeEnS CaNt bE SuCcEsSfUl iN TeCh" },
+        };
+
+        [Test, TestCaseSource(typeof(sPoNgEbObMeMeTests), "Basic_Test_Cases")]
+        public void Basic_Test(string test, string expected)
+        {
+            Assert.AreEqual(expected, sPoNgEbObMeMe.SpongeMeme(test));
+        }
+    }
+}
