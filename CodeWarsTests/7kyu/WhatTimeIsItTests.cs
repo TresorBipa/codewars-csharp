f9a3477e92edadc868b275b0d9eb72f17ac1427e
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-10-31 00:27:53 +0300

    Added solution "What time is it?" 7 kyu

---
+++ CodeWarsTests/7kyu/WhatTimeIsItTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class WhatTimeIsItTests
+    {
+        private WhatTimeIsIt _timeService = new WhatTimeIsIt();
+
+        [TestCase("12:00:01AM", "00:00:01")]
+        [TestCase("11:46:47PM", "23:46:47")]
+        public void TimeService_can_convert_from_12_hour_time_format_to_24_hour_time_format(string input,
+            string expected)
+        {
+            var result = _timeService.GetMilitaryTimeFromStandardTime(input);
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
