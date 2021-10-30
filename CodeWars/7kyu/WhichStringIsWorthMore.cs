e90e45c4052ee07e795ea86fafcc6be1a98b8a63
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-10-30 08:46:51 +0300

    Added solution "Which string is worth more?" 7 kyu

---
+++ CodeWars/7kyu/WhichStringIsWorthMore.cs
@@ -0,0 +1,33 @@
+﻿/*
+https://www.codewars.com/kata/5840586b5225616069000001/csharp
+
+7 kyu
+Which string is worth more?
+
+You will be given two ASCII strings, a and b. Your task is write a function to determine which one of these strings is "worth" more, and return it.
+
+A string's worth is determined by the sum of its ASCII codepoint indexes. 
+So, for example, the string HELLO has a value of 372: H is codepoint 72, E 69, L 76, and O is 79. The sum of these values is 372.
+
+In the event of a tie, you should return the first string, i.e. a.
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public class WhichStringIsWorthMore
+    {
+        public static string HighestValue(string a, string b)
+        {
+            return b.Sum(c => c) > a.Sum(c => c) ? b : a;
+            // return a.Sum(c => c) >= b.Sum(c => c) ? a : b;
+
+            // int Sum(string s) => s.Sum(c => c);
+            // return Sum(a) > Sum(b) ? a : b;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/WhichStringIsWorthMoreTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class WhichStringIsWorthMoreTests
+    {
+        private static readonly Random Rand = new Random();
+
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual("KkLlMmNnOoPp4567",
+                WhichStringIsWorthMore.HighestValue("AaBbCcXxYyZz0189", "KkLlMmNnOoPp4567"));
+            Assert.AreEqual("ABcd", WhichStringIsWorthMore.HighestValue("ABcd", "0123"));
+            Assert.AreEqual("{}[]@~'#:;", WhichStringIsWorthMore.HighestValue("!\"?$%^&*()", "{}[]@~'#:;"));
+            Assert.AreEqual("ABCD", WhichStringIsWorthMore.HighestValue("ABCD", "DCBA"));
+            Assert.AreEqual("Abc", WhichStringIsWorthMore.HighestValue("", "Abc"));
+        }
+
+        private static string Solution(string a, string b)
+        {
+            int Sum(string s) => s.Sum(c => c);
+            return Sum(a) >= Sum(b) ? a : b;
+        }
+
+        private static string RandomStr()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()-_=+[]{}|;':,.<>?`~ ";
+            return string.Concat(Enumerable.Repeat(chars, Rand.Next(0, chars.Length))
+                .Select(s => s[Rand.Next(s.Length)]));
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                string a = RandomStr();
+                string b = RandomStr();
+                string actual = WhichStringIsWorthMore.HighestValue(a, b);
+                string expected = Solution(a, b);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
