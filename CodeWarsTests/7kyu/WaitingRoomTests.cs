d75ca7b3b19dbce120bc7c95a02b2dc26ad3b931
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-01 07:57:24 +0300

    Added solution "Waiting room" 7 kyu

---
+++ CodeWarsTests/7kyu/WaitingRoomTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class WaitingRoomTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(WaitingRoom.LastChair(10), 9);
+        }
+    }
+}
