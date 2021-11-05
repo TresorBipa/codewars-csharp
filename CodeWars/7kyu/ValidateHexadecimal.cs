5e691fbe2b6d42cee1db6cb947f025e8211fb9f7
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-05 13:29:06 +0300

    Added solution "Validate Hexadecimal" 7 kyu

---
+++ CodeWars/7kyu/ValidateHexadecimal.cs
@@ -0,0 +1,25 @@
+﻿/*
+https://www.codewars.com/kata/55a5a70c81e8541d990000bd/csharp
+
+7 kyu
+Validate Hexadecimal
+
+Create a function to validate if a string is a valid hexadecimal.
+Two patterns is valid
+
+6 chars (ff00ff)
+3 chars (f0f).
+*/
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class ValidateHexadecimal
+    {
+        public bool IsHex(string hex)
+        {
+            return Regex.IsMatch(hex, @"^(?i)([a-f\d]{3}){1,2}$");
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ValidateHexadecimalTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ValidateHexadecimalTests
+    {
+        [Test]
+        public void _0_BasicTests()
+        {
+            var kata = new ValidateHexadecimal();
+
+            Assert.AreEqual(true, kata.IsHex("ff00ff"));
+            Assert.AreEqual(true, kata.IsHex("f0f"));
+            Assert.AreEqual(false, kata.IsHex("ffg"));
+        }
+    }
+}
