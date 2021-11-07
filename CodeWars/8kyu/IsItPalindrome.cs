21b32262533516721f01e9dc2f61d6bc560c0292
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-07 08:00:10 +0300

    Added solution "Is it a palindrome?" 7 kyu

---
+++ CodeWars/8kyu/IsItPalindrome.cs
@@ -0,0 +1,28 @@
+﻿/*
+https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/csharp
+
+8 kyu
+Is it a palindrome?
+
+Write a function that checks if a given string (case insensitive) is a palindrome.
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public static class KataIsItPalindrome
+    {
+        public static bool IsPalindrome(string s)
+        {
+            return string.Concat(s.Reverse()).ToLower() == s.ToLower();
+
+            // char[] charArray = s.ToCharArray();
+            // Array.Reverse(charArray);
+            // return String.Join("", charArray).ToLower() == s.ToLower();
+        }
+    }
+}
---
+++ CodeWarsTests/8kyu/IsItPalindromeTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataIsItPalindromeTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(true, KataIsItPalindrome.IsPalindrome(""));
+            Assert.AreEqual(true, KataIsItPalindrome.IsPalindrome("a"));
+            Assert.AreEqual(true, KataIsItPalindrome.IsPalindrome("aba"));
+            Assert.AreEqual(true, KataIsItPalindrome.IsPalindrome("Bob"));
+            Assert.AreEqual(true, KataIsItPalindrome.IsPalindrome("oWo"));
+            Assert.AreEqual(false, KataIsItPalindrome.IsPalindrome("toM"));
+            Assert.AreEqual(true, KataIsItPalindrome.IsPalindrome("Abba"));
+            Assert.AreEqual(false, KataIsItPalindrome.IsPalindrome("kata"));
+            Assert.AreEqual(true, KataIsItPalindrome.IsPalindrome("Madam"));
+            Assert.AreEqual(false, KataIsItPalindrome.IsPalindrome("hello"));
+            Assert.AreEqual(false, KataIsItPalindrome.IsPalindrome("not equal"));
+            Assert.AreEqual(true, KataIsItPalindrome.IsPalindrome("xxmZiffiZmxx"));
+            Assert.AreEqual(true, KataIsItPalindrome.IsPalindrome("AABBCCDDddccbbaa"));
+            Assert.AreEqual(false, KataIsItPalindrome.IsPalindrome("asdnaisdoaufnsdfsdfsada"));
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            for (var i = 0; i < 300; i++)
+            {
+                var str = RandomString();
+                var expected = Solution(str);
+                var message = FailureMessage(str, expected);
+                var actual = KataIsItPalindrome.IsPalindrome(str);
+                Console.WriteLine(message);
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        private static bool Solution(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return String.Join("", charArray).ToLower() == s.ToLower();
+        }
+
+        private static readonly Random Rand = new Random();
+        private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        private static string RandomString()
+        {
+            var result = string.Concat(Enumerable.Range(0, Rand.Next(15))
+                .Select(x => Chars[Rand.Next(Chars.Length)]));
+
+            if (Rand.Next(100) % 2 == 0)
+            {
+                result += string.Concat(result.Reverse());
+            }
+
+            return result;
+        }
+
+        private static string FailureMessage(string str, bool value)
+        {
+            return $"Should return {value} with {str}";
+        }
+    }
+}
