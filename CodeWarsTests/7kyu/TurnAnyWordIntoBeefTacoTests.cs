c2a8e18386bdc15c097289798fd369c57b3d55b4
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-08 09:46:05 +0300

    Added solution "Turn any word into a beef taco" 7 kyu

---
+++ CodeWarsTests/7kyu/TurnAnyWordIntoBeefTacoTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class TurnAnyWordIntoBeefTacoTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(new string[] { "shell", "shell" }, TurnAnyWordIntoBeefTaco.Tacofy(""));
+            Assert.AreEqual(new string[] { "shell", "beef", "shell" }, TurnAnyWordIntoBeefTaco.Tacofy("a"));
+            Assert.AreEqual(new string[] { "shell", "guacamole", "guacamole", "guacamole", "shell" },
+                TurnAnyWordIntoBeefTaco.Tacofy("ggg"));
+            Assert.AreEqual(new string[] { "shell", "beef", "guacamole", "lettuce", "shell" },
+                TurnAnyWordIntoBeefTaco.Tacofy("ogl"));
+            Assert.AreEqual(new string[] { "shell", "tomato", "beef", "shell" },
+                TurnAnyWordIntoBeefTaco.Tacofy("ydjkpwqrzto"));
+        }
+    }
+}
