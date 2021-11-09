5ef28f8f2418d952c9141cb03c787b222f7f6968
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-09 23:46:29 +0300

    Added solution "Triangle area" 7 kyu

---
+++ CodeWars/7kyu/TriangleArea.cs
@@ -0,0 +1,44 @@
+﻿/*
+https://www.codewars.com/kata/59bd84b8a0640e7c49002398/csharp
+
+7 kyu
+Triangle area
+
+Task.
+
+Calculate area of given triangle. 
+Create a function t_area that will take a string which will represent triangle, 
+find area of the triangle, one space will be equal to one length unit. 
+The smallest triangle will have one length unit.
+
+Hints
+
+Ignore dots.
+
+Example:
+
+.
+.      .  
+.      .       .      ---> should return 2.0
+
+.
+.      .  
+.      .       .     
+.      .       .      .      ---> should return 4.5
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class TriangleArea
+    {
+        public static double TArea(string s)
+        {
+            return Math.Pow(s.Split('\n').Length - 3, 2) / 2;
+            // return Math.Pow(s.Count(c => c == '\n') - 2, 2) / 2.0;
+            // return Math.Pow(s.Count(i => "\n".Contains(i)) - 2, 2) / 2;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/TriangleAreaTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class TriangleAreaTests
+    {
+        private static string BuildT(int n) =>
+            String.Join("", Enumerable
+                .Range(0, n + 1)
+                .Select((_, i) => string.Concat(Enumerable.Repeat(". ", i)).Trim() + "\n"));
+
+
+        private static void Act(int n, double expected)
+        {
+            var s = BuildT(n);
+            var msg = $"Invalid answer for: \"{s}\"";
+            var actual = TriangleArea.TArea(s);
+            Assert.AreEqual(expected, actual, msg);
+        }
+
+        [TestCase(2, 0.5)]
+        [TestCase(3, 2)]
+        [TestCase(9, 32)]
+        [TestCase(5, 8)]
+        public void FixedTests(int n, double expected) => Act(n, expected);
+    }
+}
