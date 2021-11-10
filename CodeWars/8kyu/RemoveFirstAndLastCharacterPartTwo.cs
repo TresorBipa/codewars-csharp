a7c59194cf28298928705bdf8c79432c3a2d2429
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-10 09:00:07 +0300

    Added solution "Remove First and Last Character Part Two" 8 kyu

---
+++ CodeWars/8kyu/RemoveFirstAndLastCharacterPartTwo.cs
@@ -0,0 +1,54 @@
+﻿/*
+https://www.codewars.com/kata/570597e258b58f6edc00230d/csharp
+
+8 kyu
+Remove First and Last Character Part Two
+
+This is a spin off of my first kata.
+
+You are given a string containing a sequence of characters separated by commas.
+
+Write a function which returns another string containing all the character sequences except the first and the last ones, separated by spaces.
+
+If the input string is empty, or the removal of the first and last items would cause the string to be empty, returns null.
+
+Examples
+// should remove the first and last character
+array('1,2,3') // => '2'
+array('1,2,3,4') // => '2 3'
+array('1,2,3,4,5') // => '2 3 4'
+
+// Should return null if the final string is empty
+array('') // => '' => null
+array('1') // => '' => null
+array('1,2') // => '' => null
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+
+namespace CodeWars
+{
+    public static class RemoveFirstAndLastCharacterPartTwo
+    {
+        public static string Array(string s)
+        {
+            var arr = s.Split(",");
+            return arr.Length > 2 ? string.Join(" ", arr[1..^1]) : null;
+
+
+            // var arr = s.Split(",").ToList();
+            // if (arr.Count > 1)
+            // {
+            //     arr.RemoveAt(0);
+            //     arr.RemoveAt(arr.Count - 1);
+            // }
+            //
+            //
+            // return s.Split(",").Length > 2 ? string.Join(" ", arr) : null;
+        }
+    }
+}
---
+++ CodeWarsTests/8kyu/RemoveFirstAndLastCharacterPartTwoTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class RemoveFirstAndLastCharacterPartTwoTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(null, RemoveFirstAndLastCharacterPartTwo.Array(""));
+            Assert.AreEqual(null, RemoveFirstAndLastCharacterPartTwo.Array("1"));
+            Assert.AreEqual(null, RemoveFirstAndLastCharacterPartTwo.Array("1, 3"));
+            Assert.AreEqual("2", RemoveFirstAndLastCharacterPartTwo.Array("1,2,3"));
+            Assert.AreEqual("2 3", RemoveFirstAndLastCharacterPartTwo.Array("1,2,3,4"));
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            for (var i = 0; i < 300; i++)
+            {
+                var str = RandomString();
+                var expected = Solution(str);
+                var message = FailureMessage(str, expected);
+                var actual = RemoveFirstAndLastCharacterPartTwo.Array(str);
+                // Console.WriteLine(message);
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        private static string Solution(string s)
+        {
+            var arr = s.Split(",");
+            return arr.Length > 2 ? string.Join(" ", arr[1..^1]) : null;
+        }
+
+        private static readonly Random Rand = new Random();
+        private const string Chars = "abcdef123456";
+
+        private static string RandomString()
+        {
+            return string.Join(",", Enumerable.Range(0, Rand.Next(10))
+                .Select(x =>
+                    string.Concat(Enumerable.Range(0, Rand.Next(1, 4)).Select(i => Chars[Rand.Next(Chars.Length)]))));
+        }
+
+        private static string FailureMessage(string str, string value)
+        {
+            return $"Should return {value ?? "null"} with \"{str}\"";
+        }
+    }
+}
