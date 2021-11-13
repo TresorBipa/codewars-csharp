372ce8a2d18ba74f5dc7cd9b64ebbd6607df02a9
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-13 11:51:12 +0300

    Added solution "Case-sensitive!" 7 kyu

---
+++ CodeWars/7kyu/CaseSensitive.cs
@@ -0,0 +1,41 @@
+﻿/*
+https://www.codewars.com/kata/5a805631ba1bb55b0c0000b8
+
+7 kyu
+Case-sensitive!
+
+Your task is very simple. Given an input string s, case_sensitive(s), check whether all letters are lowercase or not. 
+Return True/False and a list of all the entries that are not lowercase in order of their appearance in s.
+
+For example, case_sensitive('codewars') returns [True, []], but case_sensitive('codeWaRs') returns [False, ['W', 'R']].
+
+Goodluck :)
+
+Have a look at my other katas!
+
+Alphabetically ordered
+
+Find Nearest square number
+
+Not prime numbers
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public class KataCaseSensitive
+    {
+        public static Tuple<bool, char[]> CaseSensitive(string s)
+        {
+            var upperChars = s.Where(char.IsUpper).ToArray();
+            return new Tuple<bool, char[]>(!upperChars.Any(), upperChars);
+            // return new Tuple<bool, char[]>(true, new char[] { });
+        }
+    }
+}
+
+
---
+++ CodeWarsTests/7kyu/CaseSensitiveTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataCaseSensitiveTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new Tuple<bool, char[]>(true, new char[] { }), KataCaseSensitive.CaseSensitive("asd"),
+                "Should return [True, []] with \"asd\"");
+
+            Assert.AreEqual(new Tuple<bool, char[]>(false, new char[] { 'S' }),
+                KataCaseSensitive.CaseSensitive("cellS"),
+                "Should return [False, ['S']] with \"cellS\"");
+
+            Assert.AreEqual(new Tuple<bool, char[]>(true, new char[] { }), KataCaseSensitive.CaseSensitive("asd"),
+                "Should return [True, []] with \"z\"");
+
+            Assert.AreEqual(new Tuple<bool, char[]>(true, new char[] { }), KataCaseSensitive.CaseSensitive(""),
+                "Should return [True, []] with \"\"");
+
+            Assert.AreEqual(new Tuple<bool, char[]>(true, new char[] { }), KataCaseSensitive.CaseSensitive("codewars"),
+                "Should return [True, []] with \"\"");
+
+            Assert.AreEqual(new Tuple<bool, char[]>(false, new char[] { 'W', 'R' }),
+                KataCaseSensitive.CaseSensitive("codeWaRs"),
+                "Should return [False, ['W', 'R']] with \"codeWaRs\"");
+        }
+
+        private static Tuple<bool, char[]> Solution(string s)
+        {
+            char[] upperChars = s.Where(char.IsUpper).ToArray();
+            bool isUpperChars = !upperChars.Any();
+            return new Tuple<bool, char[]>(isUpperChars, upperChars);
+        }
+
+        private static readonly Random Rand = new Random();
+
+        private static string RandomStr()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            return string.Concat(Enumerable.Repeat(chars, Rand.Next(0, chars.Length))
+                .Select(s => s[Rand.Next(s.Length)]));
+        }
+
+        private static string FailureMessage(string s, Tuple<bool, char[]> value)
+        {
+            var (isUpperChars, upperChars) = value;
+            var arrayStr = string.Join(", ", upperChars.Select(x => $"'{x}'"));
+            return $"Should return [{isUpperChars}, [{arrayStr}]] with \"{s}\"";
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                string randomStr = RandomStr();
+                Tuple<bool, char[]> expected = Solution(randomStr);
+                string message = FailureMessage(randomStr, expected);
+                Tuple<bool, char[]> actual = KataCaseSensitive.CaseSensitive(randomStr);
+
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+    }
+}
+
+
