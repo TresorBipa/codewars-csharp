42182f7390dcab0c22c51d3b1706ce373eb2d539
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-14 11:56:04 +0300

    Added solution "[Code Golf] String to ASCII Character" 7 kyu

---
+++ CodeWars/7kyu/CodeGolfStringToASCIICharacter.cs
@@ -0,0 +1,30 @@
+﻿/*
+https://www.codewars.com/kata/5abbb33396194245d5000161/
+
+7 kyu
+[Code Golf] String to ASCII Character
+
+You are given a string. Your job is to convert that string to upper case, 
+then find the sum of each character converted to its ASCII value, 
+then divide the sum by the string's length and round it down then convert that to its equivalent character in ASCII.
+
+Note: do it in less than 60 characters
+
+Examples
+"abc"  -->  "B"
+"asd"  -->  "H"
+"iamareallyreallylongstringthatiscompletelyworthlessandisheretostophardcoders"  -->  "L"
+*/
+
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class CodeGolfStringToASCIICharacter
+    {
+        public char Solution(string s) => (char)(s.ToUpper().Sum(x => x) / s.Length);
+    }
+}
---
+++ CodeWarsTests/7kyu/CodeGolfStringToASCIICharacterTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CodeGolfStringToASCIICharacterTests
+    {
+        private CodeGolfStringToASCIICharacter kata = new CodeGolfStringToASCIICharacter();
+
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual('B', kata.Solution("abc"));
+            Assert.AreEqual('H', kata.Solution("asd"));
+
+            Assert.AreEqual('L',
+                kata.Solution(
+                    "iamareallyreallylongstringthatiscompletelyworthlessandisheretostophardcoders"));
+        }
+
+        private static char Solution(string s)
+        {
+            return (char)(s.ToUpper().Sum(x => x) / s.Length);
+        }
+
+        private static readonly Random Rand = new Random();
+
+        private static string RandomStr()
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz";
+            return string.Concat(Enumerable.Repeat(chars, Rand.Next(1, chars.Length))
+                .Select(s => s[Rand.Next(s.Length)]).OrderBy(x => Rand.Next()));
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                var str = RandomStr();
+                var expected = Solution(str);
+                var message = FailureMessage(str, expected);
+                var actual = kata.Solution(str);
+                Console.WriteLine(message);
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        private static string FailureMessage(string str, char value)
+        {
+            return $"Should return '{value}' with \"{str}\"";
+        }
+
+        // [Test]
+        // public void CodeSizeCheckTest()
+        // {
+        //     // var code = System.IO.File.ReadAllText(@"/workspace/solution.txt");
+        //     var code = System.IO.File.ReadAllText(
+        //         @"D:\csharp\codewars-csharp\CodeWars\CodeGolfStringToASCIICharacter.cs");
+        //     Console.WriteLine(code.Length);
+        //     Assert.AreEqual(true, code.Length < 860, $"Your code should be less than 60 characters ({code.Length})");
+        // }
+    }
+}
