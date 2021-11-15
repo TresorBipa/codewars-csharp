dd74654eb9ee85e5ee6f54e21225f79d91b1ff67
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-15 12:43:20 +0300

    Added solution "Supernatural" 7 kyu

---
+++ CodeWarsTests/7kyu/SupernaturalTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SupernaturalTests
+    {
+        [Test]
+        public static void FixedTest()
+        {
+            Assert.AreEqual("Behead it with a machete, idjits!", Supernatural.Bob("vampire"));
+            Assert.AreEqual("It depends on which one it is, idjits!", Supernatural.Bob("pagan god"));
+            Assert.AreEqual("I have friggin no idea yet, idjits!", Supernatural.Bob("werepuppy"));
+        }
+    }
+}
