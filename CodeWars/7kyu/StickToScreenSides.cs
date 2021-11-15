74a20cc87f241b5b332ef82118e02375bb52f99f
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-16 00:17:28 +0300

    Added solution "Stick to The Screen Sides" 7 kyu

---
+++ CodeWars/7kyu/StickToScreenSides.cs
@@ -0,0 +1,48 @@
+﻿/*
+https://www.codewars.com/kata/5641d9d4aceb8e30dc0000aa/csharp
+
+7 kyu
+Stick to The Screen Sides
+
+Suppose there is a computer screen. When you move the cursor, and it gets close to the sides of the screen, the cursor will stick to them.
+
+You have to implement a function which receives:
+
+the screen dimensions
+the sticky area width (a.k.a. "margin")
+the current position of the cursor
+and returns the new position of the cursor after it was possibly stuck to the screen side(s).
+
+Note: if the mouse is outside the screen dimensions, return [0, 0] instead.
+*/
+
+namespace CodeWars
+{
+    public class StickToScreenSides
+    {
+        public static double[] newCoordinate(int w, int h, double margin, double[] coordinate)
+        {
+            double x = coordinate[0];
+            double y = coordinate[1];
+
+            return x > w | y > h | x < 0 | y < 0
+                ? new double[] {0, 0}
+                : new[]
+                {
+                    x <= margin ? 0 : x >= w - margin ? w : x,
+                    y <= margin ? 0 : y >= h - margin ? h : y
+                };
+
+            // double x = coordinate[0];
+            // double y = coordinate[1];
+            //
+            // if (x < 0 || x > w || y < 0 || y > h) return new double[] {0, 0};
+            //
+            // return new[]
+            // {
+            //     x <= margin ? 0 : x >= w - margin ? w : x,
+            //     y <= margin ? 0 : y >= h - margin ? h : y
+            // };
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/StickToScreenSidesTests.cs
@@ -0,0 +1,39 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class StickToScreenSidesTests
+    {
+        [Test]
+        public void StickToRight()
+        {
+            Assert.AreEqual(new[] {100, 23.32}, StickToScreenSides.newCoordinate(100, 50, 10, new[] {91.52, 23.32}));
+        }
+
+        [Test]
+        public void StickToLeft()
+        {
+            Assert.AreEqual(new[] {0, 23.32}, StickToScreenSides.newCoordinate(120, 100, 5, new[] {5, 23.32}));
+        }
+
+        [Test]
+        public void StickToUp()
+        {
+            Assert.AreEqual(new[] {91.52, 100}, StickToScreenSides.newCoordinate(200, 100, 20, new[] {91.52, 81.32}));
+        }
+
+        [Test]
+        public void StickToDown()
+        {
+            Assert.AreEqual(new[] {21.52, 0}, StickToScreenSides.newCoordinate(40, 20, 3, new[] {21.52, 2.99}));
+        }
+
+        [Test]
+        public void OutOfScreen()
+        {
+            Assert.AreEqual(new[] {0, 0}, StickToScreenSides.newCoordinate(1000, 700, 10, new[] {-0.01, 23.32}));
+        }
+    }
+}
