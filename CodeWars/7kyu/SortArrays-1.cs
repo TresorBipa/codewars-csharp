c23f053a87da965bc5c29e520f610b36d260f1c6
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-17 09:15:46 +0300

    Added solution "Sort arrays - 1" 7 kyu

---
+++ CodeWars/7kyu/SortArrays-1.cs
@@ -0,0 +1,25 @@
+﻿/*
+https://www.codewars.com/kata/51f41b98e8f176e70d0002a8/csharp
+
+7 kyu
+Sort arrays - 1
+
+Just a simple sorting usage. Create a function that returns the elements of the input-array in a sorted manner.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SortArrays_1
+    {
+        public static string[] SortMe(string[] names)
+        {
+            // Array.Sort(names);
+            // return names;
+
+            return names.OrderBy(s => s).ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SortArrays-1Tests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SortArrays_1Tests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("one,three,two", string.Join(",", SortArrays_1.SortMe(new[] {"one", "two", "three"})));
+        }
+    }
+}
