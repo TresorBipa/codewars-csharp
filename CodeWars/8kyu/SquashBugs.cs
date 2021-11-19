bb33673836e0ddabec365e4e85e2e24d768a2b1b
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-19 20:22:52 +0300

    Added solution "Squash the bugs" 8 kyu

---
+++ CodeWars/8kyu/SquashBugs.cs
@@ -0,0 +1,55 @@
+﻿/*
+https://www.codewars.com/kata/56f173a35b91399a05000cb7/csharp
+
+8 kyu
+Squash the bugs
+
+Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. 
+Output should be the length of the longest word, as a number.
+
+There will only be one 'longest' word.
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public static class SquashBugs
+    {
+/*
+        public static int FindLongestWithBugs(string str) (
+
+          var spl = str.Split(" ");
+          var longest = 0
+
+          for (var i = 0; i > spl.Length; i+) (
+            if (spl(i).Length > longest) {
+              longest = spl[i].Length
+            )
+            }
+            return longest
+        )
+*/
+
+        public static int FindLongest(string str)
+        {
+            return str.Split().Max(x => x.Length);
+
+            // var spl = str.Split(" ");
+            // var longest = 0;
+            //
+            // for (var i = 0; i < spl.Length; i++)
+            // {
+            //     if (spl[i].Length > longest)
+            //     {
+            //         longest = spl[i].Length;
+            //     }
+            // }
+            //
+            // return longest;
+        }
+    }
+}
---
+++ CodeWarsTests/8kyu/SquashBugsTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SquashBugsTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(0, SquashBugs.FindLongest(""));
+            Assert.AreEqual(4, SquashBugs.FindLongest("Bugs"));
+            Assert.AreEqual(6, SquashBugs.FindLongest("  Spaces   "));
+            Assert.AreEqual(3, SquashBugs.FindLongest(" A  b   c xyz"));
+            Assert.AreEqual(7, SquashBugs.FindLongest("Sausage chops"));
+            Assert.AreEqual(7, SquashBugs.FindLongest("Lets all go on holiday"));
+            Assert.AreEqual(10, SquashBugs.FindLongest("Take me to tinseltown with you"));
+            Assert.AreEqual(6, SquashBugs.FindLongest("Wind your body and wiggle your belly"));
+            Assert.AreEqual(7, SquashBugs.FindLongest("The quick white fox jumped around the massive dog"));
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            for (var i = 0; i < 300; i++)
+            {
+                var str = RandomString();
+                var expected = Solution(str);
+                var message = FailureMessage(str, expected);
+                var actual = SquashBugs.FindLongest(str);
+                Console.WriteLine(message);
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        private static int Solution(string str)
+        {
+            var spl = str.Split(" ");
+            var longest = 0;
+
+            for (var i = 0; i < spl.Length; i++)
+            {
+                if (spl[i].Length > longest)
+                {
+                    longest = spl[i].Length;
+                }
+            }
+
+            return longest;
+        }
+
+        private static readonly Random Rand = new Random();
+
+        private static readonly string[] Names =
+        {
+            "Arsene", "Lupin", "III", "Daisuke", "Jigen", "Goemon", "Ishikawa", "Fujiko", "Mine", "Rebecca",
+            "Rossellini", "Koichi", "Zenigata", "Justin", "Nix", "Person", "Leonardo", "Da", "Vinci", "Vasya"
+        };
+
+        private static string RandomString()
+        {
+            return string.Join(" ",
+                Enumerable.Range(0, Rand.Next(Names.Length + 1))
+                    .Select(x => Names[Rand.Next(Rand.Next(Names.Length + 1))]));
+        }
+
+        private static string FailureMessage(string str, int value)
+        {
+            return $"Should return {value} with \"{str}\"";
+        }
+    }
+}
