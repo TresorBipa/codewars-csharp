3ec927fde759f23c913205c9862c9eec27cbc05c
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-20 13:16:47 +0300

    Added solution "Power of 4" 7 kyu

---
+++ CodeWars/7kyu/PowerOf4.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/544d114f84e41094a9000439/csharp
+
+7 kyu
+Power of 4
+
+Write a method that returns true if a given parameter is a power of 4, and false if it's not. 
+If parameter is not an Integer (eg String, Array) method should return false as well.
+
+(In C# Integer means all integer Types like Int16,Int32,.....)
+
+Examples
+Power.PowerOf4(1024); // returns true
+Power.PowerOf4(44); // returns false
+Power.PowerOf4("not a positive integer"); // returns false
+*/
+
+using System;
+
+namespace CodeWars
+{
+    public class KataPowerOf4
+    {
+        public static bool PowerOf4(object n)
+        {
+            // if (n == null || n is string || !long.TryParse(n.ToString(), out var number) || number < 4) return false;
+            // return Math.Log(number, 4) % 1 == 0;
+
+            return (n is long || n is int) && Convert.ToInt64(n) > 1 && Math.Log(Convert.ToInt64(n), 4) % 1 == 0;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/PowerOf4Tests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PowerOf4Tests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(true, KataPowerOf4.PowerOf4(4));
+            Assert.AreEqual(true, KataPowerOf4.PowerOf4(16));
+            Assert.AreEqual(false, KataPowerOf4.PowerOf4(1));
+            Assert.AreEqual(false, KataPowerOf4.PowerOf4(3.1415));
+            Assert.AreEqual(false, KataPowerOf4.PowerOf4("4"));
+            Assert.AreEqual(false, KataPowerOf4.PowerOf4(null));
+        }
+    }
+}
