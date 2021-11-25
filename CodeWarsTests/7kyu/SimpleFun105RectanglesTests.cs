4a3f416e4a19bbd71de9837cfd505f425db66343
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-11-25 15:06:38 +0300

    Added solution "Simple Fun #105: Rectangles" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun105RectanglesTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun105RectanglesTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun105Rectangles();
+            Assert.AreEqual(3, kata.Rectangles(2, 3));
+            Assert.AreEqual(1, kata.Rectangles(2, 2));
+            Assert.AreEqual(0, kata.Rectangles(1, 1));
+            Assert.AreEqual(0, kata.Rectangles(0, 1));
+            Assert.AreEqual(9, kata.Rectangles(3, 3));
+            Assert.AreEqual(24502500, kata.Rectangles(100, 100));
+        }
+    }
+}
