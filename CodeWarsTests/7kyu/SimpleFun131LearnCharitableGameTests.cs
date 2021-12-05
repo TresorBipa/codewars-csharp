d8f0b5789c4ab0fed42ed9dd85430462d811ce7f
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-12-05 10:04:00 +0300

    Added solution "Simple Fun #131: Learn Charitable Game" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun131LearnCharitableGameTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun131LearnCharitableGameTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun131LearnCharitableGame();
+
+            Assert.AreEqual(false, kata.LearnCharitableGame(new int[] {100, 100, 100, 90, 1, 0, 0}));
+
+            Assert.AreEqual(false, kata.LearnCharitableGame(new int[] {0, 0, 0, 0}));
+
+            Assert.AreEqual(true, kata.LearnCharitableGame(new int[] {0, 56, 100}));
+
+            Assert.AreEqual(false, kata.LearnCharitableGame(new int[] {33, 19, 38, 87, 93, 4}));
+
+            Assert.AreEqual(true, kata.LearnCharitableGame(new int[] {11}));
+        }
+    }
+}
