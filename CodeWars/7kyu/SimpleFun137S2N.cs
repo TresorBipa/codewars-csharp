1bf36d2689e6c4b1b34ceae9a4175b1edb2fe635
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-12-07 09:30:36 +0300

    Added solution "Simple Fun #137: S2N" 7 kyu

---
+++ CodeWars/7kyu/SimpleFun137S2N.cs
@@ -0,0 +1,43 @@
+﻿/*
+https://www.codewars.com/kata/58a6742c14b042a042000038/csharp
+
+7 kyu
+Simple Fun #137: S2N
+
+Task
+Your task is to find the sum for the range 0 ... m for all powers from `0 ... n.
+
+Example
+For m = 2, n = 3, the result should be 20
+
+0^0+1^0+2^0 + 0^1+1^1+2^1 + 0^2+1^2+2^2 + 0^3+1^3+2^3 = 20
+
+Note, that no output ever exceeds 2e9.
+
+Input/Output
+[input] integer m
+
+0 <= m <= 50000
+
+[input] integer n
+
+0 <= n <= 9
+
+[output] an integer(double in C#)
+
+The sum value.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SimpleFun137S2N
+    {
+        public double S2N(int m, int n)
+        {
+            return Enumerable.Range(0, n + 1).Sum(x => Enumerable.Range(0, m + 1).Select(i => Math.Pow(i, x)).Sum());
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SimpleFun137S2NTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun137S2NTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun137S2N();
+
+            Assert.AreEqual(20, kata.S2N(2, 3));
+
+            Assert.AreEqual(434, kata.S2N(3, 5));
+
+            Assert.AreEqual(1762344782, kata.S2N(10, 9));
+
+            Assert.AreEqual(3, kata.S2N(1, 1));
+
+            Assert.AreEqual(1, kata.S2N(0, 0));
+
+            Assert.AreEqual(9090501, kata.S2N(300, 2));
+
+            Assert.AreEqual(61083856, kata.S2N(567, 2));
+
+            Assert.AreEqual(15335280, kata.S2N(37, 4));
+
+            Assert.AreEqual(13409059, kata.S2N(36, 4));
+        }
+    }
+}
