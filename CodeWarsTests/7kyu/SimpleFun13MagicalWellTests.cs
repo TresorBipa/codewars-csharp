8aa0379a73ea3cd1dd7fb22668c1e6ea61b1d284
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-12-09 06:50:17 +0300

    Added solution "Simple Fun #13: Magical Well" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun13MagicalWellTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun13MagicalWellTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun13MagicalWell();
+
+            Assert.AreEqual(8, kata.MagicalWell(1, 2, 2), "");
+
+            Assert.AreEqual(1, kata.MagicalWell(1, 1, 1), "");
+
+            Assert.AreEqual(128, kata.MagicalWell(6, 5, 3), "");
+        }
+    }
+}
