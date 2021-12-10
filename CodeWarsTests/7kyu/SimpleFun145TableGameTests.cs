b45d2ef1a8254f7f30698ea68193bfc62adb0192
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-12-10 19:12:14 +0300

    Added solution "Simple Fun #145: Table Game" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun145TableGameTests.cs
@@ -0,0 +1,79 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun145TableGameTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun145TableGame();
+
+            Assert.AreEqual(new int[] {1, 1, 1, 1},
+                kata.TableGame(new int[][]
+                {
+                    new int[] {1, 2, 1},
+                    new int[] {2, 4, 2},
+                    new int[] {1, 2, 1}
+                }));
+
+            Assert.AreEqual(new int[] {3, 4, 2, 7},
+                kata.TableGame(new int[][]
+                {
+                    new int[] {3, 7, 4},
+                    new int[] {5, 16, 11},
+                    new int[] {2, 9, 7}
+                }));
+
+            Assert.AreEqual(new int[] {-1},
+                kata.TableGame(new int[][]
+                {
+                    new int[] {1, 4, 2},
+                    new int[] {5, 10, 5},
+                    new int[] {4, 7, 3}
+                }));
+
+            Assert.AreEqual(new int[] {-1},
+                kata.TableGame(new int[][]
+                {
+                    new int[] {1, 4, 2},
+                    new int[] {5, 10, 5},
+                    new int[] {4, 7, 3}
+                }));
+
+            Assert.AreEqual(new int[] {-1},
+                kata.TableGame(new int[][]
+                {
+                    new int[] {2, 4, 2},
+                    new int[] {4, 6, 4},
+                    new int[] {2, 4, 2}
+                }));
+
+            Assert.AreEqual(new int[] {2, 2, 2, 2},
+                kata.TableGame(new int[][]
+                {
+                    new int[] {2, 4, 2},
+                    new int[] {4, 8, 4},
+                    new int[] {2, 4, 2}
+                }));
+
+            Assert.AreEqual(new int[] {1, 2, 4, 3},
+                kata.TableGame(new int[][]
+                {
+                    new int[] {1, 3, 2},
+                    new int[] {5, 10, 5},
+                    new int[] {4, 7, 3}
+                }));
+
+            Assert.AreEqual(new int[] {-1},
+                kata.TableGame(new int[][]
+                {
+                    new int[] {1, 2, 1},
+                    new int[] {1, 2, 1},
+                    new int[] {1, 2, 1}
+                }));
+        }
+    }
+}
