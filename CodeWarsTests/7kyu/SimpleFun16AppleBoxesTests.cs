34d75c8ba210fbbc1849d13ac71bf8d2ea366ee5
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-12-15 09:00:32 +0300

    Added solution "Simple Fun #16: Apple Boxes" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun16AppleBoxesTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun16AppleBoxesTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun16AppleBoxes();
+
+            Assert.AreEqual(-15, kata.AppleBoxes(5));
+
+            Assert.AreEqual(-120, kata.AppleBoxes(15));
+
+            Assert.AreEqual(666, kata.AppleBoxes(36));
+
+            Assert.AreEqual(-1, kata.AppleBoxes(1));
+        }
+    }
+}
