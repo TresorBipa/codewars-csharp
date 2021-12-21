30ac6661300fdf00ed322999355bb84216d33b2d
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-12-21 09:12:24 +0300

    Added solution "Simple Fun #182: Happy "g"" 7 kyu

---
+++ CodeWars/7kyu/SimpleFun182HappyG.cs
@@ -0,0 +1,36 @@
+﻿/*
+https://www.codewars.com/kata/58bcd27b7288983803000002/csharp
+
+7 kyu
+Simple Fun #182: Happy "g"
+
+Task
+Let's say that "g" is happy in the given string, if there is another "g" immediately to the right or to the left of it.
+
+Find out if all "g"s in the given string are happy.
+
+Example
+For str = "gg0gg3gg0gg", the output should be true.
+
+For str = "gog", the output should be false.
+
+Input/Output
+[input] string str
+A random string of lower case letters, numbers and spaces.
+
+[output] a boolean value
+true if all "g"s are happy, false otherwise.
+*/
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class SimpleFun182HappyG
+    {
+        public bool HappyG(string str)
+        {
+            return !Regex.IsMatch(str, "(?<!g)g(?!g)");
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SimpleFun182HappyGTests.cs
@@ -0,0 +1,29 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun182HappyGTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun182HappyG();
+
+            Assert.AreEqual(true, kata.HappyG("gg0gg3gg0gg"));
+
+            Assert.AreEqual(false, kata.HappyG("gog"));
+
+            Assert.AreEqual(false, kata.HappyG("ggg ggg g ggg"));
+
+            Assert.AreEqual(true, kata.HappyG("A half of a half is a quarter."));
+
+            Assert.AreEqual(false, kata.HappyG("good grief"));
+
+            Assert.AreEqual(true, kata.HappyG("bigger is ggooder"));
+
+            Assert.AreEqual(true, kata.HappyG("gggggggggg"));
+        }
+    }
+}
