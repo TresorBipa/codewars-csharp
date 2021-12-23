1c2b29ea8828bb9539812d7aa58f1ddb39f32d36
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-12-23 10:22:30 +0300

    Added solution "Simple Fun #187: Does Fred Need A Houseboat?" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun187DoesFredNeedHouseboatTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun187DoesFredNeedHouseboatTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun187DoesFredNeedHouseboat();
+            Assert.AreEqual(1, kata.DoesFredNeedHouseboat(1, 1));
+            Assert.AreEqual(20, kata.DoesFredNeedHouseboat(25, 0));
+        }
+    }
+}
