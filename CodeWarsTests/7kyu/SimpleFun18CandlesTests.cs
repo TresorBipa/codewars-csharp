09e2fe8b2ce5af789b38507fb8ddcdc3c5eaaf25
Alexander Kozhanov <a.kozhanov@outlook.com>
2021-12-25 12:00:37 +0300

    Added solution "Simple Fun #18: Candles" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun18CandlesTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun18CandlesTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun18Candles();
+
+            Assert.AreEqual(9, kata.Candles(5, 2));
+
+            Assert.AreEqual(1, kata.Candles(1, 2));
+
+            Assert.AreEqual(4, kata.Candles(3, 3));
+
+            Assert.AreEqual(16, kata.Candles(11, 3));
+        }
+    }
+}
