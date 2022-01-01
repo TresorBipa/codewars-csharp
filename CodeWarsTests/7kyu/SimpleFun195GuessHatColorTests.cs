ddfae0176444f8e6d0c51fd4f2509395d602c425
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-01-01 14:03:30 +0300

    Added solution "Simple Fun #195: Guess Hat Color" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun195GuessHatColorTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun195GuessHatColorTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun195GuessHatColor();
+
+            Assert.AreEqual(2, kata.GuessHatColor("black", "white", "black", "white"));
+
+            Assert.AreEqual(2, kata.GuessHatColor("white", "black", "white", "black"));
+
+            Assert.AreEqual(1, kata.GuessHatColor("white", "black", "black", "white"));
+        }
+    }
+}
