122ef5ef53a6f94db8f08fb1baea2adf0fd01c7f
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-01-02 12:01:50 +0300

    Added solution "Simple Fun #199: Pass The Bill" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun199PassBillTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun199PassBillTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun199PassBill();
+            Assert.AreEqual(2, kata.PassTheBill(8, 3, 3));
+            Assert.AreEqual(-1, kata.PassTheBill(13, 4, 7));
+            Assert.AreEqual(0, kata.PassTheBill(7, 4, 3));
+            Assert.AreEqual(2, kata.PassTheBill(11, 4, 1));
+            Assert.AreEqual(1, kata.PassTheBill(11, 5, 1));
+            Assert.AreEqual(0, kata.PassTheBill(11, 6, 1));
+            Assert.AreEqual(2, kata.PassTheBill(11, 4, 4));
+            Assert.AreEqual(1, kata.PassTheBill(11, 5, 4));
+            Assert.AreEqual(1, kata.PassTheBill(11, 5, 5));
+            Assert.AreEqual(-1, kata.PassTheBill(11, 4, 6));
+            Assert.AreEqual(2, kata.PassTheBill(11, 4, 5));
+            Assert.AreEqual(0, kata.PassTheBill(15, 9, 3));
+        }
+    }
+}
