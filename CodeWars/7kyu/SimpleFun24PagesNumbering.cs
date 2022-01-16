57913ce043115a67716818a5b0d5f0703bdd0bcf
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-01-16 10:26:37 +0300

    Added solution "Simple Fun #24: Pages Numbering with Ink" 7 kyu

---
+++ CodeWars/7kyu/SimpleFun24PagesNumbering.cs
@@ -0,0 +1,74 @@
+﻿/*
+https://www.codewars.com/kata/5886da134703f125a6000033/csharp
+
+7 kyu
+Simple Fun #24: Pages Numbering with Ink
+
+Task
+You work in a company that prints and publishes books. You are responsible for designing the page numbering mechanism in the printer. 
+You know how many digits a printer can print with the leftover ink.
+
+Now you want to write a function to determine what the last page of the book is that you can number given the current page and numberOfDigits/number_of_digits left.
+
+A page is considered numbered if it has the full number printed on it 
+(e.g. if we are working with page 102 but have ink only for two digits then this page will not be considered numbered).
+
+It's guaranteed that you can number the current page, and that you can't number the last one in the book.
+
+Example
+For current = 1 and numberOfDigits = 5, the output should be 5
+
+The following numbers will be printed: 1, 2, 3, 4, 5.
+
+For current = 21 and numberOfDigits = 5, the output should be 22
+
+The following numbers will be printed: 21, 22.
+
+For current = 8 and numberOfDigits = 4, the output should be 10
+
+The following numbers will be printed: 8, 9, 10.
+
+Input/Output
+[input] integer current
+
+A positive integer, the number on the current page which is not yet printed.
+
+Constraints: 1 ≤ current ≤ 1000
+
+[input] integer numberOfDigits/number_of_digits
+
+A positive integer, the number of digits which your printer can print.
+
+Constraints: 1 ≤ numberOfDigits ≤ 1000 (or equivalent in PHP)
+
+[output] an integer
+
+The last printed page number.
+*/
+
+namespace CodeWars
+{
+    public class SimpleFun24PagesNumbering
+    {
+        public int PagesNumberingWithInk(int current, int numberOfDigits)
+        {
+            while (numberOfDigits >= $"{current}".Length)
+            {
+                numberOfDigits -= $"{current}".Length;
+                current++;
+            }
+            
+            return current - 1;
+
+
+            // while (true)
+            // {
+            //     numberOfDigits -= current.ToString().Length;
+            //     if (numberOfDigits <= 0) break;
+            //     current++;
+            // }
+            //
+            // return numberOfDigits == 0 ? current : current - 1;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SimpleFun24PagesNumberingTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun24PagesNumberingTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun24PagesNumbering();
+
+            Assert.AreEqual(5, kata.PagesNumberingWithInk(1, 5));
+
+            Assert.AreEqual(22, kata.PagesNumberingWithInk(21, 5));
+
+            Assert.AreEqual(10, kata.PagesNumberingWithInk(8, 4));
+
+            Assert.AreEqual(23, kata.PagesNumberingWithInk(21, 6));
+
+            Assert.AreEqual(166, kata.PagesNumberingWithInk(76, 250));
+
+            Assert.AreEqual(419, kata.PagesNumberingWithInk(80, 1000));
+        }
+    }
+}
