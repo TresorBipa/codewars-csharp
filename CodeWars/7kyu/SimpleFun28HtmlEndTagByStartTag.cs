c5defae5cc9b2b2d58746f159aa9feb194c644a0
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-01-17 09:11:16 +0300

    Added solution "Simple Fun #28: Html End Tag By Start Tag" 7 kyu

---
+++ CodeWars/7kyu/SimpleFun28HtmlEndTagByStartTag.cs
@@ -0,0 +1,43 @@
+﻿/*
+https://www.codewars.com/kata/5886f3713a111b620f0000dc/csharp
+
+7 kyu
+Simple Fun #28: Html End Tag By Start Tag
+
+Task
+You are implementing your own HTML editor. 
+To make it more comfortable for developers you would like to add an auto-completion feature to it.
+
+Given the starting HTML tag, find the appropriate end tag which your editor should propose.
+
+Example
+For startTag = "<button type='button' disabled>", the output should be "</button>";
+
+For startTag = "<i>", the output should be "</i>".
+
+Input/Output
+[input] string startTag/start_tag
+
+[output] a string
+*/
+
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class SimpleFun28HtmlEndTagByStartTag
+    {
+        public string HtmlEndTagByStartTag(string StartTag)
+        {
+            return "</" + Regex.Match(StartTag, @"\w+") + ">";
+            // return "</" + StartTag.Split('<', ' ', '>')[1] + ">";
+            // return $"</{Regex.Match(StartTag, "[a-z]+").Value}>";
+            // return $"</{Regex.Match(StartTag, @"<([a-z]+)").Groups[1].Value}>";
+            // return $"</{Regex.Match(StartTag, "^<([a-z]+)").Groups[1].Value}>";
+            // return $"</{string.Concat(StartTag.Skip(1).TakeWhile(char.IsLetter))}>";
+            // return Regex.Replace(Regex.Replace(StartTag, " .*?>", ">"), "^<", "</");
+            // return "</" + StartTag.Split(' ').First().TrimEnd('>').Substring(1) + ">";
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SimpleFun28HtmlEndTagByStartTagTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun28HtmlEndTagByStartTagTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun28HtmlEndTagByStartTag();
+
+            Assert.AreEqual("</button>", kata.HtmlEndTagByStartTag("<button type='button' disabled>"));
+
+            Assert.AreEqual("</i>", kata.HtmlEndTagByStartTag("<i>"));
+
+            Assert.AreEqual("</div>",
+                kata.HtmlEndTagByStartTag(
+                    "<div id='my_area' class='data' title='This is a test for title on Div tag'>"));
+        }
+    }
+}
