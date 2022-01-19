f368d3a1257088e8b67cafe44ac1a6d9f8a82d4c
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-01-19 09:22:26 +0300

    Added solution "Simple Fun #32: Create Anagram" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun32CreateAnagramTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun32CreateAnagramTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun32CreateAnagram();
+
+            Assert.AreEqual(1, kata.CreateAnagram("AABAA", "BBAAA"));
+
+            Assert.AreEqual(4, kata.CreateAnagram("OVGHK", "RPGUC"));
+
+            Assert.AreEqual(1,
+                kata.CreateAnagram("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAC"));
+        }
+    }
+}
