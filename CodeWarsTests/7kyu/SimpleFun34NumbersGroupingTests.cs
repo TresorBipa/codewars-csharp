df6e97654bb2e4e82aafef4d9893f2850256aa2e
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-01-20 10:52:50 +0300

    Added solution "Simple Fun #34: Numbers Grouping" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun34NumbersGroupingTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun34NumbersGroupingTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun34NumbersGrouping();
+
+            Assert.AreEqual(11, kata.NumbersGrouping(new int[] {20000, 239, 10001, 999999, 10000, 20566, 29999}));
+
+            Assert.AreEqual(23,
+                kata.NumbersGrouping(new int[]
+                    {10000, 20000, 30000, 40000, 50000, 60000, 10000, 120000, 150000, 200000, 300000, 1000000}));
+
+            Assert.AreEqual(2, kata.NumbersGrouping(new int[] {10000}));
+
+            Assert.AreEqual(3, kata.NumbersGrouping(new int[] {10000, 1}));
+        }
+    }
+}
