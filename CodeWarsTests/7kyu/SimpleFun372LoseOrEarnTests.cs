f95af0e3ce18ff12d73471fb892ab03e5c41a9b4
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-01-21 10:21:52 +0300

    Added solution "Simple Fun #372: Lose Or Earn?" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun372LoseOrEarnTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun372LoseOrEarnTests
+    {
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test, Description("It should works for basic tests.")]
+            public void SampleTest()
+            {
+                Assert.That(SimpleFun372LoseOrEarn.LoseOrEarn(40, 70, 100, 30), Is.EqualTo("Lost $70"));
+                Assert.That(SimpleFun372LoseOrEarn.LoseOrEarn(40, 70, 50, 20), Is.EqualTo("Lost $20"));
+                Assert.That(SimpleFun372LoseOrEarn.LoseOrEarn(14604, 32391, 3902, 153), Is.EqualTo("Earned $13885"));
+            }
+        }
+    }
+}
