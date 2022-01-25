539692cbb19d78ab9de37fcb1f6f4e371ad813f7
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-01-25 10:03:32 +0300

    Added solution "Simple Fun #39: Switch Lights" 7 kyu

---
+++ CodeWarsTests/7kyu/SimpleFun39SwitchLightsTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun39SwitchLightsTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun39SwitchLights();
+
+            Assert.AreEqual(new int[] {0, 1, 0, 1, 0}, kata.SwitchLights(new int[] {1, 1, 1, 1, 1}));
+
+            Assert.AreEqual(new int[] {0, 0}, kata.SwitchLights(new int[] {0, 0}));
+
+            Assert.AreEqual(new int[] {1, 1, 1, 0, 0, 1, 1, 0}, kata.SwitchLights(new int[] {1, 0, 0, 1, 0, 1, 0, 1}));
+
+            Assert.AreEqual(new int[] {1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0},
+                kata.SwitchLights(new int[] {1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1}));
+        }
+    }
+}
