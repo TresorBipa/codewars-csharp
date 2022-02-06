ac0ee943ebbf715a62861559e7b59216b0579d52
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-02-06 18:29:02 +0300

    Added solution "Simple Fun #50: Array Conversion"

---
+++ CodeWarsTests/7kyu/SimpleFun50ArrayConversionTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun50ArrayConversionTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun50ArrayConversion();
+
+            Assert.AreEqual(186, kata.ArrayConversion(new int[] {1, 2, 3, 4, 5, 6, 7, 8}));
+
+            Assert.AreEqual(64, kata.ArrayConversion(new int[] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}));
+
+            Assert.AreEqual(60, kata.ArrayConversion(new int[] {3, 3, 5, 5}));
+        }
+    }
+}
