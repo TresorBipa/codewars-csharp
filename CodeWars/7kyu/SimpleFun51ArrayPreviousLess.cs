cbca1eb384a97d8bea4123375f4b075d14290d70
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-02-08 08:34:32 +0300

    Added solution "Simple Fun #51: Array Previous Less"

---
+++ CodeWars/7kyu/SimpleFun51ArrayPreviousLess.cs
@@ -0,0 +1,47 @@
+﻿/*
+https://www.codewars.com/kata/588856a82ffea640c80000cc/csharp
+
+7 kyu
+Simple Fun #51: Array Previous Less
+
+Task
+Given array of integers, for each position i, search among the previous positions for the last (from the left) position that contains a smaller value. 
+Store this value at position i in the answer. If no such value can be found, store -1 instead.
+
+Example
+For items = [3, 5, 2, 4, 5], the output should be [-1, 3, -1, 2, 4].
+
+Input/Output
+[input] integer array arr
+
+Non-empty array of positive integers.
+
+Constraints: 3 ≤ arr.length ≤ 1000, 1 ≤ arr[i] ≤ 1000.
+
+[output] an integer array
+
+Array containing answer values computed as described above.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SimpleFun51ArrayPreviousLess
+    {
+        public int[] ArrayPreviousLess(int[] arr)
+        {
+            return arr.Select((x, i) => arr.Take(i).Where(n => n < x).DefaultIfEmpty(-1).Last()).ToArray();
+
+            // return arr.Select((el, i) =>
+            //     arr.Take(i)
+            //         .Where(x => x < el)
+            //         .DefaultIfEmpty(-1)
+            //         .Last()
+            // ).ToArray();
+
+            // return arr.Select((x, i) => arr.Take(i).Any(k => k < x) ? arr.Take(i).Reverse().First(y => y < x) : -1)
+            //     .ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SimpleFun51ArrayPreviousLessTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun51ArrayPreviousLessTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun51ArrayPreviousLess();
+
+            Assert.AreEqual(new int[] {-1, 3, -1, 2, 4}, kata.ArrayPreviousLess(new int[] {3, 5, 2, 4, 5}));
+
+            Assert.AreEqual(new int[] {-1, -1, -1, 1, 3, 4, 4, 1},
+                kata.ArrayPreviousLess(new int[] {2, 2, 1, 3, 4, 5, 5, 3}));
+
+            Assert.AreEqual(new int[] {-1, -1, -1}, kata.ArrayPreviousLess(new int[] {3, 2, 1}));
+        }
+    }
+}
