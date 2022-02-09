8a66cc0910170d54d863a4af6a04fc43fbcac6dd
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-02-09 11:13:19 +0300

    Added solution "Simple Fun #55: Cyclic String"

---
+++ CodeWarsTests/7kyu/SimpleFun55CyclicStringTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun55CyclicStringTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun55CyclicString();
+
+            Assert.AreEqual(3, kata.CyclicString("cabca"));
+
+            Assert.AreEqual(2, kata.CyclicString("aba"));
+
+            Assert.AreEqual(1, kata.CyclicString("ccccccccccc"));
+
+            Assert.AreEqual(4, kata.CyclicString("abaca"));
+        }
+    }
+}
