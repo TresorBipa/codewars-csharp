9653148b084ca614f815ccc85c958f4e7dcac036
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-02-12 20:55:15 +0300

    Added solution "Bouncing Balls"

---
+++ CodeWarsTests/6kyu/BouncingBallsTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class BouncingBallsTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(3, BouncingBalls.bouncingBall(3.0, 0.66, 1.5));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(15, BouncingBalls.bouncingBall(30.0, 0.66, 1.5));
+        }
+    }
+}
