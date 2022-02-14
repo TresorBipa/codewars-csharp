dc56bf4a83ee44966154c39390511524c20fd966
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-02-14 08:24:37 +0300

    Added solution "Simple Fun #61: Crossing Sum"

---
+++ CodeWarsTests/7kyu/SimpleFun61CrossingSumTests.cs
@@ -0,0 +1,35 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun61CrossingSumTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun61CrossingSum();
+
+            var matrix = new int[][]
+            {
+                new int[] {1, 1, 1, 1},
+                new int[] {2, 2, 2, 2},
+                new int[] {3, 3, 3, 3}
+            };
+            Assert.AreEqual(12, kata.CrossingSum(matrix, 1, 3));
+
+            matrix = new int[][]
+            {
+                new int[] {1, 1},
+                new int[] {3, 3},
+                new int[] {1, 1},
+                new int[] {2, 2}
+            };
+            Assert.AreEqual(9, kata.CrossingSum(matrix, 3, 0));
+
+            matrix = new int[][] {new int[] {100}};
+            Assert.AreEqual(100, kata.CrossingSum(matrix, 0, 0));
+        }
+    }
+}
