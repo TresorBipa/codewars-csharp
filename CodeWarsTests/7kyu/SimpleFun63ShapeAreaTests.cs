b5c64d5e4d018c063761ebc546b23bd26ddc15df
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-02-16 14:00:37 +0300

    Added solution "Simple Fun #63: Shape Area"

---
+++ CodeWarsTests/7kyu/SimpleFun63ShapeAreaTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun63ShapeAreaTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun63ShapeArea();
+            Assert.AreEqual(5, kata.ShapeArea(2));
+            Assert.AreEqual(13, kata.ShapeArea(3));
+            Assert.AreEqual(1, kata.ShapeArea(1));
+            Assert.AreEqual(41, kata.ShapeArea(5));
+        }
+    }
+}
