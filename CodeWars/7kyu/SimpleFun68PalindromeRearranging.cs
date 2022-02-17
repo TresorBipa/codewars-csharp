1267a0cd09732434b818aae02d80582e9df43f67
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-02-18 00:36:31 +0300

    Added solution "Simple Fun #68: Palindrome Rearranging"

---
+++ CodeWars/7kyu/SimpleFun68PalindromeRearranging.cs
@@ -0,0 +1,43 @@
+﻿/*
+https://www.codewars.com/kata/5893fa478a8a23ef82000031/csharp
+
+7 kyu
+Simple Fun #68: Palindrome Rearranging
+
+Task
+Given a string s, find out if its characters can be rearranged to form a palindrome.
+
+Example
+For s = "aabb", the output should be true.
+
+We can rearrange "aabb" to make "abba", which is a palindrome.
+
+Input/Output
+[input] string s
+
+A string consisting of lowercase English letters.
+
+Constraints:
+
+4 ≤ inputString.length ≤ 50.
+
+[output] a boolean value
+
+true if the characters of the inputString can be rearranged to form a palindrome, false otherwise.
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SimpleFun68PalindromeRearranging
+    {
+        public bool PalindromeRearranging(string s)
+        {
+            return s.GroupBy(c => c).Sum(g => g.Count() % 2) < 2;
+            //return s.GroupBy(g => g).Count(g => g.Count() % 2 != 0) <= 1;
+            //return s.Distinct().Sum(x => s.Count(y => y == x) % 2 == 1 ? 1 : 0) < 2;
+            //return s.GroupBy(x => x, (k, v) => new {k = k, c = v.Count()}).Count(x => x.c % 2 == 1) < 2;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SimpleFun68PalindromeRearrangingTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun68PalindromeRearrangingTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun68PalindromeRearranging();
+
+            Assert.AreEqual(true, kata.PalindromeRearranging("aabb"));
+
+            Assert.AreEqual(false, kata.PalindromeRearranging("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabc"));
+
+            Assert.AreEqual(true, kata.PalindromeRearranging("abbcabb"));
+
+            Assert.AreEqual(true, kata.PalindromeRearranging("zyyzzzzz"));
+
+            Assert.AreEqual(false, kata.PalindromeRearranging("aaabbb"));
+        }
+    }
+}
