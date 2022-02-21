ea70dc5df48af1ca13cf202e10dd6ced5c8ebb81
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-02-21 21:43:45 +0300

    Added solution "Simple Fun #75: Digit Degree"

---
+++ CodeWars/7kyu/SimpleFun75DigitDegree.cs
@@ -0,0 +1,40 @@
+﻿/*
+https://www.codewars.com/kata/589422431a88082ea600002a/csharp
+
+7 kyu
+Simple Fun #75: Digit Degree
+
+Task
+Let's define digit degree of some positive integer as the number of times we need to replace this number with the sum of its digits until we get to a one digit number.
+
+Given an integer n, find its digit degree.
+
+Example
+For n = 5, the output should be 0;
+
+For n = 100, the output should be 1;
+
+For n = 91, the output should be 2.
+
+Input/Output
+[input] integer n
+
+Constraints: 5 ≤ n ≤ 109.
+
+[output] an integer
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SimpleFun75DigitDegree
+    {
+        public int DigitDegree(int n)
+        {
+            //return n < 10 ? 0 : 1 + DigitDegree((int) n.ToString().Sum(char.GetNumericValue));
+            return n <= 9 ? 0 : 1 + DigitDegree(n.ToString().Sum(c => c - '0'));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/SimpleFun75DigitDegreeTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun75DigitDegreeTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun75DigitDegree();
+
+            Assert.AreEqual(0, kata.DigitDegree(5));
+
+            Assert.AreEqual(1, kata.DigitDegree(100));
+
+            Assert.AreEqual(2, kata.DigitDegree(91));
+
+            Assert.AreEqual(2, kata.DigitDegree(99));
+
+            Assert.AreEqual(3, kata.DigitDegree(1234567));
+
+            Assert.AreEqual(1, kata.DigitDegree(1000000000));
+        }
+    }
+}
