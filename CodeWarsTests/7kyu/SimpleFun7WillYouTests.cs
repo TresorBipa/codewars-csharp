4cc1ccf43d2f942503b0fd3b8b5eb5288967cab8
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-02-22 13:44:52 +0300

    Added solution "Simple Fun #7: Will You?"

---
+++ CodeWarsTests/7kyu/SimpleFun7WillYouTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun7WillYouTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun7WillYou();
+
+            Assert.AreEqual(false, kata.WillYou(true, true, true), "");
+
+            Assert.AreEqual(true, kata.WillYou(true, false, true), "");
+
+            Assert.AreEqual(false, kata.WillYou(false, false, false), "");
+
+            Assert.AreEqual(true, kata.WillYou(false, false, true), "");
+        }
+    }
+}
