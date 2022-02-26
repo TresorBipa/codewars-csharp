8a310a36535c53f203af0af20a73517a4e05176e
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-02-26 16:11:51 +0300

    Added solution "Simple Fun #19: Count Black Cells"

---
+++ CodeWars/6kyu/SimpleFun19CountBlackCells.cs
@@ -0,0 +1,49 @@
+﻿/*
+https://www.codewars.com/kata/588475d575431d0a0e000023/csharp
+
+6 kyu
+Simple Fun #19: Count Black Cells
+
+Task
+Imagine a white rectangular grid of n rows and m columns divided into two parts by a diagonal line running from the upper left to the lower right corner. 
+Now let's paint the grid in two colors according to the following rules:
+
+A cell is painted black if it has at least one point in common with the diagonal;
+Otherwise, a cell is painted white.
+Count the number of cells painted black.
+
+Example
+For n = 3 and m = 4, the output should be 6
+
+There are 6 cells that have at least one common point with the diagonal and therefore are painted black.
+
+For n = 3 and m = 3, the output should be 7
+
+7 cells have at least one common point with the diagonal and are painted black.
+
+Input/Output
+[input] integer n
+
+The number of rows.
+
+Constraints: 1 ≤ n ≤ 10000.
+
+[input] integer m
+
+The number of columns.
+
+Constraints: 1 ≤ m ≤ 10000.
+
+[output] an integer
+
+The number of black cells.
+*/
+
+namespace CodeWars
+{
+    public class SimpleFun19CountBlackCells
+    {
+        private static int Gcd(int a, int b) => b == 0 ? a : Gcd(b, a % b);
+        public static int CountBlackCells(int h, int w) => h + w + Gcd(h, w) - 2;
+    }
+}
---
+++ CodeWarsTests/6kyu/SimpleFun19CountBlackCellsTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleFun19CountBlackCellsTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new SimpleFun19CountBlackCells();
+
+            Assert.AreEqual(6, SimpleFun19CountBlackCells.CountBlackCells(3, 4));
+
+            Assert.AreEqual(7, SimpleFun19CountBlackCells.CountBlackCells(3, 3));
+
+            Assert.AreEqual(6, SimpleFun19CountBlackCells.CountBlackCells(2, 5));
+
+            Assert.AreEqual(1, SimpleFun19CountBlackCells.CountBlackCells(1, 1));
+
+            Assert.AreEqual(2, SimpleFun19CountBlackCells.CountBlackCells(1, 2));
+
+            Assert.AreEqual(239, SimpleFun19CountBlackCells.CountBlackCells(1, 239));
+
+            Assert.AreEqual(86, SimpleFun19CountBlackCells.CountBlackCells(33, 44));
+
+            Assert.AreEqual(30, SimpleFun19CountBlackCells.CountBlackCells(16, 8));
+
+            Assert.AreEqual(17774, SimpleFun19CountBlackCells.CountBlackCells(6666, 8888));
+        }
+    }
+}
