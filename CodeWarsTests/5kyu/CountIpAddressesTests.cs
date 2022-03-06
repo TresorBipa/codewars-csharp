405a097e1b12401e71ccf6a75a021909ae7b230f
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-03-06 16:24:06 +0300

    Added solution "Count IP Addresses"

---
+++ CodeWarsTests/5kyu/CountIpAddressesTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CountIpAddressesTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(50, CountIpAddresses.IpsBetween("10.0.0.0", "10.0.0.50"));
+            Assert.AreEqual(246, CountIpAddresses.IpsBetween("20.0.0.10", "20.0.1.0"));
+        }
+    }
+}
