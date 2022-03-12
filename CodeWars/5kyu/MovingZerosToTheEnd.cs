f5e9be4222970c186b2cb8cc6ebde08faf8507c9
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-03-12 11:09:58 +0300

    Added solution "Moving Zeros To The End"

---
+++ CodeWars/5kyu/MovingZerosToTheEnd.cs
@@ -0,0 +1,25 @@
+﻿/*
+https://www.codewars.com/kata/52597aa56021e91c93000cb0/csharp
+
+5 kyu
+Moving Zeros To The End
+
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+Kata.MoveZeroes(new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1}) => new int[] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class MovingZerosToTheEnd
+    {
+        public static int[] MoveZeroes(int[] arr)
+        {
+            // return arr.OrderBy(0.Equals).ToArray();
+            return arr.OrderBy(i => i == 0).ToArray();
+            // return arr.Where(i => i != 0).Concat(arr.Where(i => i == 0)).ToArray();
+        }
+    }
+}
