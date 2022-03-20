2ed55499849bdf5c6871858901780db5854f0200
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-03-20 16:08:51 +0300

    Added solution "Character with longest consecutive repetition"

---
+++ CodeWars/6kyu/CharacterWithLongestConsecutiveRepetition.cs
@@ -0,0 +1,43 @@
+﻿/*
+https://www.codewars.com/kata/586d6cefbcc21eed7a001155/csharp
+
+6 kyu
+Character with longest consecutive repetition
+
+For a given string s find the character c (or C) with longest consecutive repetition and return:
+
+Tuple<char?, int>(c, l)
+where l (or L) is the length of the repetition. 
+If there are two or more characters with the same l return the first in order of appearance.
+
+For empty string return:
+
+Tuple<char?, int>(null, 0)
+Happy coding! :)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class CharacterWithLongestConsecutiveRepetition
+    {
+        public static Tuple<char?, int> LongestRepetition(string input)
+        {
+            return input.Select((x, i) => new Tuple<char?, int>(x, input[i..].TakeWhile(c => c == x).Count()))
+                       .OrderBy(x => -x.Item2).FirstOrDefault()
+                   ?? new Tuple<char?, int>(null, 0);
+
+
+            // return input
+            //     .Aggregate(" ", (c, f) => c + (c.Last() == f ? "" : " ") + f)
+            //     .Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+            //     .OrderByDescending(x => x.Length)
+            //     .Select(x => new Tuple<char?, int>(x.First(), x.Length))
+            //     .FirstOrDefault() ?? new Tuple<char?, int>(null, 0);
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/CharacterWithLongestConsecutiveRepetitionTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CharacterWithLongestConsecutiveRepetitionTests
+    {
+        [Test]
+        public void LongestAtTheBeginning()
+        {
+            Assert.AreEqual(new Tuple<char?, int>('a', 4),
+                CharacterWithLongestConsecutiveRepetition.LongestRepetition("aaaabb"));
+            Assert.AreEqual(new Tuple<char?, int>('b', 5),
+                CharacterWithLongestConsecutiveRepetition.LongestRepetition("abbbbb"));
+        }
+
+        [Test]
+        public void LongestAtTheEnd()
+        {
+            Assert.AreEqual(new Tuple<char?, int>('a', 4),
+                CharacterWithLongestConsecutiveRepetition.LongestRepetition("bbbaaabaaaa"));
+        }
+
+        [Test]
+        public void LongestInTheMiddle()
+        {
+            Assert.AreEqual(new Tuple<char?, int>('u', 3),
+                CharacterWithLongestConsecutiveRepetition.LongestRepetition("cbdeuuu900"));
+        }
+
+        [Test]
+        public void MultipleLongest()
+        {
+            Assert.AreEqual(new Tuple<char?, int>('a', 2),
+                CharacterWithLongestConsecutiveRepetition.LongestRepetition("aabb"));
+            Assert.AreEqual(new Tuple<char?, int>('b', 1),
+                CharacterWithLongestConsecutiveRepetition.LongestRepetition("ba"));
+        }
+
+        [Test]
+        public void EmptyString()
+        {
+            Assert.AreEqual(new Tuple<char?, int>(null, 0),
+                CharacterWithLongestConsecutiveRepetition.LongestRepetition(""));
+        }
+    }
+}
