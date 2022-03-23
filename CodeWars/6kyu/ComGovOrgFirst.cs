d9ce9628f3154c67e1e4b926fd9c9d3e22c881e1
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-03-23 10:00:47 +0300

    Added solution ""com", "gov", "org" first"

---
+++ CodeWars/6kyu/ComGovOrgFirst.cs
@@ -0,0 +1,88 @@
+﻿/*
+https://www.codewars.com/kata/57f21fcd69e09cb0d2000088/csharp
+
+6 kyu
+"com", "gov", "org" first
+
+Write a code that orders collection of Uris based on it's domain next way that it will returns fisrt Uris with domain "com", "gov", "org" 
+(in alphabetical order of their domains) and then all other Uris ordered in alphabetical order of their domains In addition to that
+
+content of Uri should not be changed,
+other part of Uri doesn't affect sorting. (uris "c.com","b.com","a.com" can be placed in any order inside their group, 
+so both "c.com","b.com","a.com" and "a.com","c.com","b.com" are correct, till they are stand before *.org)
+
+e.g.
+"http://www.google.en/?x=jsdfkj"
+"http://www.google.de/?x=jsdfkj"
+"http://www.google.com/?x=jsdfkj"
+"http://www.google.com/?x=jsdfkj"
+"http://www.google.org/?x=jsdfkj"
+"http://www.google.gov/?x=jsdfkj"
+
+should be sorted into
+"http://www.google.com/?x=jsdfkj"
+"http://www.google.com/?x=jsdfkj"
+"http://www.google.gov/?x=jsdfkj"
+"http://www.google.org/?x=jsdfkj"
+"http://www.google.de/?x=jsdfkj"
+"http://www.google.en/?x=jsdfkj"
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class ComGovOrgFirst
+    {
+        public static IEnumerable<string> OrderByDomain(this IEnumerable<string> source)
+        {
+            return source.OrderBy(x => !(x.Contains(".com") || x.Contains(".gov") || x.Contains(".org"))).ThenBy(x =>
+                x[x.LastIndexOf(".")..]);
+            // return source.OrderByDescending(s => s.Contains(".com") || s.Contains(".gov") || s.Contains(".org")).ThenBy(s => s[s.LastIndexOf(".")..]);
+            // return source.OrderBy(s => (s.Contains(".com") || s.Contains(".gov") || s.Contains(".org")) ? 0 : 1).ThenBy(s => s.Substring(s.LastIndexOf(".")));
+
+            // return source.OrderBy(x =>
+            // {
+            //     var d = x.Split(".").Last().Split("/")[0];
+            //     return d switch
+            //     {
+            //         "com" => "0",
+            //         "gov" => "1",
+            //         "org" => "2",
+            //         _ => d
+            //     };
+            // });
+
+            // static string Key(string input)
+            // {
+            //     var d = input.Split(".").Last().Split("/")[0];
+            //
+            //     return d switch
+            //     {
+            //         "com" => "0",
+            //         "gov" => "1",
+            //         "org" => "2",
+            //         _ => d
+            //     };
+            // }
+
+
+            // return source
+            //     .OrderByDescending(x => x.IndexOf("com"))
+            //     .ThenByDescending(x => x.IndexOf("gov"))
+            //     .ThenByDescending(x => x.IndexOf("org"))
+            //     .ThenBy(x => new System.Uri(x).Host.Split('.').Last());
+
+
+            // var r = source.Select(x => new {Host = new Uri(x).IdnHost, URL = x})
+            //     .OrderBy(x => x.Host)
+            //     .ThenBy(x => !x.Host.EndsWith(".com"))
+            //     .ThenBy(x => !x.Host.EndsWith(".gov"))
+            //     .ThenBy(x => !x.Host.EndsWith(".org")).Select(x => x.URL);
+            //
+            // return r;
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/ComGovOrgFirstTests.cs
@@ -0,0 +1,101 @@
+﻿using CodeWars;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ComGovOrgFirstTests
+    {
+        [Test]
+        public void SimpleTests()
+        {
+            var source = new[]
+            {
+                "http://www.google.en/?x=jsdfkj",
+                "http://www.google.de/?x=jsdfkj",
+                "http://www.google.com/?x=jsdfkj",
+                "http://www.google.com/?x=jsdfkj",
+                "http://www.google.org/?x=jsdfkj",
+                "http://www.google.gov/?x=jsdfkj",
+            };
+            var result = source.OrderByDomain();
+
+            Assert.IsTrue(SortingTaskUtils.AreDomainsEquals(result, source));
+
+            //should be sorted  to
+            //"http://www.google.com/?x=jsdfkj",
+            //"http://www.google.com/?x=jsdfkj",
+            //"http://www.google.gov/?x=jsdfkj",
+            //"http://www.google.org/?x=jsdfkj",
+            //"http://www.google.de/?x=jsdfkj",
+            //"http://www.google.en/?x=jsdfkj",
+        }
+
+        [Test]
+        public void RandomTests()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                var source = SortingTaskUtils.GenerateUris();
+                var result = source.OrderByDomain();
+                Assert.IsTrue(SortingTaskUtils.AreDomainsEquals(result, source));
+            }
+        }
+    }
+
+    public static class SortingTaskUtils
+    {
+        private static IEnumerable<string> OrderDomains(this IEnumerable<string> source)
+        {
+            return source.Select(x => Regex.Match(new Uri(x).Host, @"\.[^.]*$").Value.TrimStart('.'))
+                .OrderBy(x => x, new DomainComparer());
+        }
+
+        public static bool AreDomainsEquals(IEnumerable<string> result, IEnumerable<string> source)
+        {
+            IEnumerable<string> resultDomains;
+            try
+            {
+                resultDomains = result.Select(x =>
+                    Regex.Match(new Uri(x).Host, @"\.[^.]*$")
+                        .Value.TrimStart('.'));
+            }
+            catch (Exception)
+            {
+                throw new Exception("Uri's in result were changed");
+            }
+
+            return resultDomains.SequenceEqual(source.OrderDomains());
+        }
+
+        public static IList<string> GenerateUris()
+        {
+            string[] domains = {"com", "org", "gov", "en", "us", "de", "tv"};
+            var rnd = new Random();
+
+            return Enumerable.Range(0, rnd.Next(50))
+                .Select(i => $"http://{Guid.NewGuid()}.{domains[rnd.Next(7)]}/{Guid.NewGuid()}")
+                .ToList();
+        }
+
+        private class DomainComparer : IComparer<string>
+        {
+            private string[] _main = {"com", "org", "gov"};
+
+            public int Compare(string x, string y)
+            {
+                if (_main.Contains(x) && _main.Contains(y))
+                    return string.Compare(x, y);
+                if (_main.Contains(x))
+                    return -1;
+                if (_main.Contains(y))
+                    return 1;
+                return string.Compare(x, y);
+            }
+        }
+    }
+}
