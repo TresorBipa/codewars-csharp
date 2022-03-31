5a465c803472d0bb768ff637c686d7fd0a260f34
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-03-31 12:18:51 +0300

    Added solution "Data Reverse"
    Added solution "Data Reverse"

---
+++ CodeWars/6kyu/DataReverse.cs
@@ -0,0 +1,42 @@
+﻿/*
+https://www.codewars.com/kata/569d488d61b812a0f7000015
+
+6 kyu
+Data Reverse
+
+A stream of data is received and needs to be reversed.
+
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+
+10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+The total number of bits will always be a multiple of 8.
+
+The data is given in an array as such:
+
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+Note: In the C and NASM languages you are given the third parameter which is the number of segment blocks.
+*/
+
+using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class KataDataReverse
+    {
+        public static int[] DataReverse(int[] data)
+        {
+            return data
+                .Select((v, i) => new { Value = v, Index = i })
+                .GroupBy(x => x.Index / 8)
+                .Reverse()
+                .SelectMany(x => x.Select(g => g.Value))
+                .ToArray();
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/DataReverseTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    internal class KataDataReverseTests
+    {
+
+        [Test]
+        public static void test1()
+        {
+            int[] data1 = new int[32] { 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0 };
+            int[] data2 = new int[32] { 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 };
+            Assert.AreEqual(data2, KataDataReverse.DataReverse(data1));
+        }
+
+        [Test]
+        public static void test2()
+        {
+            int[] data1 = new int[16] { 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1 };
+            int[] data2 = new int[16] { 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0 };
+            Assert.AreEqual(data2, KataDataReverse.DataReverse(data1));
+        }
+    }
+}
