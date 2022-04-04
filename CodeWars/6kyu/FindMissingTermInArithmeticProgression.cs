44566d334eb377b5fafc1496f4e22002c6f8a1bc
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-04-04 10:30:18 +0300

    Added solution "Find the missing term in an Arithmetic Progression"
    Added solution "Find the missing term in an Arithmetic Progression"

---
+++ CodeWars/6kyu/FindMissingTermInArithmeticProgression.cs
@@ -0,0 +1,53 @@
+﻿/*
+https://www.codewars.com/kata/52de553ebb55d1fca3000371/csharp
+
+6 kyu
+Find the missing term in an Arithmetic Progression
+
+An Arithmetic Progression is defined as one in which there is a constant difference between the consecutive terms of a given series of numbers. 
+You are provided with consecutive elements of an Arithmetic Progression. 
+There is however one hitch: 
+exactly one term from the original series is missing from the set of numbers which have been given to you. 
+The rest of the given series is the same as the original AP. 
+Find the missing term.
+
+You have to write a function that receives a list, list size will always be at least 3 numbers. 
+The missing term will never be the first or last one.
+
+Example
+Kata.FindMissing(new List<int> {1, 3, 5, 9, 11}) => 7
+
+PS: This is a sample question of the facebook engineer challenge on interviewstreet. 
+I found it quite fun to solve on paper using math, derive the algo that way. Have fun!
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class FindMissingTermInArithmeticProgression
+    {
+        public static int FindMissing(List<int> list)
+        {
+            var step = (list.Max() - list.Min()) / list.Count;
+            return list.First(i => !list.Contains(i + step)) + step;
+
+
+            // var step = (list.Last() - list.First()) / list.Count;
+            // return list.Select(x => x + step).ToList().Except(list).First();
+
+
+            // return (int)((long)(list[0] + list[^1]) * (list.Count + 1) / 2 - list.Select(x => (long)x).Sum());
+
+
+            // var step = (list.Last() - list.First()) / list.Count;
+            // var arr = new List<int>();
+            // for (int i = list.Min(); i <= list.Max(); i += step)
+            // {
+            //     arr.Add(i);
+            // }
+            // return arr.Except(list).First();
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/FindMissingTermInArithmeticProgressionTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class FindMissingTermInArithmeticProgressionTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(new[] { new List<int> { 1, 3, 5, 9, 11 } }).Returns(7);
+                yield return new TestCaseData(new[] { new List<int> { 0, 5, 10, 20, 25 } }).Returns(15);
+                yield return new TestCaseData(new[] { new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11 } }).Returns(10);
+                yield return new TestCaseData(new[] { new List<int> { 1040, 1220, 1580 } }).Returns(1400);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public int Test(List<int> list) => FindMissingTermInArithmeticProgression.FindMissing(list);
+    }
+}
