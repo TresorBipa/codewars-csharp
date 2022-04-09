b89fb4cc7bbf20d37c47dfb334f4b23026c2888d
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-04-09 10:55:25 +0300

    Added solution "Without the letter 'E'"

---
+++ CodeWars/7kyu/WithoutLetterE.cs
@@ -0,0 +1,34 @@
+﻿/*
+https://www.codewars.com/kata/594b8e182fa0a0d7fc000875/
+
+7 kyu
+Without the letter 'E'
+
+Is it possible to write a book without the letter 'e' ?
+
+Task
+Given String str, return:
+
+How much "e" does it contains (case-insensitive) in string format.
+If given String doesn't contain any "e", return: "There is no "e"."
+If given String is empty, return empty String.
+If given String is `null`/`None`/`nil`, return `null`/`None`/`nil`
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public class WithoutLetterE
+    {
+        public static string FindE(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return s;
+            var count = s.ToLower().Count(x => x == 'e');
+            return count == 0 ? "There is no \"e\"." : count.ToString();
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/WithoutLetterETests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class WithoutLetterETests
+    {
+        private static readonly Random Rand = new Random();
+
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("1", WithoutLetterE.FindE("e"));
+            Assert.AreEqual("2", WithoutLetterE.FindE("ee"));
+            Assert.AreEqual("3", WithoutLetterE.FindE("Eee"));
+            Assert.AreEqual("4", WithoutLetterE.FindE("eeeE"));
+            Assert.AreEqual("7", WithoutLetterE.FindE("EeeEeeE"));
+            Assert.AreEqual("There is no \"e\".", WithoutLetterE.FindE("actual"));
+            Assert.AreEqual("1", WithoutLetterE.FindE("Codewars"));
+            Assert.AreEqual("", WithoutLetterE.FindE(""));
+            Assert.AreEqual(null, WithoutLetterE.FindE(null));
+        }
+
+        [Test]
+        public void GadsbyTest()
+        {
+            string Gadsby1 =
+                "If youth, throughout all history, had had a champion to stand up for it; to show a doubting\n"
+                + "world that a child can think; and, possibly, do it practically; you wouldn’t constantly run\n"
+                + "across folks today who claim that “a child don’t know anything.” A child’s brain starts\n"
+                + "functioning at birth; and has, amongst its many infant convolutions, thousands of dormant\n"
+                + "atoms, into which God has put a mystic possibility for noticing an adult’s act, and figuring\n"
+                + "out its purport.";
+
+            Assert.AreEqual("There is no \"e\".", WithoutLetterE.FindE(Gadsby1));
+
+            string Gadsby2 =
+                "I THINK THAT now you should know this charming Gadsby family; so I will bring forth\n"
+                + "Lady Gadsby, about whom I told you at Gadsby’s inauguration as Mayor; a loyal church\n"
+                + "woman with a vocal ability for choir work; and, with good capability on piano or organ,\n"
+                + "no woman could “fill in” in so many ways; and no woman was so willing, and quick to do\n"
+                + "so. Gadsby had two sons; bright lads and popular with all. Julius was of a studious turn of\n"
+                + "mind, always poring through books of information; caring not what kind of information it\n"
+                + "was, so long as it was information, and not fiction.";
+
+            Assert.AreEqual("There is no \"e\".", WithoutLetterE.FindE(Gadsby2));
+        }
+
+        private static string Solution(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return s;
+            var count = s.ToLower().Count(x => x == 'e');
+            return count == 0 ? "There is no \"e\"." : count.ToString();
+        }
+
+        private static string RandomStr()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 ";
+            return string.Concat(Enumerable.Repeat(chars, Rand.Next(0, chars.Length))
+                .Select(s => s[Rand.Next(s.Length)]));
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            for (var i = 0; i < 200; i++)
+            {
+                var e = RandomStr();
+                var expected = Solution(e);
+                var message = FailureMessage(e, expected);
+
+                var actual = WithoutLetterE.FindE(e);
+
+                Console.WriteLine(message);
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        private static string FailureMessage(string s, string value)
+        {
+            return $"Should return \"{value}\" with \"{s}\"";
+        }
+    }
+}
+
+
