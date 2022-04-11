570ea3f0a5a0076c16589f2db624f992d2054d55
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-04-11 13:48:18 +0300

    Added solution "Get the Excel column title!"

---
+++ CodeWarsTests/6kyu/GetExcelColumnTitleTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GetExcelColumnTitleTests
+    {
+        [Test]
+        public void ExamplesInDescriptionTest()
+        {
+            Assert.AreEqual("A", GetExcelColumnTitle.GetColumnTitle(1));
+            Assert.AreEqual("Z", GetExcelColumnTitle.GetColumnTitle(26));
+            Assert.AreEqual("AZ", GetExcelColumnTitle.GetColumnTitle(52));
+            Assert.AreEqual("BA", GetExcelColumnTitle.GetColumnTitle(53));
+            Assert.AreEqual("ZZ", GetExcelColumnTitle.GetColumnTitle(702));
+            Assert.AreEqual("AYK", GetExcelColumnTitle.GetColumnTitle(1337));
+            Assert.AreEqual("XPEH", GetExcelColumnTitle.GetColumnTitle(432778));
+        }
+
+        [Test]
+        public void zeroInputShouldThrowException()
+        {
+            Assert.Throws<Exception>(() => GetExcelColumnTitle.GetColumnTitle(0));
+        }
+    }
+}
