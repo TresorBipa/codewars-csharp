c146c6d73550741a7d22de1b20ffdde0cd1acdec
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-04-13 12:32:26 +0300

    Added solution "Good vs Evil"

---
+++ CodeWarsTests/6kyu/GoodVsEvilTests.cs
@@ -0,0 +1,30 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class GoodVsEvilTests
+    {
+        [Test]
+        public static void EvilShouldWin()
+        {
+            Assert.AreEqual("Battle Result: Evil eradicates all trace of Good",
+                KataGoodVsEvil.GoodVsEvil("1 1 1 1 1 1", "1 1 1 1 1 1 1"));
+        }
+
+        [Test]
+        public static void GoodShouldTriumph()
+        {
+            Assert.AreEqual("Battle Result: Good triumphs over Evil",
+                KataGoodVsEvil.GoodVsEvil("0 0 0 0 0 10", "0 1 1 1 1 0 0"));
+        }
+
+        [Test]
+        public static void ShouldBeATie()
+        {
+            Assert.AreEqual("Battle Result: No victor on this battle field",
+                KataGoodVsEvil.GoodVsEvil("1 0 0 0 0 0", "1 0 0 0 0 0 0"));
+        }
+    }
+}
