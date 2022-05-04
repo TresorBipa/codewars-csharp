6ce490f5bda74155d160b922b9e4b48e0e6181c4
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-05-04 14:26:19 +0300

    Added solution "Playing with passphrases"

---
+++ CodeWars/6kyu/PlayingWithPassphrases.cs
@@ -0,0 +1,57 @@
+﻿/*
+https://www.codewars.com/kata/559536379512a64472000053/csharp
+
+6 kyu
+Playing with passphrases
+
+Everyone knows passphrases. 
+One can choose passphrases from poems, songs, movies names and so on but frequently they can be guessed due to common cultural references. 
+You can get your passphrases stronger by different means. One is the following:
+
+choose a text in capital letters including or not digits and non alphabetic characters,
+
+shift each letter by a given number but the transformed letter must be a letter (circular shift),
+replace each digit by its complement to 9,
+keep such as non alphabetic and non digit characters,
+downcase each letter in odd position, upcase each letter in even position (the first character is in position 0),
+reverse the whole result.
+Example:
+your text: "BORN IN 2015!", shift 1
+
+1 + 2 + 3 -> "CPSO JO 7984!"
+4 "CpSo jO 7984!"
+5 "!4897 Oj oSpC"
+
+With longer passphrases it's better to have a small and easy program. Would you write it?
+
+https://en.wikipedia.org/wiki/Passphrase
+*/
+
+using System;
+using System.Linq;
+using System.Text;
+
+namespace CodeWars
+{
+    public class PlayingWithPassphrases
+    {
+        public static string playPass(string s, int n)
+        {
+            return string.Concat(s.ToUpper().Select((c, i) =>
+                (char)(char.IsLetter(c) ? (c - 65 + n) % 26 + 65 + i % 2 * 32
+                    : char.IsDigit(c) ? 9 - c + 96 : c)).Reverse());
+
+            // return string.Concat(s.ToUpper().Select((c, i) =>
+            //     (char)(char.IsLetter(c) ? (c - 65 + n) % 26 + (i % 2 == 1 ? 97 : 65) :
+            //         char.IsDigit(c) ? 9 - c + 96 : c)).Reverse());
+
+            // return string.Concat(s.ToUpper()
+            //     .Select(c =>
+            //         char.IsLetter(c) ? (char)((c - 65 + n) % 26 + 65) :
+            //         char.IsDigit(c) ? (char)(9 - c + 96) : c)
+            //     // char.IsDigit(c) ? (char)(9 - (c - '0') + 48) : c)
+            //     .Select((c, i) => i % 2 == 0 ? char.ToUpper(c) : char.ToLower(c))
+            //     .Reverse());
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/PlayingWithPassphrasesTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PlayingWithPassphrasesTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("!!!vPz fWpM J", PlayingWithPassphrases.playPass("I LOVE YOU!!!", 1));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual("4897 NkTrC Hq fT67 GjV Pq aP OqTh gOcE CoPcTi aO",
+                PlayingWithPassphrases.playPass("MY GRANMA CAME FROM NY ON THE 23RD OF APRIL 2015", 2));
+        }
+    }
+}
