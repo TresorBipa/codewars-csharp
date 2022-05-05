a58bfed3c93c9ca1116274900658ad88d84b8e4e
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-05-05 17:36:40 +0300

    Added solution "Prize Draw"

---
+++ CodeWarsTests/6kyu/PrizeDrawTests.cs
@@ -0,0 +1,29 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class PrizeDrawTests
+    {
+        [Test]
+        public static void test1()
+        {
+            string st = "";
+            int[] we = new int[] {4, 2, 1, 4, 3, 1, 2};
+            Assert.AreEqual("No participants", PrizeDraw.NthRank(st, we, 4));
+
+            st = "Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin";
+            we = new int[] {4, 2, 1, 4, 3, 1, 2};
+            Assert.AreEqual("Not enough participants", PrizeDraw.NthRank(st, we, 8));
+
+            st = "Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin";
+            we = new int[] {4, 2, 1, 4, 3, 1, 2};
+            Assert.AreEqual("Benjamin", PrizeDraw.NthRank(st, we, 4));
+
+            st = "Elijah,Chloe,Elizabeth,Matthew,Natalie,Jayden";
+            we = new int[] {1, 3, 5, 5, 3, 6};
+            Assert.AreEqual("Matthew", PrizeDraw.NthRank(st, we, 2));
+        }
+    }
+}
