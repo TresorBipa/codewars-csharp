37abac0c5474a2785caad0e382e38d7ff1182049
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-05-11 23:15:35 +0300

    Added solution "Rectangle into Squares"

---
+++ CodeWarsTests/6kyu/RectangleIntoSquaresTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class RectangleIntoSquaresTests
+    {
+        [Test]
+        public void Test1()
+        {
+            List<int> r = new List<int> {3, 2, 1, 1};
+            Assert.AreEqual(r, RectangleIntoSquares.sqInRect(5, 3));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(null, RectangleIntoSquares.sqInRect(5, 5));
+        }
+    }
+}
