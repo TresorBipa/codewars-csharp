aab7de80f3265a7ca4b1b9a9e1971ea629f3041c
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-05-23 13:32:31 +0300

    Added solution "Simple sum of pairs"

---
+++ CodeWars/6kyu/SimpleSumOfPairs.cs
@@ -0,0 +1,34 @@
+﻿/*
+https://www.codewars.com/kata/5bc027fccd4ec86c840000b7/csharp
+
+6 kyu
+Simple sum of pairs
+
+Given an integer n, find two integers a and b such that:
+
+A) a >= 0 and b >= 0
+B) a + b = n
+C) DigitSum(a) + Digitsum(b) is maximum of all possibilities.  
+You will return the digitSum(a) + digitsum(b).
+
+For example:
+solve(29) = 11. If we take 15 + 14 = 29 and digitSum = 1 + 5 + 1 + 4 = 11. There is no larger outcome.
+n will not exceed 10e10.
+
+More examples in test cases.
+Good luck!
+*/
+
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SimpleSumOfPairs
+    {
+        public static int solve(long n)
+        {
+            var s = n.ToString();
+            return s.Sum(c => c - '0') + (s.Length - 1) * 9 - (s[^1] == '9' ? 9 : 0);
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/SimpleSumOfPairsTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class SimpleSumOfPairsTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(0, SimpleSumOfPairs.solve(0));
+            Assert.AreEqual(1, SimpleSumOfPairs.solve(1));
+            Assert.AreEqual(18, SimpleSumOfPairs.solve(18));
+            Assert.AreEqual(11, SimpleSumOfPairs.solve(29));
+            Assert.AreEqual(33, SimpleSumOfPairs.solve(1140));
+            Assert.AreEqual(68, SimpleSumOfPairs.solve(50000000));
+            Assert.AreEqual(144, SimpleSumOfPairs.solve(15569047737));
+            Assert.AreEqual(116, SimpleSumOfPairs.solve(2452148459));
+        }
+    }
+}
