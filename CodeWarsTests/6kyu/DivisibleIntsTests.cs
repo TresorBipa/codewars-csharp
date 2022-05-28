06b56c80b986c25dd80df06fe44f4bafa3d7091a
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-05-28 10:32:16 +0300

    Added solution "Divisible Ints"

---
+++ CodeWarsTests/6kyu/DivisibleIntsTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DivisibleIntsTests
+    {
+        [Test]
+        public void GenericTest()
+        {
+            Assert.AreEqual(2, DivisibleInts.GetCount(123));
+        }
+
+        [Test]
+        public void ZeroTest()
+        {
+            Assert.AreEqual(5, DivisibleInts.GetCount(1230));
+        }
+
+        [Test]
+        public void SingleDigitTest()
+        {
+            Assert.AreEqual(0, DivisibleInts.GetCount(1));
+        }
+
+        [Test]
+        public void LongIntegerTest()
+        {
+            Assert.AreEqual(25, DivisibleInts.GetCount(1111111111));
+        }
+    }
+}
