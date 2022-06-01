f99b5b5ebf83aa59e3060bdb55a61424d446544d
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-06-01 14:55:21 +0300

    Added solution "String average"

---
+++ CodeWars/6kyu/StringAverage.cs
@@ -0,0 +1,31 @@
+﻿/*
+https://www.codewars.com/kata/5966847f4025872c7d00015b/csharp
+
+6 kyu
+String average
+
+You are given a string of numbers between 0-9.
+Find the average of these numbers and return it as a floored whole number (ie: no decimal places) written out as a string. 
+
+Eg:
+"zero nine five two" -> "four"
+
+If the string is empty or includes a number greater than 9, return "n/a"
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class StringAverage
+    {
+        public static string AverageString(string str)
+        {
+            string[] n = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+            var arr = str.Split();
+            return arr.Any(s => !n.Contains(s)) ? "n/a" : n[(int) arr.Select(x => Array.IndexOf(n, x)).Average()];
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/StringAverageTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class StringAverageTests
+    {
+        [Test]
+        public void SampleTests()
+        {
+            Assert.AreEqual("four", StringAverage.AverageString("zero nine five two"));
+            Assert.AreEqual("three", StringAverage.AverageString("four six two three"));
+            Assert.AreEqual("three", StringAverage.AverageString("one two three four five"));
+            Assert.AreEqual("four", StringAverage.AverageString("five four"));
+            Assert.AreEqual("zero", StringAverage.AverageString("zero zero zero zero zero"));
+            Assert.AreEqual("two", StringAverage.AverageString("one one eight one"));
+            Assert.AreEqual("n/a", StringAverage.AverageString(""));
+        }
+    }
+}
