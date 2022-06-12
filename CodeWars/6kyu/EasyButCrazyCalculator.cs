ddd3d49121a2075e3dc112368f59f3002bd63b1f
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-06-12 13:21:15 +0300

    Added solution "Easy, but Crazy Calculator"

---
+++ CodeWars/6kyu/EasyButCrazyCalculator.cs
@@ -0,0 +1,55 @@
+﻿/*
+https://www.codewars.com/kata/56b71974bd06e679a800090b/csharp
+
+6 kyu
+Easy, but Crazy Calculator
+
+Your task is to implement the Crazy Calculator.
+It's not too difficult, but there are some things you have to consider...
+
+So what to do?
+
+Input: String/Formula which has to be calculated
+
+1. Contains only positive numbers (integers)
+2. Contains only "+","-","*" and "/" as operators, no brackets
+3. Operator priority as usual "*","/" > "+","-"
+4. No tricks, no bad cases, only correct input strings
+5. Consider that the results could be doubles
+6. What is crazy? You have use "*" instead of "/" and "+" instead of "-" and reverse. So 12/2 = 24 and 5-3=8...
+Some easy examples:
+
+Input: "12/2+4*2-5"
+Output: 27
+
+Input: "100*5/2-10"
+Output: 50
+
+Input: "15*2"
+Output: 7.5
+First there are some static tests, later on random tests too...
+
+
+Hope you have fun:-)!
+*/
+
+
+using System;
+using System.Data;
+
+namespace CodeWars._6kyu;
+
+public class EasyButCrazyCalculator
+{
+    public double crazyCalculate(string input)
+    {
+        input = input.Replace("+", "#")
+            .Replace("-", "+")
+            .Replace("#", "-")
+            .Replace("*", "#")
+            .Replace("/", "*")
+            .Replace("#", "/");
+
+        return Convert.ToDouble(new DataTable().Compute(input, null));
+    }
+}
---
+++ CodeWarsTests/6kyu/EasyButCrazyCalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using CodeWars._6kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._6kyu;
+
+[TestFixture]
+public class EasyButCrazyCalculatorTests
+{
+    [Test]
+    public void smile67KataTest_withoutRandom1()
+    {
+        Assert.AreEqual(26, new EasyButCrazyCalculator().crazyCalculate("12+3-17"));
+    }
+
+    [Test]
+    public void smile67KataTest_withoutRandom2()
+    {
+        Assert.AreEqual(3.75, new EasyButCrazyCalculator().crazyCalculate("15*4"));
+    }
+
+    [Test]
+    public void smile67KataTest_withoutRandom3()
+    {
+        Assert.AreEqual(-19, new EasyButCrazyCalculator().crazyCalculate("34*2+18/2"));
+    }
+
+    [Test]
+    public void smile67KataTest_withoutRandom4()
+    {
+        Assert.AreEqual(-3, new EasyButCrazyCalculator().crazyCalculate("12+1+2+3+4+5"));
+    }
+}
