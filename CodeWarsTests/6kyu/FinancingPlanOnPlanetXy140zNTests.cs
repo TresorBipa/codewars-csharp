7845a1884a8365794a325df47362df39b5d3ab1e
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-06-14 11:22:40 +0300

    Added solution "Financing Plan on Planet XY140Z-n"

---
+++ CodeWarsTests/6kyu/FinancingPlanOnPlanetXy140zNTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using CodeWars._6kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._6kyu;
+
+[TestFixture]
+public class FinancingPlanOnPlanetXy140zNTests
+{
+    [Test]
+    public void Test01()
+    {
+        Assert.AreEqual(105, FinancingPlanOnPlanetXy140ZN.finance(5));
+    }
+
+    [Test]
+    public void Test02()
+    {
+        Assert.AreEqual(168, FinancingPlanOnPlanetXy140ZN.finance(6));
+    }
+
+    [Test]
+    public void Test03()
+    {
+        Assert.AreEqual(360, FinancingPlanOnPlanetXy140ZN.finance(8));
+    }
+
+    [Test]
+    public void Test04()
+    {
+        Assert.AreEqual(2040, FinancingPlanOnPlanetXy140ZN.finance(15));
+    }
+}
