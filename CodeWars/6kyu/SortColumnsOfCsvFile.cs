e8d82152fa5b58168198e9dd0e41ae98857464ca
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-06-16 14:14:24 +0300

    Added solution "Sort the columns of a csv-file"

---
+++ CodeWars/6kyu/SortColumnsOfCsvFile.cs
@@ -0,0 +1,57 @@
+﻿/*
+https://www.codewars.com/kata/57f7f71a7b992e699400013f/csharp
+
+6 kyu
+Sort the columns of a csv-file
+
+You get a string with the content of a csv-file. The columns are separated by semicolons.
+The first line contains the names of the columns.
+Write a method that sorts the columns by the names of the columns alphabetically and incasesensitive.
+
+An example:
+
+Before sorting:
+As table (only visualization):
+|myjinxin2015|raulbc777|smile67|Dentzil|SteffenVogel_79|
+|17945       |10091    |10088  |3907   |10132          |
+|2           |12       |13     |48     |11             |
+
+The csv-file:
+myjinxin2015;raulbc777;smile67;Dentzil;SteffenVogel_79\n
+17945;10091;10088;3907;10132\n
+2;12;13;48;11
+
+----------------------------------
+
+After sorting:
+As table (only visualization):
+|Dentzil|myjinxin2015|raulbc777|smile67|SteffenVogel_79|
+|3907   |17945       |10091    |10088  |10132          |
+|48     |2           |12       |13     |11             |
+
+The csv-file:
+Dentzil;myjinxin2015;raulbc777;smile67;SteffenVogel_79\n
+3907;17945;10091;10088;10132\n
+48;2;12;13;11
+
+There is no need for prechecks. 
+You will always get a correct string with more than 1 line und more than 1 row. All columns will have different names.
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+I have created other katas. Have a look if you like coding and challenges.
+*/
+
+
+using System.Linq;
+
+namespace CodeWars._6kyu;
+
+public static class SortColumnsOfCsvFile
+{
+    public static string SortCsvColumns(string csvFileContent)
+    {
+        var table = csvFileContent.Split('\n').Select(s => s.Split(';')).ToArray();
+        var sorted = Enumerable.Range(0, table[0].Length).OrderBy(i => table[0][i]).ToArray();
+        return string.Join('\n', table.Select(s => string.Join(';', s.Select((_, i) => s[sorted[i]]))));
+    }
+}
+
