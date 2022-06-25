d1136066a15c0d56b566e3e85f1afa660a232a34
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-06-25 12:16:31 +0300

    Added solution "Fibonacci, Tribonacci and friends"

---
+++ CodeWars/6kyu/Xbonacci.cs
@@ -0,0 +1,48 @@
+﻿/*
+https://www.codewars.com/kata/556e0fccc392c527f20000c5/csharp
+
+6 kyu
+Fibonacci, Tribonacci and friends
+
+If you have completed the Tribonacci sequence kata, you would know by now that mister Fibonacci has at least a bigger brother. 
+If not, give it a quick look to get how things work.
+
+Well, time to expand the family a little more: think of a Quadribonacci starting with a signature of 4 elements and each following element is the sum of the 4 previous, 
+a Pentabonacci (well Cinquebonacci would probably sound a bit more italian, 
+but it would also sound really awful) with a signature of 5 elements and each following element is the sum of the 5 previous, and so on.
+
+Well, guess what? You have to build a Xbonacci function that takes a signature of X elements - and remember each next element is the sum of the last X elements - and returns the first n elements of the so seeded sequence.
+
+xbonacci {1,1,1,1} 10 = {1,1,1,1,4,7,13,25,49,94}
+xbonacci {0,0,0,0,1} 10 = {0,0,0,0,1,1,2,4,8,16}
+xbonacci {1,0,0,0,0,0,1} 10 = {1,0,0,0,0,0,1,2,3,6}
+xbonacci {1,1} produces the Fibonacci sequence
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class Xbonacci
+    {
+        public double[] xbonacci(double[] signature, int n)
+        {
+            var result = new List<double>(signature);
+            var len = signature.Length;
+
+            // for (var i = len; i < n; i++)
+            // result.Add(result.TakeLast(len).Sum());
+            // result.Add(result.GetRange(i, len).Sum());
+            // result.Add(result.Skip(i - signature.Length).Sum());
+
+            while (result.Count < n)
+                result.Add(result.TakeLast(len).Sum());
+
+
+            return result.Take(n).ToArray();
+            // return result.ToArray()[..n];
+        }
+    }
+}
---
+++ CodeWarsTests/6kyu/XbonacciTests.cs
@@ -0,0 +1,36 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class XbonacciTests
+    {
+        private Xbonacci variabonacci;
+
+        [SetUp]
+        public void SetUp()
+        {
+            variabonacci = new Xbonacci();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            variabonacci = null;
+        }
+
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(new double[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 },
+                variabonacci.xbonacci(new double[] { 0, 1 }, 10));
+            Assert.AreEqual(new double[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 },
+                variabonacci.xbonacci(new double[] { 1, 1 }, 10));
+            Assert.AreEqual(new double[] { 0, 0, 0, 0, 1, 1, 2, 4, 8, 16 },
+                variabonacci.xbonacci(new double[] { 0, 0, 0, 0, 1 }, 10));
+            Assert.AreEqual(new double[] { 1, 0, 0, 0, 0, 0, 1, 2, 3, 6 },
+                variabonacci.xbonacci(new double[] { 1, 0, 0, 0, 0, 0, 1 }, 10));
+        }
+    }
+}
