6b597a5630b72808c9f09f03c5f045a6ae45bc55
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-07-02 14:16:04 +0300

    Add solution "What century is it?"

---
+++ CodeWars/6kyu/WhatCenturyIsIt.cs
@@ -0,0 +1,42 @@
+﻿/*
+https://www.codewars.com/kata/52fb87703c1351ebd200081f/csharp
+
+6 kyu
+What century is it?
+
+Return the century of the input year. 
+The input will always be a 4 digit string, so there is no need for validation.
+
+Examples
+"1999" --> "20th"
+"2011" --> "21st"
+"2154" --> "22nd"
+"2259" --> "23rd"
+"1124" --> "12th"
+"2000" --> "20th"
+*/
+
+namespace CodeWars._6kyu;
+
+public class WhatCenturyIsIt
+{
+    public static string WhatCentury(string year)
+    {
+        // var c = (int.Parse(year) - 1) / 100 + 1;
+        // return $"{c}{c switch {< 20 => "th", _ => (c % 10) switch {1 => "st", 2 => "nd", 3 => "rd", _ => "th"}}}";
+
+
+        var c = (int.Parse(year) - 1) / 100 + 1;
+        return c + c switch
+        {
+            < 20 => "th",
+            _ => (c % 10) switch
+            {
+                1 => "st",
+                2 => "nd",
+                3 => "rd",
+                _ => "th"
+            }
+        };
+    }
+}
---
+++ CodeWarsTests/6kyu/WhatCenturyIsItTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars._6kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._6kyu;
+
+[TestFixture]
+public class WhatCenturyIsItTests
+{
+    [Test]
+    public void BasicTests()
+    {
+        Assert.AreEqual("20th", WhatCenturyIsIt.WhatCentury("1999"),
+            "With input '1999' solution produced wrong output.");
+        Assert.AreEqual("21st", WhatCenturyIsIt.WhatCentury("2011"),
+            "With input '2011' solution produced wrong output.");
+        Assert.AreEqual("22nd", WhatCenturyIsIt.WhatCentury("2154"),
+            "With input '2154' solution produced wrong output.");
+        Assert.AreEqual("23rd", WhatCenturyIsIt.WhatCentury("2259"),
+            "With input '2259' solution produced wrong output.");
+        Assert.AreEqual("13th", WhatCenturyIsIt.WhatCentury("1234"),
+            "With input '1234' solution produced wrong output.");
+        Assert.AreEqual("11th", WhatCenturyIsIt.WhatCentury("1023"),
+            "With input '1023' solution produced wrong output.");
+        Assert.AreEqual("20th", WhatCenturyIsIt.WhatCentury("2000"),
+            "With input '2000' solution produced wrong output.");
+    }
+}
