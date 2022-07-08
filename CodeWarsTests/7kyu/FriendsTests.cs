3d08b7036c0d67119ff5666552e5b1d73a8486d3
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-07-08 13:36:03 +0300

    Add solution "Friends"

---
+++ CodeWarsTests/7kyu/FriendsTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataFriendsTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(0, KataFriends.Friends(0));
+            Assert.AreEqual(0, KataFriends.Friends(1));
+            Assert.AreEqual(0, KataFriends.Friends(2));
+        }
+    }
+}
