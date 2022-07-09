ba203ee3738d1de37d3b99ca80bc0b2da5d0894b
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-07-09 17:35:47 +0300

    Add solution "Friend or Foe?"

---
+++ CodeWars/7kyu/FriendOrFoe.cs
@@ -0,0 +1,32 @@
+﻿/*
+https://www.codewars.com/kata/55b42574ff091733d900002f/csharp
+
+7 kyu
+Friend or Foe?
+
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+Note: keep the original order of the names in the output.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class KataFriendOrFoe
+    {
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            return names.Where(s => s.Length == 4);
+            // return Array.FindAll(names, n => n.Length == 4);
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/FriendOrFoeTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class KataFriendOrFoeTests
+    {
+        [Test]
+        public void Test1()
+        {
+            string[] expected = { "Ryan", "Mark" };
+            string[] names = { "Ryan", "Kieran", "Mark", "Jimmy" };
+            CollectionAssert.AreEqual(expected, KataFriendOrFoe.FriendOrFoe(names));
+        }
+    }
+}
