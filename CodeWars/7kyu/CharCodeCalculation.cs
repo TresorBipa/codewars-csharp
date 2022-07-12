54843df5d37651bcc6cc03d87ceedea5eeb73026
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-07-12 16:14:20 +0300

    Add solution "Char Code Calculation"

---
+++ CodeWars/7kyu/CharCodeCalculation.cs
@@ -0,0 +1,44 @@
+﻿/*
+https://www.codewars.com/kata/57f75cc397d62fc93d000059/csharp
+
+7 kyu
+Char Code Calculation
+
+Given a string, turn each character into its ASCII character code and join them together to create a number - let's call this number total1:
+
+'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+Then replace any incidence of the number 7 with the number 1, and call this number 'total2':
+
+total1 = 656667
+              ^
+total2 = 656661
+              ^
+Then return the difference between the sum of the digits in total1 and total2:
+
+  (6 + 5 + 6 + 6 + 6 + 7)
+- (6 + 5 + 6 + 6 + 6 + 1)
+-------------------------
+                       6
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeWars;
+
+public class CharCodeCalculation
+{
+    public static int Calc(string s)
+    {
+        return string.Concat(s.Select(x => (int) x)).Count(x => x == '7') * 6;
+        // return s.Aggregate("", (a, c) => a + (int) c).Count(c => c == '7') * 6;
+
+        // var total1 = string.Concat(s.Select(x => (int) x));
+        // var total2 = total1.Replace("7", "1");
+        // return total1.Sum(x => x - '0') - total2.Sum(x => x - '0');
+        // return total1.Sum(x => int.Parse(x.ToString())) - total2.Sum(x => int.Parse(x.ToString()));
+    }
+}
---
+++ CodeWarsTests/7kyu/CharCodeCalculationTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests;
+
+[TestFixture]
+public class CharCodeCalculationTests
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.AreEqual(0, CharCodeCalculation.Calc(""));
+        Assert.AreEqual(6, CharCodeCalculation.Calc("abc"));
+        Assert.AreEqual(6, CharCodeCalculation.Calc("ABC"));
+        Assert.AreEqual(6, CharCodeCalculation.Calc("abcdef"));
+        Assert.AreEqual(6, CharCodeCalculation.Calc("ifkhchlhfd"));
+        Assert.AreEqual(6, CharCodeCalculation.Calc("jfmgklf8hglbe"));
+        Assert.AreEqual(12, CharCodeCalculation.Calc("jaam"));
+        Assert.AreEqual(18, CharCodeCalculation.Calc("AFHJD"));
+        Assert.AreEqual(18, CharCodeCalculation.Calc("CodeWars"));
+        Assert.AreEqual(24, CharCodeCalculation.Calc("FVJFVDF"));
+        Assert.AreEqual(24, CharCodeCalculation.Calc("AOUCUAOF"));
+        Assert.AreEqual(30, CharCodeCalculation.Calc("aaaaaddddr"));
+        Assert.AreEqual(36, CharCodeCalculation.Calc("XLdNDcAbUkMnkV"));
+        Assert.AreEqual(42, CharCodeCalculation.Calc("cSuLifxDQkOiypJsxOJSE"));
+        Assert.AreEqual(48, CharCodeCalculation.Calc("ZHAUnSGoIbgPSezhjePIbHFrHUOv"));
+        Assert.AreEqual(54, CharCodeCalculation.Calc("VFhSMbZETZVHxYiiYsBMrWuecDN"));
+        Assert.AreEqual(60, CharCodeCalculation.Calc("sphPoGbicTCLbiuUcwFMEGaFmy"));
+        Assert.AreEqual(72, CharCodeCalculation.Calc("oXoQKiCflHIHFyGizCYCuaHhX"));
+        Assert.AreEqual(84, CharCodeCalculation.Calc("sUuPmNnnJOOCAGOuyzmcHQGJhXHYgZLY"));
+    }
+
+    private static readonly Random Rand = new();
+
+    [Test]
+    public void RandomTest()
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            var s = RandomStr();
+
+            var total1 = string.Concat(s.Select(x => (int) x));
+            var total2 = total1.Replace("7", "1");
+            var expected = total1.Sum(x => int.Parse(x.ToString())) - total2.Sum(x => int.Parse(x.ToString()));
+
+            var message = FailureMessage(s, expected);
+            var actual = CharCodeCalculation.Calc(s);
+
+            Assert.AreEqual(expected, actual, message);
+        }
+    }
+
+    private static string RandomStr()
+    {
+        const string chars = "abcdefghijklmnopqrsyuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var result = string.Concat(Enumerable.Repeat(chars, Rand.Next(1, chars.Length))
+            .Select(s => s[Rand.Next(s.Length)]).Take(Rand.Next(0, 8)));
+
+        return result;
+    }
+
+    private static string FailureMessage(string s, int value)
+    {
+        return $"Should return {value} with s=\"{s}\"";
+    }
+}
