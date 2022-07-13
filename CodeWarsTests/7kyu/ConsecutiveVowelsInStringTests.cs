c9728d68b56770302bc5eb744bc4326c99161a93
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-07-13 14:06:44 +0300

    Add solution "Consecutive Vowels in a String"

---
+++ CodeWarsTests/7kyu/ConsecutiveVowelsInStringTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests;
+
+[TestFixture]
+public class ConsecutiveVowelsInStringTests
+{
+    [Test]
+    public void SampleTests()
+    {
+        Assertion(0, string.Empty);
+        Assertion(6, "agrtertyfikfmroyrntbvsukldkfa");
+        Assertion(4, "erfaiekjudhyfimngukduo");
+        Assertion(7, "akfheujfkgiaaaofmmfkdfuaiiie");
+        Assertion(0, "eiknfhjrytueiouesxdczbeuiuoimnmfhfiuou");
+        Assertion(16,
+            "desrehakkjfuteknvfiyrtfbehjdjrobchrunbcbbhdhehbvudjsnanbakkjndhfjenfndinmfnbfondndendnfudnfnanndhdemdmcnfdemnfjimdfofnmfnfjanmdnhdua");
+        Assertion(11,
+            "sudnfhrakekdhhfkakjdjdhvneidkvnudntomcnnamjemdmfudkfhjamvcjedkfdnridnmnbvfhbdjdidncbvchencchdjdodncvchfndnrnencncnffduncbhjdfja");
+        Assertion(11, "jurjfdleiifjriisiouajjfyhekkfjvnnmsuimsnvyuhvcodnmfnsumvbjshhsadkvhfeixoua");
+        Assertion(3, "jrknjffuolgtahamhaevbgeosreljtbhuiaamxpyxdmyb");
+        Assertion(6,
+            "sowqapfqbrjpygtmmulybwusgjijbrnchgkxiaazkcniaxllmupjcmkxwcsmzdaessgkkgzctmbchladughtkitcognxvjwiiiltkuebrwemopvxqpeurfsfgjmeuzgvhjnssbhwkfbejzygbytipjtdcyscwofrpwshiakbdnqfioxqulohdlnmmmfpfrriwpwbddpj");
+    }
+
+    private static void Assertion(int expected, string input) =>
+        Assert.AreEqual(
+            expected,
+            ConsecutiveVowelsInString.GetTheVowels(input),
+            $"\n  Word: \"{input}\""
+        );
+}
