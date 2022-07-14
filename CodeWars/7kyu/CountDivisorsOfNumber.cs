d38164bfc5f477990292d1c8e74e6f04d35de72e
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-07-14 13:26:03 +0300

    Add solution "Count the divisors of a number"

---
+++ CodeWars/7kyu/CountDivisorsOfNumber.cs
@@ -0,0 +1,39 @@
+﻿/*
+https://www.codewars.com/kata/542c0f198e077084c0000c2e/csharp
+
+7 kyu
+Count the divisors of a number
+
+Count the number of divisors of a non-negative integer n.
+
+Random tests go up to n = 500000.
+
+Examples
+Kata.Divisors(4)  -> 3 -- 1, 2, 4
+Kata.Divisors(5)  -> 2 -- 1, 5
+Kata.Divisors(12) -> 6 -- 1, 2, 3, 4, 6, 12
+Kata.Divisors(30) -> 8 -- 1, 2, 3, 5, 6, 10, 15, 30
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class CountDivisorsOfNumber
+    {
+        public static int Divisors(int n)
+        {
+            return Enumerable.Range(1, n).Count(i => n % i == 0);
+
+
+            // var div = 0;
+            // for (var i = 1; i <= n; i++)
+            //     if (n % i == 0)
+            //         div++;
+            //
+            // return div;
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/CountDivisorsOfNumberTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CountDivisorsOfNumberTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(1, CountDivisorsOfNumber.Divisors(1));
+            Assert.AreEqual(4, CountDivisorsOfNumber.Divisors(10));
+            Assert.AreEqual(2, CountDivisorsOfNumber.Divisors(11));
+            Assert.AreEqual(8, CountDivisorsOfNumber.Divisors(54));
+        }
+    }
+}
