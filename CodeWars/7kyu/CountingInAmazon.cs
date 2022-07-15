8ec4366b5f9a676fcbbb628c26a0836fd41ad750
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-07-15 09:14:19 +0300

    Add solution "Counting in the Amazon"

---
+++ CodeWars/7kyu/CountingInAmazon.cs
@@ -0,0 +1,42 @@
+﻿/*
+https://www.codewars.com/kata/55b95c76e08bd5eef100001e/csharp
+
+7 kyu
+Counting in the Amazon
+
+The Arara are an isolated tribe found in the Amazon who count in pairs. For example one to eight is as follows:
+
+1 = anane
+2 = adak
+3 = adak anane
+4 = adak adak
+5 = adak adak anane
+6 = adak adak adak
+7 = adak adak adak anane
+8 = adak adak adak adak
+
+Take a given number and return the Arara's equivalent.
+
+e.g.
+
+Kata.CountArara(3) // "adak anane"
+
+Kata.CountArara(8) // "adak adak adak adak"
+https://en.wikipedia.org/wiki/Arara_people
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class CountingInAmazon
+    {
+        public static string CountArara(int n)
+        {
+            return string.Concat(Enumerable.Repeat("adak ", (n - 1) / 2)) + (n % 2 == 1 ? "anane" : "adak");
+            // return string.Join(" ", Enumerable.Repeat("adak", n / 2).Concat(Enumerable.Repeat("anane", n % 2)));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/CountingInAmazonTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class CountingInAmazonTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("anane", CountingInAmazon.CountArara(1));
+            Assert.AreEqual("adak anane", CountingInAmazon.CountArara(3));
+            Assert.AreEqual("adak adak adak adak", CountingInAmazon.CountArara(8));
+        }
+    }
+}
