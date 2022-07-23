a958a3a7477a81f9aabe39c6bf3683940d9857da
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-07-23 17:26:37 +0300

    Update solution "Switch it Up!"

--- CodeWars/8kyu/SwitchItUp.cs
+++ CodeWars/8kyu/SwitchItUp.cs
@@ -18,23 +18,21 @@ namespace CodeWars
 {
     public class KataSwitchItUp
     {
-        public static string SwitchItUp(int number)
-        {
-            switch (number)
+        public static string SwitchItUp(int number) =>
+            number switch
             {
-                case 0: return "Zero";
-                case 1: return "One";
-                case 2: return "Two";
-                case 3: return "Three";
-                case 4: return "Four";
-                case 5: return "Five";
-                case 6: return "Six";
-                case 7: return "Seven";
-                case 8: return "Eight";
-                case 9: return "Nine";
-                default: return "";
-            }
-        }
+                0 => "Zero",
+                1 => "One",
+                2 => "Two",
+                3 => "Three",
+                4 => "Four",
+                5 => "Five",
+                6 => "Six",
+                7 => "Seven",
+                8 => "Eight",
+                9 => "Nine",
+                _ => ""
+            };
 
 
         //public static string SwitchItUp(int number)
