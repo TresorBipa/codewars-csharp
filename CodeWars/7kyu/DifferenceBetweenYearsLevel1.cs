a6a2c2716543b351af119a04087ae22372256f5e
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-07-29 12:38:15 +0300

    Add solution "Difference between years. (Level 1)"

---
+++ CodeWars/7kyu/DifferenceBetweenYearsLevel1.cs
@@ -0,0 +1,44 @@
+﻿/*
+https://www.codewars.com/kata/588f5a38ec641b411200005b
+
+7 kyu
+Difference between years. (Level 1)
+
+Write a function that receives two strings as parameter. 
+This strings are in the following format of date: YYYY/MM/DD. 
+Your job is: Take the years and calculate the difference between them.
+
+Examples:
+'1997/10/10' and '2015/10/10' -> 2015 - 1997 = returns 18 
+'2015/10/10' and '1997/10/10' -> 2015 - 1997 = returns 18
+At this level, you don't need validate months and days to calculate the difference.
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CodeWars
+{
+    public class DifferenceBetweenYearsLevel1
+    {
+        public static int HowManyYears(string date1, string date2)
+        {
+            return Math.Abs(DateTime.Parse(date1).Year - DateTime.Parse(date2).Year);
+
+
+            // var d1 = DateTime.Parse(date1).Year;
+            // var d2 = DateTime.Parse(date2).Year;
+            // if (d1 > d2)
+            // {
+            //     return d1 - d2;
+            // }
+            //
+            // return d2 - d1;
+        }
+    }
+}
+
+
---
+++ CodeWarsTests/7kyu/DifferenceBetweenYearsLevel1Tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Globalization;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DifferenceBetweenYearsLevel1Tests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(18, DifferenceBetweenYearsLevel1.HowManyYears("1997/10/10", "2015/10/10"));
+            Assert.AreEqual(25, DifferenceBetweenYearsLevel1.HowManyYears("1990/10/10", "2015/10/10"));
+            Assert.AreEqual(25, DifferenceBetweenYearsLevel1.HowManyYears("2015/10/10", "1990/10/10"));
+            Assert.AreEqual(23, DifferenceBetweenYearsLevel1.HowManyYears("1992/10/24", "2015/10/24"));
+            Assert.AreEqual(18, DifferenceBetweenYearsLevel1.HowManyYears("2018/10/10", "2000/10/10"));
+        }
+
+        private static int Solution(string date1, string date2)
+        {
+            return Math.Abs(DateTime.Parse(date1).Year - DateTime.Parse(date2).Year);
+        }
+
+        private static readonly Random Rand = new Random();
+
+        private static string RandomDateTime()
+        {
+            return new DateTime().AddYears(Rand.Next(1, 2100)).AddMonths(Rand.Next(1, 12)).AddDays(Rand.Next(1, 31))
+                .ToString("yyyy/MM/dd", CultureInfo.InvariantCulture);
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            for (var i = 0; i < 200; i++)
+            {
+                var dt1 = RandomDateTime();
+                var dt2 = RandomDateTime();
+                var expected = Solution(dt1, dt2);
+                var message = FailureMessage(dt1, dt2, expected);
+                var actual = DifferenceBetweenYearsLevel1.HowManyYears(dt1, dt2);
+                // Console.WriteLine(message);
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        private static string FailureMessage(string dt1, string dt2, int value)
+        {
+            return $"Should return {value} with \"{dt1}\" and \"{dt2}\"";
+        }
+    }
+}
