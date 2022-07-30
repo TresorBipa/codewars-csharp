dbf890b707801d393c936d28dd623c746772cb29
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-07-30 16:38:30 +0300

    Add solution "Disemvowel Trolls"

---
+++ CodeWars/7kyu/DisemvowelTrolls.cs
@@ -0,0 +1,35 @@
+﻿/*
+https://www.codewars.com/kata/52fba66badcd10859f00097e/csharp
+
+7 kyu
+Disemvowel Trolls
+
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public class DisemvowelTrolls
+    {
+        public static string Disemvowel(string str)
+        {
+            return Regex.Replace(str, "(?i)[aeiou]", "");
+            // return Regex.Replace(str, "[aeiouAEIOU]", "");
+            // return Regex.Replace(str,"[aeiou]", "", RegexOptions.IgnoreCase);
+            // return string.Concat(str.Where(c => !"aeiouAEIOU".Contains(c)));
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/DisemvowelTrollsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class DisemvowelTrollsTests
+    {
+        [Test]
+        public void ShouldRemoveAllVowels()
+        {
+            Assert.AreEqual("Ths wbst s fr lsrs LL!", DisemvowelTrolls.Disemvowel("This website is for losers LOL!"));
+        }
+
+        [Test]
+        public void MultilineString()
+        {
+            Assert.AreEqual("N ffns bt,\nYr wrtng s mng th wrst 'v vr rd",
+                DisemvowelTrolls.Disemvowel("No offense but,\nYour writing is among the worst I've ever read"));
+        }
+
+        [Test]
+        public void OneMoreForGoodMeasure()
+        {
+            Assert.AreEqual("Wht r y,  cmmnst?", DisemvowelTrolls.Disemvowel("What are you, a communist?"));
+        }
+    }
+}
