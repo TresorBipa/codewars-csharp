be2e6178b6124c7cf1a7636697532dc23b38af3f
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-07-31 14:40:22 +0300

    Add solution "Divide and Conquer"

---
+++ CodeWars/7kyu/DivideAndConquer.cs
@@ -0,0 +1,54 @@
+﻿/*
+https://www.codewars.com/kata/57eaec5608fed543d6000021/csharp
+
+7 kyu
+Divide and Conquer
+
+Given a mixed array of number and string representations of integers, 
+add up the string integers and subtract this from the total of the non-string integers.
+
+Return as a number.
+*/
+
+
+using System.Linq;
+
+namespace CodeWars;
+
+public class DivideAndConquer
+{
+    public static int DivCon(object[] objArray)
+    {
+        return objArray.Sum(o => (o is int ? 1 : -1) * int.Parse(o.ToString()));
+
+        // var s = 0;
+        // foreach (var o in objArray)
+        // {
+        //     if (o is int)
+        //     {
+        //         s += int.Parse(o.ToString()!);
+        //     }
+        //     else
+        //     {
+        //         s -= int.Parse(o.ToString()!);
+        //     }
+        // }
+        //
+        // return s;
+
+
+        // foreach (var o in obj)
+        // {
+        //     if (o.GetType() == typeof(int))
+        //     {
+        //         s += int.Parse(o.ToString()!);
+        //     }
+        //     else
+        //     {
+        //         s -= int.Parse(o.ToString()!);
+        //     }
+        // }
+        //
+        // return s;
+    }
+}
---
+++ CodeWarsTests/7kyu/DivideAndConquerTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests;
+
+[TestFixture]
+public class DivideAndConquerTests
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.AreEqual(2, DivideAndConquer.DivCon(new object[] {9, 3, "7", "3"}));
+        Assert.AreEqual(14, DivideAndConquer.DivCon(new object[] {"5", "0", 9, 3, 2, 1, "9", 6, 7}));
+        Assert.AreEqual(13, DivideAndConquer.DivCon(new object[] {"3", 6, 6, 0, "5", 8, 5, "6", 2, "0"}));
+        Assert.AreEqual(11, DivideAndConquer.DivCon(new object[] {"1", "5", "8", 8, 9, 9, 2, "3"}));
+        Assert.AreEqual(61, DivideAndConquer.DivCon(new object[] {8, 0, 0, 8, 5, 7, 2, 3, 7, 8, 6, 7}));
+        Assert.AreEqual(-6, DivideAndConquer.DivCon(new object[] {"0", "1", "2", "3"}));
+        Assert.AreEqual(6, DivideAndConquer.DivCon(new object[] {0, 1, 2, 3}));
+        Assert.AreEqual(0, DivideAndConquer.DivCon(new object[] {1, "1"}));
+        Assert.AreEqual(-2, DivideAndConquer.DivCon(new object[] {-1, "1"}));
+        Assert.AreEqual(2, DivideAndConquer.DivCon(new object[] {1, "-1"}));
+        Assert.AreEqual(1, DivideAndConquer.DivCon(new object[] {1}));
+        Assert.AreEqual(-1, DivideAndConquer.DivCon(new object[] {"1"}));
+        Assert.AreEqual(0, DivideAndConquer.DivCon(new object[] { }));
+    }
+
+    private static readonly Random Rand = new();
+
+    [Test]
+    public void RandomTest()
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            var objArray = RandomObjectArray();
+
+            var s = 0;
+            foreach (var o in objArray)
+            {
+                if (o is int)
+                    s += int.Parse(o.ToString()!);
+                else
+                    s -= int.Parse(o.ToString()!);
+            }
+
+            var expected = s;
+            var actual = DivideAndConquer.DivCon(objArray);
+            var message = FailureMessage(objArray, expected);
+
+            Assert.AreEqual(expected, actual, message);
+        }
+    }
+
+    private static object[] RandomObjectArray()
+    {
+        return Enumerable.Range(0, Rand.Next(0, 11))
+            .Select(x => Rand.Next(0, 10))
+            .Select(x => (object) (Rand.Next(0, 3) == 0 ? x.ToString() : x)).ToArray();
+    }
+
+    private static string FailureMessage(object[] objArray, int value)
+    {
+        var strArray = "";
+        foreach (var o in objArray)
+        {
+            if (o is int)
+                strArray += $"{o},";
+            else
+                strArray += $"\"{o}\",";
+        }
+
+        strArray = strArray.TrimEnd(',');
+        return $"Should return {value} with objArray=[{strArray}]";
+    }
+}
