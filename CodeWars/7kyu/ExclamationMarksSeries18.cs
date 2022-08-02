5c26a382cc65c6bdf07f02a892fc476beab84b2c
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-08-02 09:42:24 +0300

    Add solution "Exclamation marks series #18"

---
+++ CodeWars/7kyu/ExclamationMarksSeries18.cs
@@ -0,0 +1,73 @@
+﻿/*
+https://www.codewars.com/kata/57fb4b289610ce39f70000de/csharp
+
+7 kyu
+Exclamation marks series #18: a simple slot machine that only contains exclamation marks and question marks
+
+Description:
+You are playing a simple slot machine that only contains exclamation marks and question marks. 
+Every time the slot machine is started, a string of 5 length is obtained. 
+If you're lucky enough to get a Special permutation, you'll win the bonus. 
+Give you a string s, return the highest bonus.
+
+Bouns list:
+
+Five-of-a-Kind:   ---- 1000
+"!!!!!" or "?????"
+
+Four-of-a-Kind:    ---- 800
+The string contains a "!!!!" or "????"
+"!!!!?","?!!!!","????!","!????"
+
+Full House:         ----500
+such as "!!!??" or "???!!"...
+
+Three-of-a-Kind:    ---- 300
+The string contains a "!!!" or "???"
+such as "!!!?!","!???!"...
+
+Two pair:           ---- 200
+The string contains two "!!" or "??"
+such as "??!!?","!!?!!"
+
+A Pair:             ---- 100
+The string contains a "!!" or "??"
+such as "?!??!","!!?!?"
+
+Others              ---- 0
+such as "?!?!?","!?!?!"
+Examples
+slot("!!!!!") === 1000
+slot("!!!!?") === 800
+slot("!!!??") === 500
+slot("!!!?!") === 300
+slot("!!?!!") === 200
+slot("!!?!?") === 100
+slot("!?!?!") === 0
+*/
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public class ExclamationMarksSeries18
+    {
+        public static int Slot(string s)
+        {
+            // var m = Regex.Matches(s, @"!+|\?+").Select(x => x.Value).OrderBy(x => -x.Length).ToArray();
+            var m = Regex.Matches(s, @"!+|\?+").OrderBy(x => -x.Length).ToArray();
+
+            return m.Length switch
+            {
+                1 => 1000,
+                2 => m[0].Length == 4 ? 800 : 500,
+                3 => m[0].Length == 3 ? 300 : 200,
+                4 => 100,
+                _ => 0
+            };
+        }
+    }
+}
---
+++ CodeWarsTests/7kyu/ExclamationMarksSeries18Tests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    public class ExclamationMarksSeries18Tests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(1000, ExclamationMarksSeries18.Slot("!!!!!"));
+            Assert.AreEqual(1000, ExclamationMarksSeries18.Slot("?????"));
+            Assert.AreEqual(800, ExclamationMarksSeries18.Slot("!!!!?"));
+            Assert.AreEqual(800, ExclamationMarksSeries18.Slot("????!"));
+            Assert.AreEqual(500, ExclamationMarksSeries18.Slot("!!!??"));
+            Assert.AreEqual(500, ExclamationMarksSeries18.Slot("???!!"));
+            Assert.AreEqual(300, ExclamationMarksSeries18.Slot("!!!?!"));
+            Assert.AreEqual(300, ExclamationMarksSeries18.Slot("???!?"));
+            Assert.AreEqual(200, ExclamationMarksSeries18.Slot("!!?!!"));
+            Assert.AreEqual(200, ExclamationMarksSeries18.Slot("??!??"));
+            Assert.AreEqual(100, ExclamationMarksSeries18.Slot("!!?!?"));
+            Assert.AreEqual(100, ExclamationMarksSeries18.Slot("??!?!"));
+            Assert.AreEqual(0, ExclamationMarksSeries18.Slot("!?!?!"));
+            Assert.AreEqual(0, ExclamationMarksSeries18.Slot("?!?!?"));
+        }
+
+        private static int Solution(string s)
+        {
+            // var m = Regex.Matches(s, @"!+|\?+").Select(x => x.Value).OrderBy(x => -x.Length).ToArray();
+            var m = Regex.Matches(s, @"!+|\?+").OrderBy(x => -x.Length).ToArray();
+
+            return m.Length switch
+            {
+                1 => 1000,
+                2 => m[0].Length == 4 ? 800 : 500,
+                3 => m[0].Length == 3 ? 300 : 200,
+                4 => 100,
+                5 => 0,
+                _ => 0
+            };
+        }
+
+        private static readonly Random Rand = new Random();
+
+        private static string RandomStr()
+        {
+            return string.Concat(Enumerable.Range(0, 5).Select(x => new[] { "!", "?" }[Rand.Next(0, 2)]).ToArray());
+        }
+
+        [Test]
+        public void RandomTest()
+        {
+            for (var i = 0; i < 300; i++)
+            {
+                var str = RandomStr();
+                var expected = Solution(str);
+                var message = FailureMessage(str, expected);
+                var actual = ExclamationMarksSeries18.Slot(str);
+                Console.WriteLine(message);
+                Assert.AreEqual(expected, actual, message);
+            }
+        }
+
+        private static string FailureMessage(string str, int value)
+        {
+            return $"Should return {value} with \"{str}\"";
+        }
+    }
+}
