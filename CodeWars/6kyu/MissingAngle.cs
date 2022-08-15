5663d29d7384314707bf298275d465cfe2987a75
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-08-15 17:10:10 +0300

    Add solution "Missing Angle"

---
+++ CodeWars/6kyu/MissingAngle.cs
@@ -0,0 +1,41 @@
+﻿/*
+https://www.codewars.com/kata/58417e9ab9c25c774500001f/csharp
+
+6 kyu
+Missing Angle
+
+Below is a right-angled triangle:
+
+  |\
+  | \
+  |  \
+  |   \ 
+o |    \ h 
+  |     \
+  |    θ \
+  |_______\ 
+     a
+
+Your challange is to write a function (missingAngle in C/C#, missing_angle in Ruby), 
+that calculates the angle θ in degrees to the nearest integer. 
+
+You will be given three arguments representing each side: 
+o, h and a. One of the arguments equals zero. Use the length of the two other sides to calculate θ. 
+
+You will not be expected to handle any erronous data in your solution.
+*/
+
+
+using System;
+
+namespace CodeWars;
+
+public static class MissingAngle
+{
+    public static double missingAngle(double h, double a, double o)
+    {
+        var theta = h == 0 ? Math.Atan(o / a) : a == 0 ? Math.Asin(o / h) : Math.Acos(a / h);
+        return Math.Round(theta * 180 / Math.PI);
+    }
+}
+
---
+++ CodeWarsTests/6kyu/MissingAngleTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests;
+
+[TestFixture]
+public class MissingAngleTests
+{
+    [Test]
+    public void ExampleTests()
+    {
+        Assert.AreEqual(37, MissingAngle.missingAngle(0, 400, 300));
+        Assert.AreEqual(37, MissingAngle.missingAngle(5, 4, 0));
+        Assert.AreEqual(37, MissingAngle.missingAngle(5, 4, 0));
+        Assert.AreEqual(39, MissingAngle.missingAngle(8, 0, 5));
+        Assert.AreEqual(39, MissingAngle.missingAngle(8, 0, 5));
+        Assert.AreEqual(47, MissingAngle.missingAngle(16.7, 0, 12.3));
+        Assert.AreEqual(44, MissingAngle.missingAngle(7, 5, 0));
+    }
+}
