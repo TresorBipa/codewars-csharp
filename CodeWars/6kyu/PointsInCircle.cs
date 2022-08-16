bb4ce54d33c8e85d4bcef96edda4773532ffb8c3
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-08-16 13:30:19 +0300

    Add solution "Points in the circle"

---
+++ CodeWars/6kyu/PointsInCircle.cs
@@ -0,0 +1,35 @@
+﻿/*
+https://www.codewars.com/kata/5b55c49d4a317adff500015f/csharp
+
+6 kyu
+Points in the circle
+
+You have the radius of a circle with the center in point (0,0).
+
+Write a function that calculates the number of points in the circle where (x,y) - the cartesian coordinates of the points - are integers.
+
+Example: for radius = 2 the result should be 13.
+
+0 <= radius <= 1000
+*/
+
+
+using System;
+
+namespace CodeWars._6kyu;
+
+/// <summary>
+/// https://www.codewars.com/kata/5b55c49d4a317adff500015f/train/csharp
+/// 6 kyu
+/// Points in the circle
+/// </summary>
+public static class PointsInCircle
+{
+    public static int pointsNumber(int radius)
+    {
+        var count = 0;
+        for (var x = 0; x <= radius; x++)
+            count += (int) Math.Sqrt(radius * radius - x * x);
+        return 4 * count + 1;
+    }
+}
---
+++ CodeWarsTests/6kyu/PointsInCircleTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWars._6kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._6kyu;
+
+[TestFixture]
+public class PointsInCircleTests
+{
+    [Test]
+    public void BasicTests()
+    {
+        Assert.AreEqual(5, PointsInCircle.pointsNumber(1));
+        Assert.AreEqual(13, PointsInCircle.pointsNumber(2));
+        Assert.AreEqual(29, PointsInCircle.pointsNumber(3));
+        Assert.AreEqual(81, PointsInCircle.pointsNumber(5));
+        Assert.AreEqual(3141549, PointsInCircle.pointsNumber(1000));
+    }
+}
