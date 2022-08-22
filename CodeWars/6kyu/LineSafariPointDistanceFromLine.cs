5b4d856483e2807701f463c4ba1ca3881e1e1caf
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-08-22 15:49:57 +0300

    Add solution "Line Safari : Point distance from a line"

---
+++ CodeWars/6kyu/LineSafariPointDistanceFromLine.cs
@@ -0,0 +1,43 @@
+﻿/*
+https://www.codewars.com/kata/59c053f070a3b7d19100007e/csharp
+
+6 kyu
+Line Safari : Point distance from a line
+
+Kata Task
+Given 3 points a, b, c
+
+            c
+            
+                     b         
+  a
+
+Find the shortest distance from point c to a straight line that passes through points a and b
+
+Notes
+
+all points are of the form [x,y] where x >= 0 and y >= 0
+if a and b are the same then just return distance between a and c
+use Euclidean distance
+*/
+
+
+using System;
+
+namespace CodeWars;
+
+public static class LineSafariPointDistanceFromLine
+{
+    public static double DistanceFromLine((int, int) a, (int, int) b, (int, int) c)
+    {
+        var (ax, ay) = a;
+        var (bx, by) = b;
+        var (cx, cy) = c;
+
+        return a == b
+            ? Math.Sqrt(Math.Pow(cx - ax, 2) + Math.Pow(cy - ay, 2))
+            : Math.Abs((cx - ax) * (by - ay) - (cy - ay) * (bx - ax)) /
+              Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2));
+    }
+}
+
---
+++ CodeWarsTests/6kyu/LineSafariPointDistanceFromLineTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests;
+
+[TestFixture]
+public class LineSafariPointDistanceFromLineTests
+{
+    [Test]
+    public void Example()
+    {
+        (int, int) a, b, c;
+
+        a = (10, 10);
+        c = (20, 25);
+        b = (30, 10);
+        Assert.AreEqual(15d, LineSafariPointDistanceFromLine.DistanceFromLine(a, b, c), 0.001);
+
+        a = (10, 10);
+        c = (40, 40);
+        b = (70, 70);
+        Assert.AreEqual(0d, LineSafariPointDistanceFromLine.DistanceFromLine(a, b, c), 0.001);
+    }
+}
