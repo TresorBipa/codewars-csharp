94074b6a6d6756a7ac808e9e99b2b9314da5a6e7
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-08-29 16:39:48 +0300

    Add solution "Ghostbusters (whitespace removal)"

---
+++ CodeWars/7kyu/GhostbustersWhitespaceRemoval.cs
@@ -0,0 +1,54 @@
+﻿/*
+https://www.codewars.com/kata/5668e3800636a6cd6a000018/csharp
+
+7 kyu
+Ghostbusters (whitespace removal)
+
+Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
+
+In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+
+So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+
+Example:
+
+ghostBusters("Sky scra per");
+Should return:
+
+"Skyscraper"
+If the building contains no ghosts, return the string:
+
+"You just wanted my autograph didn't you?"
+*/
+
+
+using System.Linq;
+
+namespace CodeWars._7kyu;
+
+public class GhostbustersWhitespaceRemoval
+{
+    public static string GhostBusters(string building)
+    {
+        // return building.Any(c => c == ' ')
+        //     ? string.Concat(building.Where(c => c != ' '))
+        //     : "You just wanted my autograph didn't you?";
+
+        // return building.IndexOf(' ') > -1
+        //     ? string.Concat(building.Where(x => x != ' '))
+        //     : "You just wanted my autograph didn't you?";
+
+        // var s = building.Replace(" ", "");
+        // return s.Length == building.Length
+        //     ? "You just wanted my autograph didn't you?"
+        //     : s;
+
+        // return building.Contains(' ')
+        //     ? building.Replace(" ", "")
+        //     : "You just wanted my autograph didn't you?";
+
+        return building.Contains(' ')
+            ? building.Replace(" ", string.Empty)
+            : "You just wanted my autograph didn't you?";
+    }
+}
---
+++ CodeWarsTests/7kyu/GhostbustersWhitespaceRemovalTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeWars._7kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._7kyu;
+
+[TestFixture]
+public class GhostbustersWhitespaceRemovalTests
+{
+    [Test]
+    public void GenericTests()
+    {
+        Assert.AreEqual("Factory", GhostbustersWhitespaceRemoval.GhostBusters("Factor y"),
+            "Nope, there may still be a ghost in the building. Try again.");
+        Assert.AreEqual("Office", GhostbustersWhitespaceRemoval.GhostBusters("O  f fi ce"),
+            "Nope, there may still be a ghost in the building. Try again.");
+        Assert.AreEqual("You just wanted my autograph didn't you?",
+            GhostbustersWhitespaceRemoval.GhostBusters("BusStation"),
+            "Nope, as there were no ghosts in the BusStation you need to return a witty retort.");
+    }
+}
