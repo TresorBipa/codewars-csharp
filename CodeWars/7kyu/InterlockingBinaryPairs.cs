47326b47c7e24fd69d3cb1610750bf25aa0ae770
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-09-02 17:18:40 +0300

    Added solution "Interlocking Binary Pairs"

---
+++ CodeWars/7kyu/InterlockingBinaryPairs.cs
@@ -0,0 +1,47 @@
+﻿/*
+https://www.codewars.com/kata/628e3ee2e1daf90030239e8a/csharp
+
+7 kyu
+Interlocking Binary Pairs
+
+Task
+Write a function that checks if two non-negative integers make an "interlocking binary pair".
+
+Interlock ?
+numbers can be interlocked if their binary representations have no 1's in the same place
+comparisons are made by bit position, starting from right to left (see the examples below)
+when representations are of different lengths, the unmatched left-most bits are ignored
+Examples
+a = 9, b = 4
+Stacking representations shows how they can interlock. Here, no 1's share any position, so the function returns true.
+
+ 9    1001
+ 4     100
+a = 3, b = 6
+These representations do not interlock. Finding they both have a 1 in the same position, the function returns false.
+
+ 3      11
+ 6     110
+Input
+Two non-negative integers in the range 0 <= n <= 2 ** 64 - 1.
+
+Output
+Boolean true or false whether or not these integers are interlockable.
+
+Enjoy!
+Consider one of the following kata to solve next:
+
+Crossword Puzzle! (2x2)
+Four Letter Words ~ Mutations
+Is Sator Square?
+Playing With Toy Blocks ~ Can you build a 4x4 square?
+*/
+
+
+namespace CodeWars;
+
+public static class InterlockingBinaryPairs
+{
+    public static bool Interlockable(ulong a, ulong b) => (a & b) == 0;
+}
+
---
+++ CodeWarsTests/7kyu/InterlockingBinaryPairsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests;
+
+[TestFixture]
+public class InterlockingBinaryPairsTests
+{
+    [Test]
+    public void SampleTests()
+    {
+        Object[][] tests =
+        {
+            new Object[] {true, 9UL, 4UL},
+            new Object[] {false, 5UL, 6UL},
+            new Object[] {true, 2UL, 5UL},
+            new Object[] {false, 7UL, 1UL},
+            new Object[] {true, 0UL, 8UL}
+        };
+        foreach (Object[] test in tests)
+        {
+            bool expected = (bool) test[0];
+            ulong a = (ulong) test[1];
+            ulong b = (ulong) test[2];
+            bool submitted = InterlockingBinaryPairs.Interlockable(a, b);
+            string message = "a = " + a + "\n  b = " + b;
+            Assert.AreEqual(expected, submitted, message);
+        }
+    }
+}
