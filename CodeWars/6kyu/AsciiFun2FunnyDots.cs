de678a1175fce61a1d0f4d5042943febc7731540
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-09-08 15:19:38 +0300

    Add solution "ASCII Fun #2: Funny Dots"

---
+++ CodeWars/6kyu/AsciiFun2FunnyDots.cs
@@ -0,0 +1,44 @@
+﻿/*
+https://www.codewars.com/kata/59098c39d8d24d12b6000020/csharp
+
+6 kyu
+ASCII Fun #2: Funny Dots
+
+Funny Dots
+You will get two integers n (width) and m (height) and your task is to draw the following pattern. 
+Each line is seperated with a newline (\n)
+
+Both integers are equal or greater than 1; no need to check for invalid parameters.
+
+Examples
+
+                                            +---+---+---+
+             +---+                          | o | o | o |
+dot(1, 1) => | o |            dot(3, 2) =>  +---+---+---+            
+             +---+                          | o | o | o |
+                                            +---+---+---+
+
+Serie: ASCII Fun
+ASCII Fun #1: X-Shape
+ASCII Fun #2: Funny Dots
+ASCII Fun #3: Puzzle Tiles
+ASCII Fun #4: Build a pyramid
+*/
+
+
+using System.Linq;
+
+namespace CodeWars._6kyu;
+
+public static class AsciiFun2FunnyDots
+{
+    public static string Dot(int n, int m)
+    {
+        var a = string.Concat(Enumerable.Repeat("+---", n)) + "+\n";
+        var b = "" + string.Concat(Enumerable.Repeat("| o ", n)) + "|\n";
+        return string.Concat(Enumerable.Repeat(a + b, m)) + a[..^1];
+    }
+}
+
+
+
---
+++ CodeWarsTests/6kyu/AsciiFun2FunnyDotsTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeWars._6kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._6kyu;
+
+[TestFixture]
+public class AsciiFun2FunnyDotsTests
+{
+    [Test]
+    public void SampleTests()
+    {
+        Assertion((1, 1), "+---+\n| o |\n+---+");
+        Assertion((3, 2), "+---+---+---+\n| o | o | o |\n+---+---+---+\n| o | o | o |\n+---+---+---+");
+    }
+
+    private static void Assertion((int, int) inputs, string expected) =>
+        Assert.AreEqual(
+            expected,
+            AsciiFun2FunnyDots.Dot(inputs.Item1, inputs.Item2),
+            $"\n  N = {inputs.Item1}" +
+            $"\n  M = {inputs.Item2}"
+        );
+}
