76d55a3b305db539b86b8a174a8fd2bde1bf29b5
Alexander Kozhanov <a.kozhanov@outlook.com>
2022-09-09 15:25:23 +0300

    Add solution "Product of consecutive Fib numbers"

---
+++ CodeWarsTests/5kyu/ProductOfConsecutiveFibNumbersTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace CodeWarsTests;
+
+[TestFixture]
+public class ProductOfConsecutiveFibNumbersTests
+{
+    [Test]
+    public void Test1()
+    {
+        ulong[] r = new ulong[] { 55, 89, 1 };
+        Assert.AreEqual(r, ProductOfConsecutiveFibNumbers.productFib(4895));
+    }
+}
